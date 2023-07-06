f31dc129e623e2220be07b5010f167fbc2ff412a
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-07-06 13:59:17 +0100

    Add new baseline migration to prisma schema

--- services/pipedriveInterface.ts
+++ services/pipedriveInterface.ts
@@ -1,19 +1,15 @@
 // Responsible for updating the database given the state of pipedrive.
-// There's only a subset of information on Pipedrive we actually care about for
-// the installer <-> customer matching algorithm.
 import axios from 'axios';
-import { PrismaClient } from '@prisma/client';
+import { Installer, Job, PrismaClient } from '@prisma/client';
 
 
 const prisma = new PrismaClient();
 
+const PIPEDRIVE_API_TOKEN ='77a5356773f422eb97c617fd7c37ee526da11851'
 
 export async function getInstallerDataFromPipedrive() {
   const response = await axios.get('https://api.pipedrive.com/api/v1/organizations', {
-      params: {
-          api_token: '77a5356773f422eb97c617fd7c37ee526da11851',
-          filter_id: 115
-      }
+      params: { api_token: PIPEDRIVE_API_TOKEN, filter_id: 115 },
   });
 
   const responseData = response.data;
@@ -24,28 +20,17 @@ export async function getInstallerDataFromPipedrive() {
 
   const orgs = responseData.data;
 
+  console.log(orgs);
+
   // TODO: get latitude and longitude
   let latitude = 90.0;
   let longitude = 100.0;
 
-  type Installer = {
-      id: number;
-      name: string;
-      address: string;
-      postcode: string;
-      latitude: string;
-      longitude: string;
-      isPartner: boolean;
-  };
-
   const installers: Installer[] = orgs.map(org => ({
-    // In pipedrive
     id: org.id,
     name: org.name,
     address: org.address,
     postcode: org.address_postal_code,
-
-    // Not in pipedrive
     latitude: latitude,
     longitude: longitude,
     isPartner: false
@@ -54,25 +39,91 @@ export async function getInstallerDataFromPipedrive() {
   return installers;
 }
 
+export async function getJobDataFromPipedrive() {
+  try {
+    console.log("getting job data from pipedrive...");
+
+    const response = await axios.get('https://api.pipedrive.com/api/v1/pipelines/23/deals', {
+      params: { api_token: PIPEDRIVE_API_TOKEN }
+    });
+
+    console.log("done");
+    const responseData = response.data;
+
+    if (!responseData["success"]) {
+      return [];
+    }
+
+    const jobData = responseData.data;
+
+    // TODO: get latitude and longitude
+    let latitude = 15.0;
+    let longitude = 30.0;
+
+    const jobs: Job[] = jobData.map(job => ({
+      id: job.id,
+      // customerName: job.name,
+      customerName: "Joe Bloggs",
+      // address: job.address,
+      address: "some place",
+      postcode: job['80ebeccb5c4130caa1da17c6304ab63858b912a1_postal_code'],
+      latitude: latitude,
+      longitude: longitude
+    }));
+
+    return jobs;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function syncInstallers() {
+  const installers = await getInstallerDataFromPipedrive();
+
+  const operations = installers.map((installer) => {
+    installer.postcode = installer.postcode === null ? "NA" : installer.postcode;
+
+    let { id, ...new_details } = installer;
+
+    return prisma.installer.upsert({
+      where: { id: installer.id },
+      update: installer,
+      create: new_details
+    })
+  });
+
+  await prisma.$transaction(operations);
+}
+
+async function syncJobs() {
+  const jobs = await getJobDataFromPipedrive();
+
+  const operations = jobs.map((job) => {
+    job.postcode = job.postcode === null ? "NA" : job.postcode;
+
+    let { id, ...new_details } = job;
+
+    return prisma.job.upsert({
+      where: { id: job.id },
+      update: job,
+      create: new_details
+    })
+  });
+
+  await prisma.$transaction(operations);
+}
+
+async function syncDeals() {
+  // todo
+}
+
 export async function syncDatabaseWithPipedrive() {
   try {
-    const installers = await getInstallerDataFromPipedrive();
-
-    // const user = await prisma.installer.updateMany({
-    //   data: {
-    //     postcode: "NEW POSTCODE HERE"
-    //   }
-    // })
-
-    const operations = installers.map((installer) =>
-      prisma.installer.updateMany({
-        where: { id: installer.id },
-        data: {
-          postcode: "bob"
-        }
-      })
-    );
-    await prisma.$transaction(operations);
+    console.log("synchronising database with pipedrive...");
+    // await syncInstallers();
+    await syncJobs();
+    // syncDeals();
 
   } catch (error) {
     console.error('Error fetching or storing data:', error);
