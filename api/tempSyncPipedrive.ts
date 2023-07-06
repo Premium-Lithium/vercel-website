d033086f2b1c510d4c17b0bfafaa6c274acaac7b
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-07-06 09:12:19 +0100

    Rename temporary sync api

---
+++ api/tempSyncPipedrive.ts
@@ -0,0 +1,18 @@
+// Temporary endpoint for manually triggering pipedrive fetch
+import { syncDatabaseWithPipedrive } from '../services/pipedriveInterface.js'
+
+export default async function (req, res) {
+  if (req.method === 'GET') {
+    try {
+
+      await syncDatabaseWithPipedrive();
+      res.status(200).json({ message: 'Pipedrive sync was fine.' });
+
+    } catch (error) {
+      console.error('Error during Pipedrive sync:', error);
+      res.status(500).json({ message: 'An error occurred while fetching or storing data' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' }); // Only allow POST requests
+  }
+}
