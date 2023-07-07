3fc07cc39807524f47a3a15c692c339de4c1e886
Evan Snoswell <evan.snoswell@gmail.com>
2023-07-07 08:38:16 +0000

    fix parsing of deal id route parameter

--- src/routes/installer_portal/leads/[deal_id]/+page.server.ts
+++ src/routes/installer_portal/leads/[deal_id]/+page.server.ts
@@ -1,14 +1,15 @@
 import prisma from '$lib/prisma';
 
 export const load = async ({ params: { deal_id } }) => {
+    const dealInt = parseInt(deal_id)
     const response = await prisma.Deal.findUnique({
         where: {
-            id: parseInt(deal_id),
+            id: dealInt,
         },
         include: {
             Job: true,
         },
     })
-    return { deal_id, data: response };
+    return { deal_id: dealInt, data: response };
 }
 
