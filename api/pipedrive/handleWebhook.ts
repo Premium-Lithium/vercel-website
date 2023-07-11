2f64cdf307a21161954fac965ec3aa1305cf64a6
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-07-11 22:11:26 +0100

    Print webhook update type

--- api/pipedrive/handleWebhook.ts
+++ api/pipedrive/handleWebhook.ts
@@ -6,31 +6,34 @@ const prisma = new PrismaClient();
 
 
 export default async function (request: VercelRequest, response: VercelResponse) {
-  console.log("Syncing installer data...");
-
   if (request.method !== 'GET')
     return response.status(405).json({ message: 'Method not allowed' });
 
   const update = request.body;
   if (update.meta.object == "organization") {
-    var installer = {
-      id: update.current.id,
-      address: update.current., // todo
-      postcode: update.current.,
+    console.log("Syncing organization data...");
 
-    };
+    // var installer = {
+    //   id: update.current.id,
+    //   address: update.current., // todo
+    //   postcode: update.current.,
 
-    return prisma.installer.upsert({
-      where: { id: installer.id },
-      update: installer_data,
-      create: installer
-    });
+    // };
 
-    await prisma.$transaction(operations);
+    // return prisma.installer.upsert({
+    //   where: { id: installer.id },
+    //   update: installer_data,
+    //   create: installer
+    // });
+
+    // await prisma.$transaction(operations);
 
   }
   else if (update.meta.object == "deal") {
-
+    console.log("Syncing deal data...");
+  }
+  else {
+    console.log("Unknown webhook type.");
   }
 
   var success = true;
@@ -50,7 +53,4 @@ export default async function (request: VercelRequest, response: VercelResponse)
     return response.status(500).json({ message: 'Failed to update installer data.' });
 
   return response.status(200).json({ message: 'Successfully updated installer data.' });
-}
-
-async function syncInstallerData(todo_installerUpdateData) {
 }
