108612bd27e542b6e1da7911c01ee6aa91dd6a79
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-07-17 14:13:14 +0100

    Fix LeadView component not rendering

--- src/routes/installer-portal/+page.server.ts
+++ src/routes/installer-portal/+page.server.ts
@@ -51,7 +51,7 @@ export const load = async () => {
         })
         return {data: response};
     } catch(e) {
-        console.log("we avoided the error!", e);
+        console.log("we avoided the error!\n", e);
         return {
             data: null
         }
