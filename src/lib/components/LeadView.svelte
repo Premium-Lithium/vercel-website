1ded48a7aebaf8694e8835258a88afd3ea85d193
Joe Lonsdale <jl3067@york.ac.uk>
2023-07-20 14:22:33 +0100

    Add logic for modal popup, along with updating pending/accepted deals lists.
    Update to fix reactive statement not triggering.

--- src/lib/components/LeadView.svelte
+++ src/lib/components/LeadView.svelte
@@ -14,69 +14,97 @@
 
     export let data;
     
-
     let possibleFilters = ["REJECTED", "ACCEPTED", "PENDING"];
     let currentFilters = [...possibleFilters];
     let acceptedDeals;
     let pendingDeals;
 
-    let rejectModal, acceptModal = false;
-    let currentDealId;
-    let dialog;
+    let rejectModal = false;
+    let acceptModal = false;
+    let rejectDialog = false;
+    let acceptDialog = false;
+
+    let currentDealId = -1;
 
     let failedLoad = (data.data === null);
-    if (failedLoad) {
+    $: {
+      if (failedLoad) {
         console.log("failed to load")
-    } else {
-        $: acceptedDeals = data.data.Deals
+      } else {
+        acceptedDeals = data.data.Deals
             .filter((deal) => deal.status === 'ACCEPTED')
-        $: pendingDeals = data.data.Deals
+        pendingDeals = data.data.Deals
             .filter((deal) => deal.status === 'PENDING')
+      }
     }
 
+    function openAcceptModal(deal) {
+      acceptModal=true;
+      currentDealId = deal.id;
+    }
 
+    function openRejectModal(deal) {
+      rejectModal=true;
+      currentDealId = deal.id;
+    }
 
     async function acceptLead(dealId) {
-        const acceptUrl = `${$page.url.origin}/api/installer/leads/accept-lead`
+      // Update the client-side acceptedDeals and pendingDeals list
+      // while we wait for a response from server.
+      acceptedDeals = acceptedDeals.concat(pendingDeals.filter((deal) => deal.id === dealId));
+      pendingDeals = [...pendingDeals].filter((deal) => deal.id !== dealId);
+      const acceptUrl = `${$page.url.origin}/api/installer/leads/accept-lead`
 
-        const res = await fetch(acceptUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                "deal_id": dealId,
-            }),
-        })
+      const res = await fetch(acceptUrl, {
+          method: 'POST',
+          body: JSON.stringify({
+              "deal_id": dealId,
+          }),
+      })
 
-        invalidateAll();
+      const response = await res.json();
+      await invalidateAll();
     }
 
     async function rejectLead(dealId) {
-        const rejectUrl = `${$page.url.origin}/api/installer/leads/reject-lead`
+      // Update the client-side pendingDeals list, while we wait for a response from server.
+      pendingDeals = [...pendingDeals].filter((deal) => dealId !== deal.id);
+      const rejectUrl = `${$page.url.origin}/api/installer/leads/reject-lead`
 
-        console.log("Before the fetch")
-        const res = await fetch(rejectUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                "deal_id": dealId,
-            }),
-        });
+      const res = await fetch(rejectUrl, {
+          method: 'POST',
+          body: JSON.stringify({
+              "deal_id": dealId,
+          }),
+      });
 
-        const data = await res.json();
-
-        invalidateAll();
-      }
+      const response = await res.json();
+      await invalidateAll();
+    }
 </script>
 
 This is the lead view
 {#if failedLoad}
     Failed Load
 {:else}
-<Modal bind:showModal={rejectModal} bind:dialog>
+
+<Modal bind:showModal={rejectModal} bind:dialog={rejectDialog}>
   <h2 slot="header" class="modal-header">
-    Are you sure?
+    Are you sure you want to reject this lead?
   </h2>
   <div class="modal-outer">
-    <button on:click={() => {dialog.close()}}>no</button>
-    <button on:click={async () => {dialog.close(); await rejectLead(currentDealId)}}>yes</button>
+    <button on:click={() => {rejectDialog.close()}}>no</button>
+    <button on:click={async () => {rejectDialog.close(); await rejectLead(currentDealId)}}>yes</button>
+  </div>
+</Modal>
+
+<Modal bind:showModal={acceptModal} bind:dialog={acceptDialog}>
+  <h2 slot="header" class="modal-header">
+    Are you sure you want to accept this lead?
+  </h2>
+  <div class="modal-outer">
+    <button on:click={() => {acceptDialog.close()}}>no</button>
+    <button on:click={async () => {acceptDialog.close(); await acceptLead(currentDealId)}}>yes</button>
   </div>
 </Modal>
 
@@ -84,16 +112,16 @@ This is the lead view
     <Filter bind:currentFilters bind:possibleFilters/> 
 
   <div class="title">New Leads:</div>
-    {#each pendingDeals as deal}
+    {#each pendingDeals as deal (deal.id)}
     {#if currentFilters.includes(deal.status)}
     <div class="deal-container" transition:slide>
         <div class="deal-header">
             <a href="/installer_portal/leads/{deal.id}" class="deal-link">{deal.Job.customerName ?? "Customer"} at {deal.Job.postcode.toString().toUpperCase()} ...</a>
           <div>
-              <a href="" on:click={() => {rejectModal=true; currentDealId = deal.id}}>
+              <a href="" on:click={() => {openRejectModal(deal)}}>
                   <Close/>
               </a>
-              <a href="" on:click={() => {acceptModal=true; currentDealId = deal.id}}>
+              <a href="" on:click={() => {openAcceptModal(deal)}}>
                   <Check/>
               </a>
           </div>
@@ -103,7 +131,7 @@ This is the lead view
     {/each}
 
   <div class="title">Accepted Leads:</div>
-    {#each acceptedDeals as deal}
+    {#each acceptedDeals as deal (deal.id)}
     {#if currentFilters.includes(deal.status)}
     <div class="deal-container" transition:slide>
       <Accordian>
