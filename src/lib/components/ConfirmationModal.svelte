49391b877d151a15840e08245b3f7e2e2f1b7980
Joe Lonsdale <jl3067@york.ac.uk>
2023-07-20 17:29:22 +0100

    Changed how the header is styled

--- src/lib/components/ConfirmationModal.svelte
+++ src/lib/components/ConfirmationModal.svelte
@@ -9,29 +9,29 @@
 </script>
 
 <Modal bind:showModal bind:dialog>
-    <slot name="header" class="modal-header">
-        <h2>Are you sure?</h2>
-    </slot> 
+    <div slot="header" class="modal-header">
+        <slot name="header"/>
+    </div>
     <div class="modal-outer">
         <button on:click={noFunc}>no</button>
         <button on:click={yesFunc}>yes</button>
     </div>
 </Modal>
-
 <style>
+
 /* Modal Styles */
 
 .modal-header {
   font-family: 'Roboto', sans-serif;
-  font-size: 24px;
+  font-size: 14px;
   font-weight: 500;
   color: #1C2428;
   margin-bottom: 10px;
 }
 
-.modal-outer {
+.modal-outer{
   display: flex;
   justify-content: space-between;
-  padding: 5px;
+  padding: 10px;
 }
 </style>
