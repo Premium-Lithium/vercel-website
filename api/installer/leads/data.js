40c09c4167fd472d59743c9daf9933fd420d7aa0
Evan Snoswell <evan.snoswell@gmail.com>
2023-07-20 16:30:25 +0000

    WIP

--- api/installer/leads/data.js
+++ api/installer/leads/data.js
@@ -7,14 +7,11 @@ const prisma = new PrismaClient();
 const jwksUri = "https://dev-a8lw8imwybb5wby4.uk.auth0.com/.well-known/jwks.json";
 
 export default async function (request, response) {
-    console.log(request.headers.authorization)
     const accessToken = request?.headers?.authorization;
 
     if (accessToken === undefined) return;
 
     const userinfoAuthUrl = "https://dev-a8lw8imwybb5wby4.uk.auth0.com/userinfo";
-
-    const tempAccessToken = "Bearer eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIiwiaXNzIjoiaHR0cHM6Ly9kZXYtYThsdzhpbXd5YmI1d2J5NC51ay5hdXRoMC5jb20vIn0..wpLmxQrMSWWyGbwQ.xxQDcRZ4X3DgE4PBj8iiHMsA-TUzFsIg0mmTk0ykXGaDRybSCekp1T6q3RiCcRrQGpUvgbTII3619Zdb3owiHVnuB_TeNKvfWx8QQ0Wc4jHLQtIw60k3Dj4gyUsfivZl7NCn1TXLN_-xX_59CHpQQLSPq0jyJ89qc3Thjupqv74uZ-ACHFSuCLlQDTJjs-3aSlKDgclK-ezlmqE0NO-kDZSFsguhBURGcNCMXcjb8n7PlhbGr8cxQAOseDBxq3hs9j7S9E-jSDMrvmEbol_WQAZKJglZjBhGbiCjtUTaL3WRzhODI0tJXlhN3Axf4BFR8fyNLwRmxt23Lc4tUc_P2GHRoc3KpQE.REO3Tx_ZazjH6inE6pU0tA"
 
     const res = await fetch(userinfoAuthUrl, {
         method: 'GET',
@@ -23,10 +20,57 @@ export default async function (request, response) {
         },
     })
 
+    try {
+        const result = res.json()
+        const returnData = load(result['http://localhost:3000/userdata'].installerId)
+        response.status(200).json(returnData)
 
-
+    } catch (e) {
+        response.status(400).json({nope: "my dude"})
+    }
     console.log(await res.text())
 
+
+
+}
+
+async function load(id) {
+    console.log("Trying to find installer with id ", id);
+
+    try {
+        const response = await prisma.installer.findUnique({
+            where: {
+                'id': id
+            },
+            include: {
+                Deals: {
+                    where: {
+                        status: { not: 'REJECTED' },
+                    },
+                    include: {
+                        Job: true,
+                    },
+                },
+            },
+        });
+        if (response === undefined) {
+            return {
+                data: null
+            }
+        }
+        response.Deals.forEach((deal) => {
+            if (deal.status === 'ACCEPTED')
+                return;
+        
+            deal.Job = censorSensitiveJobInfo(deal.Job)
+        })
+        return {data: response.json()};
+    } catch(e) {
+        console.log("We avoided the error!\n", e);
+        return {
+            data: null
+        }
+    }
 
 
 }
