8ed2e0f54231ffa4547584a570a147a0f8b0bd34
lbowes <33807016+lbowes@users.noreply.github.com>
2023-07-21 15:28:48 +0100

    Add deal expiry logic

--- api/installer/leads/accept-lead.js
+++ api/installer/leads/accept-lead.js
@@ -1,21 +1,85 @@
 import { PrismaClient, DealStatus } from "@prisma/client"
 
+
 const prisma = new PrismaClient()
 
+
 export default async function (request, response) {
-    const json = JSON.parse(request.body)
-    const dealId = json.deal_id;
+  const dealId = request.body.deal_id;
 
-    const result = await prisma.deal.updateMany({
-        where: {
-            id: dealId,
-            status: DealStatus.PENDING,
-        },
-        data: {
-            status: DealStatus.ACCEPTED,
-        },
-    })
+  const deal = await prisma.deal.findUnique({
+    where: {
+      id: dealId,
+    },
+    select: {
+      id: true,
+      status: true,
+      Job: {
+        select: {
+          numQuotes: true,
+          Deals: {
+            select: {
+              id: true,
+              status: true,
+            }
+          }
+        }
+      }
+    }
+  });
 
-    return response.status(200).json({ message: 'Successfully updated installer data.' });
+  if (deal.status !== DealStatus.PENDING)
+  return response.status(400).json({ message: 'Deal is not pending. Can only accept pending deals.' });
+
+  const numQuotesNeeded = deal.Job.numQuotes;
+  const otherDealsOnThisJob = deal.Job.Deals.filter(d => d.id !== deal.id);
+  const numAcceptedDeals = otherDealsOnThisJob.filter(d => d.status === DealStatus.ACCEPTED).length;
+
+  // Rare edge case where someone has accepted this deal before we could update it
+  if(numAcceptedDeals >= numQuotesNeeded)
+    return response.status(400).json({ message: 'This job has already received enough quotes.' });
+
+  // Accept this deal
+  acceptDeal(dealId);
+
+  // If the total number of accepted deals now we've accepted this one is sufficient, then we need to expire all other non-accepted deals
+  if(numAcceptedDeals + 1 >= numQuotesNeeded)
+    expirePending(otherDealsOnThisJob);
+
+  return response.status(200).json({ message: 'Successfully updated installer data.' });
 }
 
+
+async function acceptDeal(dealId) {
+  console.log("Accepting deal: ", dealId);
+
+  const result = await prisma.deal.update({
+    where: {
+      id: dealId
+    },
+    data: {
+      status: DealStatus.ACCEPTED,
+    },
+  });
+
+  // TODO: add return code here
+}
+
+
+async function expirePending(deals) {
+  const pendingDealIds = deals.filter(d => d.status === DealStatus.PENDING).map(d => d.id);
+  console.log("Expiring deals: ", pendingDealIds);
+
+  const result = await prisma.deal.updateMany({
+    where: {
+      id: {
+        in: pendingDealIds,
+      },
+    },
+    data: {
+      status: DealStatus.EXPIRED,
+    },
+  });
+
+  // TODO: add return code here
+}
