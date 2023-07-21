c90ab14372125e4da5dabd1a3ce6ef55ae27d47a
lbowes <33807016+lbowes@users.noreply.github.com>
2023-07-21 15:57:29 +0100

    Add missing `await` to `acceptDeal` call

--- api/installer/leads/accept-lead.js
+++ api/installer/leads/accept-lead.js
@@ -1,12 +1,15 @@
 import { PrismaClient, DealStatus } from "@prisma/client"
+import { log } from "console";
 
 
 const prisma = new PrismaClient()
 
 
 export default async function (request, response) {
-  const dealId = request.body.deal_id;
+  const json = JSON.parse(request.body)
+  const dealId = json.deal_id;
 
+  console.log("Finding info on deal: ", dealId);
   const deal = await prisma.deal.findUnique({
     where: {
       id: dealId,
@@ -29,22 +32,28 @@ export default async function (request, response) {
   });
 
   if (deal.status !== DealStatus.PENDING)
-  return response.status(400).json({ message: 'Deal is not pending. Can only accept pending deals.' });
+    return response.status(400).json({ message: 'Deal is not pending. Can only accept pending deals.' });
 
   const numQuotesNeeded = deal.Job.numQuotes;
   const otherDealsOnThisJob = deal.Job.Deals.filter(d => d.id !== deal.id);
   const numAcceptedDeals = otherDealsOnThisJob.filter(d => d.status === DealStatus.ACCEPTED).length;
+
+  console.log("numQuotesNeeded: ", numQuotesNeeded);
+  console.log("number of other deals on this job: ", otherDealsOnThisJob.length);
+  console.log("numAcceptedDeals: ", numAcceptedDeals);
 
   // Rare edge case where someone has accepted this deal before we could update it
   if(numAcceptedDeals >= numQuotesNeeded)
     return response.status(400).json({ message: 'This job has already received enough quotes.' });
 
   // Accept this deal
-  acceptDeal(dealId);
+  await acceptDeal(dealId);
 
   // If the total number of accepted deals now we've accepted this one is sufficient, then we need to expire all other non-accepted deals
-  if(numAcceptedDeals + 1 >= numQuotesNeeded)
+  if(numAcceptedDeals + 1 >= numQuotesNeeded) {
+    console.log("We have enough quotes now. Expiring other deals.")
     expirePending(otherDealsOnThisJob);
+  }
 
   return response.status(200).json({ message: 'Successfully updated installer data.' });
 }
@@ -61,6 +70,8 @@ async function acceptDeal(dealId) {
       status: DealStatus.ACCEPTED,
     },
   });
+
+  console.log("Successfully accepted deal: ", result);
 
   // TODO: add return code here
 }
