1ecb3f9465418f6ec635f25e427b58f1cd79ff2b
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-07-26 16:46:09 +0100

    fixed error

--- api/blacklistEmail.js
+++ api/blacklistEmail.js
@@ -21,7 +21,5 @@ export default async function PUT(request, response) {
             return response.status(200).json({message: "done"});
         }
     });
-    return response.status(200).json({message: "done"});
-
 }
 
