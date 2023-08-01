805b079e85df1e59fb2224ce1266987098b2b991
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-01 17:54:29 +0100

    Working LangChain implementation of ConversationalRetrievalQAChain. Allows a conversation flow with references to an external knowledgebase document.

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -1,55 +1,162 @@
 import { OPENAI_API_KEY, OPENAI_ORG_ID, KV_URL } from '$env/static/private';
 import { Configuration, OpenAIApi } from "openai";
 import { json } from '@sveltejs/kit';
-import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { RetrievalQAChain } from "langchain/chains";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { onMount } from 'svelte';
-import { Document } from 'langchain/document';
+import { BufferMemory } from 'langchain/memory';
+
+const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question,\
+return the conversation history excerpt that includes any relevant context to the question if it exists\
+and rephrase the follow up question to be a standalone question.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Your answer should follow the following format:
+\`\`\`
+Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------
+<Relevant chat history excerpt as context here>
+Standalone question: <Rephrased question here>
+\`\`\`
+Your answer:`;
 
 let chain;
 let model;
 let vectorStore;
 if(true){
-    // const loader = new CheerioWebBaseLoader("https://premiumlithium.com/products/powerpod?_pos=1&_fid=0a2ecd092&_ss=c");
-    // const data = await loader.load();
-    // const textSplitter = new RecursiveCharacterTextSplitter( {
-    //     chunkSize: 500,
-    //     chunkOverlap: 0,
-    // })
+    // do embedding, we will want to store this at some point.
+    const text = `SAVE UP TO 55% WITH A PRE-ORDER ON ALL PRODUCTS
+    Choose your delivery date:
+    
+    JUN '24 | Save 55%
+    
+    -------------------------------------
+    
+    Faraday - Your Portable Powerhouse
+    Sale price
+    £313 (ex. VAT)
+    Regular price
+    £695
+    
+    Product power: 1.28kWh
+    Nominal voltage: 12V (12.8V)
+    Nominal capacity: 100Ah
+    Dimensions: 330 x 172 x 217 mm
+    Weight: 12kg
+    
+    --------------------------------------
+    
+    Edison - Power Meets Portability
+    Sale price
+    £628 (ex. VAT)
+    Regular price
+    £1,395
+    
+    Product power: 2.56kWh
+    Nominal voltage: 12V (12.8V)
+    Nominal capacity: 200Ah
+    Dimensions: 521 x 238 x 218 mm
+    Weight: 22kg
+    
+    --------------------------------------
+    
+    Franklin - Power Meets Portability
+    Sale price
+    £628 (ex. VAT)
+    Regular price
+    £1,395
+    
+    Product power: 2.56kWh
+    Nominal voltage: 24V (25.6V)
+    Nominal capacity: 100Ah
+    Dimensions: 521 x 238 x 218 mm
+    Weight: 22kg
+    
+    --------------------------------------
+    
+    Maxwell - Unrivalled Power
+    Sale price
+    £1,213 (ex. VAT)
+    Regular price
+    £2,695
+    
+    Product power: 5.12kWh
+    Nominal voltage: 24V (25.6V)
+    Nominal capacity: 200Ah
+    Dimensions: 520 x 240 x 270 mm
+    Weight: 42kg
+    
+    --------------------------------------
+    
+    Watson - Unrivalled Power
+    Sale price
+    £1,213 (ex. VAT)
+    Regular price
+    £2,695
+    
+    Product power: 5.12kWh
+    Nominal voltage: 48V (51.2V)
+    Nominal capacity: 100Ah
+    Dimensions: 520 x 360 x 190 mm/
+    Weight: 45kg/
+    
+    --------------------------------------/
+`
+    const textSplitter = new RecursiveCharacterTextSplitter( {
+        chunkSize: 500,
+        chunkOverlap: 0,
+    })
+    const docs = await textSplitter.createDocuments([text]);
 
-    // const splitDocs = await textSplitter.splitDocuments(data);
-
-    const docs = [
-    new Document({
-        metadata: { foo: "bar" },
-        pageContent: "redis is fast",
-    }),
-    new Document({
-        metadata: { foo: "bar" },
-        pageContent: "the quick brown fox jumped over the lazy dog",
-    }),
-    new Document({
-        metadata: { baz: "qux" },
-        pageContent: "lorem ipsum dolor sit amet",
-    }),
-    new Document({
-        metadata: { baz: "qux" },
-        pageContent: "consectetur adipiscing elit",
-    }),
-    ];
+    const splitDocs = await textSplitter.splitDocuments(docs);
 
     vectorStore = await MemoryVectorStore.fromDocuments(
-        docs,
+        splitDocs,
         new OpenAIEmbeddings(),
     );
-    // // We will want to store this embedding somewhere, and load it instead of creating it each time it's needed.
 
-    model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
-    chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    model = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0.2, maxTokens: 250 });
+
+
+    // let parser = new CustomListOutputParser({length: 3, separator: "|"});
+    // const prompt = new ChatPromptTemplate({
+    //     promptMessages: [ 
+    //         SystemMessagePromptTemplate.fromTemplate("You are a helpful, friendly, upbeat customer assistant named Evie for one of the fastest growing\
+    //         green energy companies in the UK: Premium Lithium, who provide integrated solutions\
+    //         enabling energy dependence, from design, manufacturing and installation of\
+    //         Smart Home Batteries, Solar Panels, EV Chargers, and UPS. Do your best to answer the above query, and then provide\
+    //         3 follow up questions which the user may ask. {formatInstructions}"),
+    //         // The customer will ask you questions, and your only goal is to provide them with the necessary\
+    //         // information and ask them any essential follow up questions in order for them to choose which system\
+    //         // to purchase. {formatInstructions} After each answer you should provide 3 follow up questions which the user may want to ask, based on their query.\n\
+    //         // Here's an example of an initial prompt you should send:\
+    //         // Hello, how can I assist you today?|I'd like to know about your solar panels|\
+    //         // I'd like to know about your EV chargers|How much power would I get from solar panels on my roof?\n
+    //         HumanMessagePromptTemplate.fromTemplate("{inputText}"),
+    //     ],
+    //     inputVariables: ["inputText"],
+    //     partialVariables: {
+    //         formatInstructions: parser.getFormatInstructions(),
+    //     },
+    // });    
+    
+    chain = ConversationalRetrievalQAChain.fromLLM(
+        model, 
+        vectorStore.asRetriever(),
+        {
+        memory: new BufferMemory({
+            memoryKey: "chat_history",
+            returnMessages: true,
+        }),
+        questionGeneratorChainOptions: {
+            template: CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT,
+        }
+        },   
+    );
 }
 
 export async function POST({ request }) {
@@ -57,9 +164,8 @@ export async function POST({ request }) {
         const { prompt } = await request.json();
         console.log(prompt[prompt.length-1]['content']);
         const response = await chain.call({
-            query: prompt[prompt.length-1]['content']
+            question: prompt[prompt.length-1]['content']
         });
-        console.log(response);
         return json({message: response}, {status: 200});
     } catch (error)
     {
@@ -78,8 +184,8 @@ const completion = async (messages) => {
     await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: messages,
-        max_tokens: 500,
-        temperature: 0.7,
+        max_tokens: 250,
+        temperature: 0.05,
     });
     return(output.data.choices[0].message);
 };
