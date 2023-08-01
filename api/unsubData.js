0550bee4370144a8abb082a2bc2f2eb3c4d4b290
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-08-01 15:42:31 +0100

    returns quotes data and unsubscribed data

--- api/unsubData.js
+++ api/unsubData.js
@@ -2,20 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function GET(request, response) {
-    const quotes = await prisma.quote.findMany();
-    const columns =   ['installerId', 
-        'dealId',
-        'totalQuote', 
-        'quoteLabour',
-        'quoteScaffolding',
-        'quoteMaterials',
-        'quoteCertification',
-        'dateOfCompletion',
-        'currTime'];
-    const quoteData = `${columns.join(',')}\n${quotes
-      .map((row) => columns.map((col) => row[col]).join(','))
-      .join('\n')}`;
-    console.log(quoteData);
     const unsubed = await prisma.unsubscribedEmails.findMany();
     const unsubcolumns = ['email', 'reason']
     const unsubedData = `${unsubcolumns.join(',')}\n${unsubed
@@ -23,6 +9,6 @@ export default async function GET(request, response) {
         .join('\n')}`;
     console.log(unsubedData);
     response.writeHead(200, { 'Content-Type': 'apllication/json' });
-    response.end(quoteData);
+    response.end(unsubedData);
     return response;
 }
