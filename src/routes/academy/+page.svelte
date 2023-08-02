59d90017716c5ca44ee58314ef4ce213a183ea3e
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-08-02 12:01:24 +0100

    changed wording

--- src/routes/academy/+page.svelte
+++ src/routes/academy/+page.svelte
@@ -1,11 +1,10 @@
-<script>
-
-</script>
-
 <div class="body">
     <img class="logo" src="https://premiumlithium.com/cdn/shop/files/Website_Logo_PNG_8c3726b3-6ebd-489e-9a38-06885f16236b.png?v=1653833196&width=500">
-    <h1>Academy Registration</h1>
-    <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/c52ZWEwVqlYePQNnG2tLjaykDOToJpX6swh3EoqHQMLiPud4OqfqVTM39prRN2pLIn"><script src="https://webforms.pipedrive.com/f/loader"></script></div>
+    <h1>Become an academy registration partner</h1>
+    <h2> Please complete the form below and one of the team will be in touch to discuss this great opportunity: </h2>
+    <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/c52ZWEwVqlYePQNnG2tLjaykDOToJpX6swh3EoqHQMLiPud4OqfqVTM39prRN2pLIn">
+        <script src="https://webforms.pipedrive.com/f/loader"></script>
+    </div>
     <script src="webform.js"></script>
 </div>
 
@@ -19,6 +18,7 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+        text-align: center;
     }
 
     .pipedriveWebForms {
