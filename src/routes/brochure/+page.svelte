047ded45853b38633920495ae1a97b435b192d9b
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-04 11:08:49 +0100

    WIP - brochure

--- src/routes/brochure/+page.svelte
+++ src/routes/brochure/+page.svelte
@@ -1,1 +1,112 @@
-<h1>brochure will go here </h1>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Brochure</title>
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Brochure",
+		"url": "/",
+		"logo": "https://premiumlithium.com/cdn/shop/files/Website_Logo_PNG_8c3726b3-6ebd-489e-9a38-06885f16236b.png?v=1653833196&width=500"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Brochure">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+    <link rel="canonical" href="/">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-home-page-title="Brochure" class="main-body" data-lang="en"><header class="header"></header>
+    <div class="logo-div">
+        <img src="https://premiumlithium.com/cdn/shop/files/Website_Logo_PNG_8c3726b3-6ebd-489e-9a38-06885f16236b.png?v=1653833196&amp;width=500" width=400 class="logo">
+    </div>
+    <div class="section partners">
+        <h1>Premium Partners</h1>
+        <h2>Introduction</h2>
+        <p> Firstly, thank for you considering partnering with Premium Lithium. Our objective is simple: to create a platform<br>that facilitates and supports the uptake of renewable energy solutions to assist the UK in achieving net zero.<br>We understand you’re likely incredibly busy, so we have kept this document as short as possible while ensuring<br>you have the essential information you need to work as one of our partners.
+        </p>
+        <h2>Why Partner with Premium Lithium?</h2>
+        <p> Premium Lithium are on the way to becoming the UK’s first and only renewable energy platform to assist and<br>support you in growing your renewable energy business.
+        </p>
+        <h3 >1. Perfect 5-Star Reviews<br>
+        </h3>
+        <p> Premium Lithium’s 100% 5-star reviews on Trustpilot reflect our dedication to perfection and customer<br>experience. As an exclusive installer partner with Premium Lithium, you’ll also benefit from the same 5-star<br>service, ensuring you are supported in your business growth by a company with a perfect track record.
+        </p>
+        <h3> 2. Exceptional Brand<br>
+        </h3>
+        <p> Premium Lithium has an exceptional brand and reputation already. We have been featured on Classic FM, LBC<br>Radio as well as in the National Trust. Customers come to us because they trust our brand and ability to support<br>them on their renewable journey. By partnering with Premium Lithium, you are working with one of the fastest<br>growing and most respected renewable companies in the industry.
+        </p>
+        <h3> 3. Complete One-Stop Supplier of Premium Hardware and Equipment for You<br>
+        </h3>
+        <p> Our Smart Batteries have best-in-class specifications and grade-A lithium cells, whilst also being modular and<br>scalable. We currently have stock in our UK warehouse ready to fulfil your installation requirements. We can also<br>supply you with solar panels, EV chargers and inverters too!
+        </p>
+        <h3> 4. Generous trade pricing providing excellent profit margins to you<br>
+        </h3>
+        <p> We manufacture / purchase at scale to offer you the best product at the best price, ensuring high customer<br>satisfaction levels and good profit margins for you too.
+        </p>
+        <h3> 5. Unlimited Support<br>
+        </h3>
+        <p> From installation guides, knowledge bases and ‘EVIE’ our AI chatbot, you’ll always have the right tools on hand for<br>support. Need the human touch? Our Installation Managers have decades of experience and are always available<br>by chat, email, or phone. In-person support can also be made available.
+        </p>
+    </div>
+    <div class="price-list section">
+        <h2>Price List</h2>
+    
+        <p>Please see below pricing. All prices are in £ and ex-VAT.<br>
+            To see a detailed spec sheet for the product, please click on the product SKU link.</p>
+    </div>
+</body></html>
+
+<style>
+    .logo-div {
+        position: absolute;
+        background-image: linear-gradient(180deg, rgba(255,255,255,1) 50%, rgba(255,255,255,0));
+        height: 100px;
+        width: 100vw;
+    }
+
+
+    .logo {
+        float: right;
+        right: 0;
+    }
+
+    .partners > h1,h2 {
+        color: var(--plblue);
+    }
+
+    .partners > h1 {
+        text-align: center;
+        font-size: 3em;
+
+    }
+
+    .partners > h2 {
+        font-size: 1.6em;
+        margin: 40px 0px 20px;
+    }
+
+    .partners > h3 {
+        font-size: 1.4em;
+        margin: 20px 0px 5px;
+    }
+
+    .partners > p, .price-list > p {
+        font-size: 1.3em;
+        margin-top: 10px;
+    }
+
+    .section {
+        padding: 10px 20px 40px;
+        border-bottom: #28AAE2 double 8px;
+    }
+
+    .partners {
+        margin-top: 100px;
+    }
+
+    .price-list {
+        margin-top: 40px;
+    }
+</style>
