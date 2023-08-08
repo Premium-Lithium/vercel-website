2ee09ce6db6711056d6360719019c435b37492eb
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-08 21:53:08 +0100

    Sketch out call hierarchy

--- src/routes/send-quote-request/+server.ts
+++ src/routes/send-quote-request/+server.ts
@@ -13,18 +13,36 @@ AjvErrors(ajv);
 
 export async function POST({ request }) {
     const requestData = await request.json();
+
+    // First check that the request body obeys the schema
     const validationErrors = validate(requestData);
 
-    // Check that the request body obeys the schema
     if(validationErrors.length) {
-        const message = validationErrors.join(", ");
-        return json({ message: `${message}` }, { status: 400 })
+        const errors = validationErrors.join(", ");
+        return json({ message: `${errors}` }, { status: 400 })
     }
 
-    // TODO: call internal send-email API to send quote request
+    // Then work out who we'd like to send emails to
+    let targetInstallers = [];
+    // todo: search for nearest installers here
+
+    // Remove any installers from the list who've already responded (either by quote, or asking to unsubscribe)
+    let installers = removeAlreadyResponded(targetInstallers);
+
+    if(installers.length == 0)
+        return json({ message: "All installers specified have either unsubscribed or already quoted for this job." }, { status: 202 })
+
+    // If there are people to send an email to, then build the email
+    const quoteRequest = createEmailBodyFrom(dealInfo);
+
+    // ...and send it off to each installer
+    installers.forEach(installer => {
+        sendQuoteRequestTo(installer, quoteRequest);
+    });
 
     return json({ message: "Quote request sent" }, { status: 200 })
 }
+
 
 function validate(requestData) {
     const validate = ajv.compile(quoteRequestSchema);
@@ -36,4 +54,29 @@ function validate(requestData) {
         requestErrors = validate.errors.map(error => error.message);
 
     return requestErrors;
+}
+
+
+function removeAlreadyResponded(targetRecipients, dealId) {
+    const unsubscribed = getUnsubscribed();
+    const alreadyQuoted = getAlreadyQuoted(dealId);
+
+    const alreadyResponded = [...unsubscribed, ...alreadyQuoted];
+    const notYetResponded = targetRecipients.filter(email => !alreadyResponded.includes(email));
+
+    return notYetResponded;
+}
+
+function getUnsubscribed() {
+    // todo: call unsubscribe-data api, call quote-data api remove
+}
+
+
+function getAlreadyQuoted(dealId) {
+    // todo: call quote-data api, call quote-data api remove
+}
+
+
+function sendQuoteRequestTo(recipient, emailBody) {
+    // todo: call send-mail api
 }
