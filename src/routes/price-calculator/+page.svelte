b0ccf1aaa3550c7cd8983e09623c182c0ad3998a
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-09 14:56:35 +0100

    Add simple page.svelte

---
+++ src/routes/price-calculator/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { page } from '$app/stores'
+	// import { json } from '@sveltejs/kit';
+    import { onMount } from 'svelte'
+
+    // todo: fetch all price calculation options from the URL
+    const batteryType = $page.url.searchParams.get('batteryType');
+
+    onMount(async () => {
+        // todo: Make fetch requests to load extra data
+
+    });
+</script>
+
+<div>
+battery type: {batteryType}
+</div>
+
+<style>
+</style>
