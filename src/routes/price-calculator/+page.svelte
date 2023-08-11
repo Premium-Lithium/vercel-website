30d9b2375f2d68fd18b0aa9d493dc1eee8ba6bfb
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-11 01:18:51 +0100

    Integrate mini pricing model into new calculator page

--- src/routes/price-calculator/+page.svelte
+++ src/routes/price-calculator/+page.svelte
@@ -1,20 +1,52 @@
 <script>
     import { page } from '$app/stores'
-	// import { json } from '@sveltejs/kit';
-    import { onMount } from 'svelte'
+    import priceOf from './price-model';
 
-    // todo: fetch all price calculation options from the URL
-    const batteryType = $page.url.searchParams.get('batteryType');
+    // todo: add default options here if they're not provided in query params
+    let batterySize_kWh = `${$page.url.searchParams.get('battSize_kWh')}`;
+    let evCharger = `${$page.url.searchParams.get('evCharger')}` == "1";
+    let price = "Calculating...";
 
-    onMount(async () => {
-        // todo: Make fetch requests to load extra data
+    function fetchPrice() {
+        // todo: build complete description of customer solution using form input
+        const customerSolution = {
+            batterySize_kWh,
+            evCharger
+        };
 
-    });
+        price = `$${priceOf(customerSolution)}`;
+    }
+
+    $: fetchPrice();
 </script>
 
 <div>
-battery type: {batteryType}
+    <form on:change={fetchPrice}>
+        <select bind:value={batterySize_kWh}>
+            <option value="5">5 kWh</option>
+            <option value="10">10 kWh</option>
+            <option value="20">20 kWh</option>
+        </select>
+
+        <input type="checkbox" bind:checked={evCharger}> Add EV charger
+    </form>
+
+    <p>Total price: {price}</p>
 </div>
 
 <style>
+    div {
+        font-family: Arial, sans-serif;
+        font-size: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    select {
+        padding: 5px;
+        font-size: inherit;
+    }
 </style>
