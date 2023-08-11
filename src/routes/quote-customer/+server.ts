a1b9a361e90a388345a0dbad254bc07886290610
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-11 14:10:44 +0100

    Load email template from local file

--- src/routes/quote-customer/+server.ts
+++ src/routes/quote-customer/+server.ts
@@ -1,6 +1,14 @@
 import { json } from '@sveltejs/kit';
+import { promises as fs } from 'fs';
+import { join, dirname as pathDirname } from 'path';  // Rename dirname to pathDirname to avoid naming conflicts
+import { fileURLToPath } from 'url';
 import priceOf from '../price-calculator/price-model';
 import pipedrive from 'pipedrive';
+import nunjucks from 'nunjucks';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = pathDirname(__filename);
 
 
 // todo: move this into a separate file, initialise once there and import here
@@ -29,20 +37,34 @@ export async function POST({ request }) {
     const price = priceOf(customerSolution);
 
     // 3. build price-calculator url link with correct query params
+    let params = {
+        batterySize_kWh: customerSolution.batterySize_kWh.toString(),
+        evCharger: customerSolution.evCharger ? "1" : "0",
+    };
 
-    // 4. Load template file
+    // Create an instance of URLSearchParams with the params object
+    let searchParams = new URLSearchParams(params);
+
+    // Construct the full URL
+    let priceCalculatorURL = "https://vercel-website-liart.vercel.app/price-calculator";
+    let priceCalcLink = priceCalculatorURL + '?' + searchParams.toString();
+    console.log(priceCalcLink);
 
     // 5. render template with content, including breakdown of price into components
-    const templateVariables = {
+    const customerData = {
         // todo: include template variables here like BDM name, customer name, install date, custom msg etc
+        customer_name: customerName,
         products: [
             { quantity: 1, name: "10 kWh PowerPod" },
             { quantity: 1, name: "EV Charger" }
         ],
-        totalPrice: price.total
+        total_price: price.total
     };
 
     // 5. send email
+    const emailContent = await loadQuoteEmailWith(customerData);
+
+    sendMail();
 
     return json({}, {status: 200})
 }
@@ -52,4 +74,19 @@ async function getDealInfo(dealId) {
     const dealInfo = await pdApi.getDeal(dealId);
 
     return dealInfo;
+}
+
+
+async function loadQuoteEmailWith(customerData) {
+    const filePath = join(__dirname, 'customer_quote_template.html'); // Replace 'emailTemplate.html' with your actual file name
+    try {
+        const templateString = await fs.readFile(filePath, 'utf8');
+
+        nunjucks.configure({ autoescape: true });
+        const renderedEmail = nunjucks.renderString(templateString, customerData);
+
+        return renderedEmail;
+    } catch (err) {
+        console.error('Error processing the email template:', err);
+    }
 }
