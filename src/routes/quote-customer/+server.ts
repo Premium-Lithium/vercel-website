9af2de40021addd5799031f2119bc6ade24ec545
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-12 22:08:42 +0100

    Read more information from customer's deal data (pl contact, ev charger, new battery size)

--- src/routes/quote-customer/+server.ts
+++ src/routes/quote-customer/+server.ts
@@ -5,6 +5,8 @@ import { fileURLToPath } from 'url';
 import priceOf from '../price-calculator/price-model';
 import pipedrive from 'pipedrive';
 import nunjucks from 'nunjucks';
+import sendMail from '../send-mail/sendMail.js';
+import readCustomDealField from '../../lib/pipedrive-utils.js'
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -22,38 +24,19 @@ export async function POST({ request }) {
 
     // todo: validate request body
 
-    const dealId = data.deal_id; // todo: get this from the request
+    const dealId = data.deal_id;
+    const customer = await getCustomerInfo(dealId);
 
-    const dealInfo = await getDealInfo(dealId);
-    const customerName = dealInfo.data.person_name;
+    if(customer === null)
+        return json({}, {status: 400});
 
-    // 1. todo: build description of customer solution from pipedrive given dealId above
-    const customerSolution = {
-        batterySize_kWh: 10,
-        evCharger: true
-    };
+    const price = priceOf(customer.solution);
+    const priceCalcLink = buildPriceCalcLinkFrom(customer.solution);
 
-    // 2. Use the customer's solution to calculate a price
-    const price = priceOf(customerSolution);
-
-    // 3. build price-calculator url link with correct query params
-    let params = {
-        batterySize_kWh: customerSolution.batterySize_kWh.toString(),
-        evCharger: customerSolution.evCharger ? "1" : "0",
-    };
-
-    // Create an instance of URLSearchParams with the params object
-    let searchParams = new URLSearchParams(params);
-
-    // Construct the full URL
-    let priceCalculatorURL = "https://vercel-website-liart.vercel.app/price-calculator";
-    let priceCalcLink = priceCalculatorURL + '?' + searchParams.toString();
-    console.log(priceCalcLink);
-
-    // 5. render template with content, including breakdown of price into components
     const customerData = {
-        // todo: include template variables here like BDM name, customer name, install date, custom msg etc
-        customer_name: customerName,
+        pl_contact_name: customer.pl_contact.name,
+        price_calculator_link: priceCalcLink,
+        customer_name: customer.name,
         products: [
             { quantity: 1, name: "10 kWh PowerPod" },
             { quantity: 1, name: "EV Charger" }
@@ -64,16 +47,113 @@ export async function POST({ request }) {
     // 5. send email
     const emailContent = await loadQuoteEmailWith(customerData);
 
-    sendMail();
+    sendMail(
+        [ customer.email ],
+        customer.pl_contact.email,
+        "Your quote",
+        emailContent,
+        "HTML"
+    );
 
     return json({}, {status: 200})
 }
 
-async function getDealInfo(dealId) {
-    const pdApi = new pipedrive.DealsApi(pd)
-    const dealInfo = await pdApi.getDeal(dealId);
 
-    return dealInfo;
+async function getCustomerInfo(dealId) {
+    const pdApi = new pipedrive.DealsApi(pd)
+    const request = await pdApi.getDeal(dealId);
+
+    if(request.success === false) {
+        console.log(`Error fetching customer data for deal ${dealId} on pipedrive`);
+        return null;
+    }
+
+    const customerData = request.data;
+
+    const name = customerData.person_name;
+    const email = extractEmailFrom(customerData);
+    const solution = extractSolutionFrom(customerData);
+    const plContactPerson = extractPLContactFrom(customerData);
+
+    const customer = {
+        name: name,
+        email: email,
+        solution: solution,
+        pl_contact: plContactPerson
+    }
+
+    return customer;
+}
+
+
+function buildPriceCalcLinkFrom(customerSolution) {
+    const params = {
+        batterySize_kWh: customerSolution.batterySize_kWh.toString(),
+        evCharger: customerSolution.evCharger ? "1" : "0",
+    };
+
+    const searchParams = new URLSearchParams(params);
+
+    // let priceCalculatorURL = "https://vercel-website-liart.vercel.app/price-calculator";
+    const priceCalculatorURL = "http://localhost:3000/price-calculator";
+
+    const priceCalcLink = priceCalculatorURL + '?' + searchParams.toString();
+
+    return priceCalcLink;
+}
+
+
+function extractEmailFrom(customerData) {
+    const emails = customerData.person_id.email;
+
+    // Try to find a home email first
+    const homeEmail = emails.find(email => email.label === 'home');
+    if(homeEmail !== undefined)
+        return homeEmail.value;
+
+    // Fall back to work email if this isn't found
+    console.log("No home email found, searching for work email...");
+    const workEmail = emails.find(email => email.label === 'work');
+    if(workEmail !== undefined)
+        return workEmail.value;
+
+    // Use any other email that's added, if there is one
+    console.log("No work email found, searching for any other email...");
+    if(emails.length > 0 && emails[0].value !== '') {
+        const firstEmail = emails[0].value;
+        console.log(`Using ${firstEmail}...`);
+        return firstEmail;
+    }
+
+    console.log(`Could not find any email address for ${customerData.person_name}.`);
+    return null;
+}
+
+
+function extractSolutionFrom(customerData) {
+    const solution = {
+        batterySize_kWh: parseInt(readCustomDealField("New Battery size (kWh)", customerData)),
+        evCharger: readCustomDealField("EV Charger?", customerData) == "Yes"
+        // todo: Build a complete description of the solution Premium Lithium will provide
+    };
+
+    return solution;
+}
+
+
+function extractPLContactFrom(customerData) {
+    // todo: get PL contact email from pipedrive
+
+    // Fetch the pipedrive call logs and get the name of the most recent person who called the customer.
+
+    // todo: Could there ever be a case where the deal isn't actually linked to someone from premium lithium?
+
+    const plContact = {
+        name: "Lewis Bowes",
+        email: "lewis.bowes@premiumlithium.com"
+    };
+
+    return plContact;
 }
 
 
@@ -87,6 +167,8 @@ async function loadQuoteEmailWith(customerData) {
 
         return renderedEmail;
     } catch (err) {
-        console.error('Error processing the email template:', err);
+        const message = "Error processing the email template";
+        console.error(message, err);
+        return message;
     }
 }
