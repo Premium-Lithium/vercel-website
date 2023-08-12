8d8aca32ba79f3091bb58d62b163429a648d9b95
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-12 18:58:25 +0100

    Move send-email business logic into separate file

--- src/routes/send-mail/+server.ts
+++ src/routes/send-mail/+server.ts
@@ -2,9 +2,9 @@ import { json } from '@sveltejs/kit';
 import addFormats from "ajv-formats";
 import Ajv from 'ajv';
 import AjvErrors from 'ajv-errors';
-import querystring from 'querystring';
 
 import emailSchema from './schema.js';
+import sendMail from './sendMail.js';
 
 
 const ajv = new Ajv({ allErrors: true });
@@ -22,49 +22,8 @@ export async function POST({ request }) {
         return json({ message: `${message}` }, { status: 400 })
     }
 
-    // Build the email
-    const messagePayload = {
-        message: {
-            subject: requestData.subject,
-            body: {
-                contentType: requestData.content_type,
-                content: requestData.mail_body
-            },
-            toRecipients: requestData.recipients.map(email => ({ emailAddress: { address: email } }))
-        }
-    };
-
-    // Send the email
-    const apiToken = await getNewAPIToken();
-
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiToken
-    };
-
-    const options = {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(messagePayload)
-    };
-
-    const apiUrl = `/v1.0/users/${requestData.sender}/sendMail`;
-
-    fetch(`https://graph.microsoft.com${apiUrl}`, options)
-        .then(res => {
-            if (res.status === 202) {
-                return { statusCode: 202 };
-            } else {
-                console.log(`API request failed with status ${res.status} ${res.statusText}`);
-                return {
-                    statusCode: res.status,
-                    body: res.statusText
-                };
-            }
-        })
-        .catch(error => {
-            return json({ message: `Failed to send email: ${error.message}`}, { status: 500 })
-        });
+    // todo
+    sendMail(...Object.values(requestData));
 
     return json({ message: `Email sent successfully from ${requestData.sender}`}, { status: 200 })
 }
@@ -86,46 +45,4 @@ function validate(requestData) {
         requestErrors.push("Sender must be a Premium Lithium email address. ");
 
     return requestErrors;
-}
-
-
-async function getNewAPIToken() {
-    const mailClientID = process.env.MICROSOFT_CLIENT_ID;
-    const mailClientSecret = process.env.MICROSOFT_CLIENT_SECRET;
-
-    const data = {
-        grant_type: "client_credentials",
-        client_id: mailClientID,
-        client_secret: mailClientSecret,
-        scope: "https://graph.microsoft.com/.default"
-    };
-    const payload = querystring.stringify(data);
-
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
-
-    const apiUrl = `/${process.env.AUTH0_TENANT_ID}/oauth2/v2.0/token`;
-
-    const options = {
-        method: 'POST',
-        headers: headers,
-        body: payload
-    };
-
-    try {
-        const response = await fetch(`https://login.microsoftonline.com${apiUrl}`, options);
-        const data = await response.json();
-
-        if (data.access_token) {
-            const token = data.access_token;
-            return token;
-        } else {
-            console.log('Error:', data.error_description || 'Unknown error');
-            return null;
-        }
-    } catch (error) {
-        console.error(`Fetch error: ${error.message}`);
-        return null;
-    }
 }
