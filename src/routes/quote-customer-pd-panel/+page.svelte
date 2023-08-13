92c6e1a2e2c83512a978a6505b2f32da41371ac7
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-13 22:25:32 +0100

    Install pipedrive/app-extensions-sdk

--- src/routes/quote-customer-pd-panel/+page.svelte
+++ src/routes/quote-customer-pd-panel/+page.svelte
@@ -1,8 +1,21 @@
 <script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores'
+
+    let userId;
+
+    onMount(() => {
+        // Get the code from the url query params
+        userId = `${$page.url.searchParams.get('userId')}`;
+        console.log(`userId: ${userId}`);
+
+        // Use this code to get an auth token by making request to pipedrive
+    });
 </script>
 
 <div>
     custom deal panel here
+    userId: {$userId}
 </div>
 
 <style>
