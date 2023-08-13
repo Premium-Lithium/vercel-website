4a80bd5302ae3fb7ec6816d9cbc7813b9b34dd44
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-13 01:42:55 +0100

    Implement v1 of logic for preorder discount pricing

--- src/routes/price-calculator/price-model.js
+++ src/routes/price-calculator/price-model.js
@@ -1,33 +1,89 @@
 // Calculate all the information that would be included in a customer quote,
 // given their chosen solution
-export default function priceOf(customerSolution) {
+function quoteFor(customerSolution) {
     // todo: validate customer solution object passed in (ajv here or before call?)
 
-    let price = {
-        total: 0
+    // todo: define this as typescript interface?
+    let quote = {
+        price: {
+            total: 0,
+            breakdown: []
+        },
+        discount: {
+            value: 0,
+            multiplier: 0
+        }
     };
 
     // todo: load pricing model parameters from spreadsheet/settings/elsewhere
     switch(customerSolution.batterySize_kWh) {
         case 5:
-            price.total = 2698;
+            quote.price.total = 2698;
             break;
         case 10:
-            price.total = 4498;
+            quote.price.total = 4498;
             break;
         case 20:
-            price.total = 8093;
+            quote.price.total = 8093;
             break;
         default:
-            price.total = 0;
+            quote.price.total = 0;
     }
 
     // todo: complete pricing model
+    if (customerSolution.evCharger.included) {
+        const chargerPrice = getEVChargerPrice(customerSolution.evCharger);
+        quote.price.total += chargerPrice;
 
-    if (customerSolution.evCharger)
-        price.total += 235;
+        // todo: add ev charger to component list
+    }
 
-    // todo: break down the price into its components
+    // Apply pre-order discount
+    const installMonth = new Date(customerSolution.installMonth);
+    const discountMultiplier = calculateDiscountFrom(installMonth);
 
-    return price;
+    quote.discount.multiplier = discountMultiplier;
+    console.log(1.0 - discountMultiplier);
+    quote.discount.value = quote.price.total * discountMultiplier;
+    quote.price.total -= quote.discount.value;
+
+    // todo: break down the price into its components (so these can be tabulated in an email)
+
+    return quote;
 }
+
+
+function getEVChargerPrice(evCharger) {
+    // todo: get ev charger price from pipedrive products api
+    // Use evCharger.type to look up the price
+
+    return 1300; // for now, this is just the price of the 7kW charger
+}
+
+
+function calculateDiscountFrom(installMonth) {
+    const DISCOUNT_PER_MONTH = 0.05;
+
+    const earliest = earliestInstallMonth();
+    const monthsInFuture = monthsBetween(earliest, installMonth);
+
+    const discountMultiplier = Math.min(monthsInFuture, 11) * DISCOUNT_PER_MONTH;
+
+    return discountMultiplier;
+}
+
+
+function earliestInstallMonth() {
+  const now = new Date();
+  const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  return nextMonth;
+}
+
+
+function monthsBetween(date1, date2) {
+  const months = (date2.getFullYear() - date1.getFullYear()) * 12 + (date2.getMonth() - date1.getMonth());
+  return Math.abs(months);
+}
+
+
+export { quoteFor, earliestInstallMonth };
