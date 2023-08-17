2c2d4ef15d3c6af0cc10a69e8a3c0c8245b0e84f
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-17 11:00:22 +0100

    Added endpoint validation

--- src/routes/transcribe/+server.ts
+++ src/routes/transcribe/+server.ts
@@ -1,16 +1,29 @@
 import { OPENAI_API_KEY, OPENAI_ORG_ID } from "$env/static/private";
 import OpenAI from "openai";
 import {json} from '@sveltejs/kit';
+import validate from '$lib/validation-utils.js';
 
 const openai = new OpenAI({
     organization: OPENAI_ORG_ID,
     apiKey: OPENAI_API_KEY,
 });
 
+const allowedFileTypes = ['wav', 'mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'webm'].map((x) => {return `audio/${x}`});
 
 export async function POST({request}) {
-    let form: FormData = await request.formData()
-    let audioFile: File = form.entries().next().value[1];
+    if(!request.body)
+        return json({ message: "No request body found" }, { status: 400 });
+    let form: FormData;
+    try{
+        form = await request.formData()
+    } catch (error) {
+        return json({message: error}, {status: 400})
+    }
+    let content = form.entries().next().value;
+    if(content.length != 2 || content[1].size == 0 || content[0] != 'audio' || !allowedFileTypes.includes(content[1].type)) {
+        return json({message: "Form must contain a valid audio file."}, {status: 400});
+    }
+    let audioFile: File = content[1];
     let res = undefined;
     try{
     res = await openai.audio.transcriptions.create({
