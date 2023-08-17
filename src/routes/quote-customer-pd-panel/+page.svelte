95f5d9a42b8a66a1770f382521a2b0c516a5e61b
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-17 11:12:43 +0100

    Add some styling to quote button

--- src/routes/quote-customer-pd-panel/+page.svelte
+++ src/routes/quote-customer-pd-panel/+page.svelte
@@ -8,13 +8,15 @@
 
     onMount(async () => {
         const dealId = $page.url.searchParams.get('selectedIds');
-        customerName = "todo: load name using dealId";
+        customerName = `${dealId}`;
 
         sdk = await new AppExtensionsSDK().initialize();
     });
 
     async function sendQuoteEmail() {
+        // REMOVE IN PRODUCTION
         const dealId = 6193;
+        // REMOVE IN PRODUCTION
 
         // todo: uncomment this and make sure to gracefully handle case where deal is not found
         // const dealId = $page.url.searchParams.get('selectedIds');
@@ -34,8 +36,28 @@
     <br>
     customer name: {customerName}
     <br>
-    <button on:click={sendQuoteEmail}>Send Quote</button>
+    <button on:click={sendQuoteEmail} class="quote-button"><b>Send Quote</b></button>
 </div>
 
 <style>
+    .quote-button {
+        background-color: #28AAE2;
+        border-color: #a4c1ce;
+        border-width: 1.5px;
+        border-radius: 5px;
+        border-style: solid;
+        color: white;
+        padding: 7px 15px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        transition: background-color 0.2s ease-in-out;
+    }
+
+    .quote-button:hover {
+    background-color: #1c8cb9;
+    }
 </style>
