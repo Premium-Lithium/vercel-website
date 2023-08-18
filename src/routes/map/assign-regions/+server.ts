5747c3aea678e6c5747e78caa99cbce655ba4443
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-18 14:10:42 +0100

    Initial endpoint testing

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -2,5 +2,7 @@ import { json } from "@sveltejs/kit";
 
 export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
-    console.log(request);
+    let data = await request.json();
+    console.log(data);
+    console.log(data.current, data.previous);
 }
