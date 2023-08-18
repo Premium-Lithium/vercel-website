59bc63f9b28b247f063819a26b30ec4ef6e63197
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-18 14:21:44 +0100

    Update endpoint to display status of current and previous state

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -3,6 +3,5 @@ import { json } from "@sveltejs/kit";
 export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let data = await request.json();
-    console.log(data);
-    console.log(data.current, data.previous);
+    console.log(data.current.status, data.previous.status);
 }
