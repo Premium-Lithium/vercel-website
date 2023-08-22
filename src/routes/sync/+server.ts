35a77f98c68f12ab678064745b52cbbe31666d5b
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-22 12:02:55 +0100

    remove unnecessary file causing issues

--- src/routes/sync/+server.ts
+++
@@ -1,8 +0,0 @@
-import { syncAll } from "$lib/pipedrive/syncAll"
-import { json } from '@sveltejs/kit';
-
-export async function POST(response) {
-    await syncAll();
-    return json({ status: 200 });
-}
-
