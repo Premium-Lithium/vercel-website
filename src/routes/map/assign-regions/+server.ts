4527f05c07cba73f9991d24e1320af214e166fbd
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-22 16:14:18 +0100

    test stuff

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -15,8 +15,9 @@ export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let dealInfo = await request.json();
     polygons = loadPolygonsFromDatabase();
+    console.log(dealInfo);
     let relevantDealInfo = await fetchRelevantData(dealInfo, "job");
-    console.log(relevantDealInfo);
+    
     let dealGeographicalPoint = point([relevantDealInfo.latitude, relevantDealInfo.longitude]);
     console.log(pointsInPolygonFromList([dealGeographicalPoint], polygons));
     return json({message: "okay"}, {status: 200});
