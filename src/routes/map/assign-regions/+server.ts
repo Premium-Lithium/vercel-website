722f54b9c71a74f0da58e101f2d395142eaa7a2f
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-22 17:48:51 +0100

    attempted fix

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -8,14 +8,14 @@ import { deserializeCoordinates, serializeCoordinates, fetchLatlonFromPostcodesP
 
 const DB_NAME: string = "installation-manager-regions";
 
-let installationManagerDetails: [{id: Number, name: string}] | [] = [];
-let polygons: [Feature<Polygon, Properties>] | [] = [];
+let installationManagerDetails = [];
+let polygons = [];
 
 export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let dealInfo = await request.json();
     await loadPolygonsFromDatabase();
-    console.log(dealInfo.current);
+    console.log(polygons);
     let latlon = (await fetchLatlonFromPostcodesPostcodes([dealInfo.current['80ebeccb5c4130caa1da17c6304ab63858b912a1_postal_code']]))[0];
     console.log(latlon);
     let dealGeographicalPoint = point([latlon.result.longitude, latlon.result.latitude]);
