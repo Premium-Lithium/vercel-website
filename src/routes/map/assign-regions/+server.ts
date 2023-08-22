1186edd22c422e2eb026379926f23b870c6325bd
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-22 16:45:45 +0100

    test stuff

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -4,7 +4,7 @@ import { featureCollection, point, polygon,
          type Polygon, type Feature, type Properties } from '@turf/helpers';
 import { deserializeCoordinates, serializeCoordinates, fetchLatlonFromPostcodesPostcodes,
          fetchInstallerDataFromPipedrive, fetchJobDataFromPipedrive, pointsInPolygonFromList,
-         fetchRelevantData } from "$lib/mapUtils";
+         fetchRelevantData, fetchLatlonFromPostcodesPostcodes } from "$lib/mapUtils";
 
 const DB_NAME: string = "installation-manager-regions";
 
@@ -15,7 +15,8 @@ export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let dealInfo = await request.json();
     polygons = loadPolygonsFromDatabase();
-    let relevantDealInfo = await fetchRelevantData(dealInfo.current, "job");
+    let latlon = fetchLatlonFromPostcodesPostcodes(dealInfo.current['80ebeccb5c4130caa1da17c6304ab63858b912a1_postal_code']);
+    console.log(latlon);
     let dealGeographicalPoint = point([relevantDealInfo.latitude, relevantDealInfo.longitude]);
     console.log(pointsInPolygonFromList([dealGeographicalPoint], polygons));
     return json({message: "okay"}, {status: 200});
