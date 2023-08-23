e7875efbc3bcde822bdcc7ac5d2f87f8af71e69c
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-23 09:12:25 +0100

    log points found within polygons

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -15,11 +15,11 @@ export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let dealInfo = await request.json();
     await loadPolygonsFromDatabase();
-    console.log(polygons.map((p) => p.geometry.coordinates));
     let latlon = (await fetchLatlonFromPostcodesPostcodes([dealInfo.current['80ebeccb5c4130caa1da17c6304ab63858b912a1_postal_code']]))[0];
     console.log(latlon);
     let dealGeographicalPoint = point([latlon.result.longitude, latlon.result.latitude]);
     pointsInPolygonFromList([dealGeographicalPoint], polygons).forEach((p, i, a) => {
+        console.log(p.features);
         if(p.features.length != 0) {
             console.log(installationManagerDetails[i].name);
         }
