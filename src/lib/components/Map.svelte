6ea1fa16ee5342347eb6bc41c3b77d23f021eb0d
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-23 15:51:22 +0100

    Add searchbar to map

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,24 +1,33 @@
 <svelte:head>
     <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
-    <script id='search.js' defer src='https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
+    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
 </svelte:head>
 
 <script>
 
 import { onMount } from 'svelte';
-
 onMount(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibGV3aXNib3dlcyIsImEiOiJjbGppa2MycW0wMWRnM3Fwam1veTBsYXd1In0.Xji31Ii0B9Y1Sibc-80Y7g';
-
+    const mapboxGlAccessToken = 'pk.eyJ1IjoibGV3aXNib3dlcyIsImEiOiJjbGppa2MycW0wMWRnM3Fwam1veTBsYXd1In0.Xji31Ii0B9Y1Sibc-80Y7g';
+    mapboxgl.accessToken = mapboxGlAccessToken;
     const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
+        style: 'mapbox://styles/mapbox/satellite-streets-v11?optimize=true',
         center: [-3.435973, 55.378051], // longitude and latitude of the center of the UK
         zoom: 5 // zoom level
     });
 
     map.on('load', async () => {
+        map.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxGlAccessToken,
+                mapboxgl: mapboxgl,
+                flyTo: {
+                    speed: 2.5,
+                },
+            })
+        );
     });
 });
 
@@ -28,8 +37,8 @@ onMount(() => {
 
 <style>
     #map{
-        width: 25vw;
-        height: 25vh;
+        width: 60vw;
+        height: 60vh;
     }
     
 </style>
--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -6,29 +6,10 @@
     });
 </script>
 
-<div class="grid">
-    <div class="middle">
-        <Map/>
-    </div>
-    
+<div>
+    <Map/>
 </div>
 
 <style>
-    /* 5 column grid layout, 2 outer padding columns and 3 middle content columns. */
-    .grid {
-        display: grid;
-        width: 100vw;
-        height: 100vh;
-        grid-template-columns: 0.5fr repeat(3, 1fr) 0.5fr;
-        grid-template-rows: repeat(3, 1fr);
-        align-items: center;
-        justify-items: center;
-    }
-
-    .middle {
-        grid-column-start: 4;
-        grid-row-start: 1;
-    }
-
     
 </style>
