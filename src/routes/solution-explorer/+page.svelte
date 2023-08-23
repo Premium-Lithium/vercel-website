0ee53352be4d0eff2e802642c9dfadc8da82bb87
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-23 11:08:34 +0100

    Add page for new `solution-explorer` route

---
+++ src/routes/solution-explorer/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores'
+
+    let someData;
+
+    onMount(async () => {
+    });
+</script>
+
+<div class="visualisation-panel">
+  <div class="canvas-container">
+    <canvas></canvas>
+  </div>
+</div>
+
+<style>
+    .visualisation-panel {
+        width: 50vh;
+        height: 50vh;
+        background-color: white;
+        border-radius: 10px;
+        border: 1px solid lightgrey;
+    }
+
+    .canvas-container {
+        padding: 20px;
+        width: calc(100% - 40px);
+        height: calc(100% - 40px);
+    }
+
+    canvas {
+        background-color: black;
+        width: 100%;
+        height: 100%;
+    }
+</style>
