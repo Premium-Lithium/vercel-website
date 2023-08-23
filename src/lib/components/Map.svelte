9fc551e47462c5fc9535dd5e89dbf61575eb784f
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-23 15:26:36 +0100

    Initial map component

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
+    <script id='search.js' defer src='https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
+</svelte:head>
+
+<script>
+
+import { onMount } from 'svelte';
+
+onMount(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGV3aXNib3dlcyIsImEiOiJjbGppa2MycW0wMWRnM3Fwam1veTBsYXd1In0.Xji31Ii0B9Y1Sibc-80Y7g';
+
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-3.435973, 55.378051], // longitude and latitude of the center of the UK
+        zoom: 5 // zoom level
+    });
+
+    map.on('load', async () => {
+    });
+});
+
+</script>
+
+<div id="map" ></div>
+
+<style>
+    #map{
+        width: 25vw;
+        height: 25vh;
+    }
+    
+</style>
--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -1,13 +1,34 @@
 <script>
     import { onMount } from 'svelte';
+    import Map from '$lib/components/Map.svelte';
 
     onMount(async () => {
     });
 </script>
 
-<div>
-    solution explorer here
+<div class="grid">
+    <div class="middle">
+        <Map/>
+    </div>
+    
 </div>
 
 <style>
+    /* 5 column grid layout, 2 outer padding columns and 3 middle content columns. */
+    .grid {
+        display: grid;
+        width: 100vw;
+        height: 100vh;
+        grid-template-columns: 0.5fr repeat(3, 1fr) 0.5fr;
+        grid-template-rows: repeat(3, 1fr);
+        align-items: center;
+        justify-items: center;
+    }
+
+    .middle {
+        grid-column-start: 4;
+        grid-row-start: 1;
+    }
+
+    
 </style>
