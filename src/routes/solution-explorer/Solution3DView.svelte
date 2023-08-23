fa697aca938f32f31fc7a6827352456b21a46381
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-23 15:33:20 +0100

    Add simple `Solution3DView` component, inside a panel

---
+++ src/routes/solution-explorer/Solution3DView.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Scene from './Scene.svelte'
+    import { Canvas } from '@threlte/core'
+</script>
+
+<div class="visualisation-panel">
+  <div class="canvas-container">
+    <Canvas>
+        <Scene />
+    </Canvas>
+  </div>
+</div>
+
+<style>
+    .visualisation-panel {
+        width: 50vh;
+        height: 50vh;
+        background-color: white;
+        border-radius: 10px;
+        border: 1px solid lightgrey;
+    }
+
+    .canvas-container {
+        padding: 20px;
+        width: calc(100% - 40px);
+        height: calc(100% - 40px);
+    }
+</style>
