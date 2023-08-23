d090ce34dec677b84fee82d9e8e1dd1acb5bf961
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-24 00:29:45 +0100

    Fit scene panel to smaller of width, height

--- src/routes/solution-explorer/Solution3DView.svelte
+++ src/routes/solution-explorer/Solution3DView.svelte
@@ -1,28 +1,44 @@
 <script>
-    import Scene from './Scene.svelte'
-    import { Canvas } from '@threlte/core'
+  import { onMount, afterUpdate } from 'svelte';
+  import Scene from './Scene.svelte'
+  import { Canvas } from '@threlte/core'
+
+  let scenePanel;
+
+  function resizeCanvas() {
+    const size = Math.min(window.innerWidth, window.innerHeight);
+    scenePanel.style.width = `${size}px`;
+    scenePanel.style.height = `${size}px`;
+  }
+
+  onMount(() => {
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+  });
+
+  afterUpdate(resizeCanvas);
 </script>
 
-<div class="visualisation-panel">
+<div class="visualisation-panel" bind:this={scenePanel}>
   <div class="canvas-container">
     <Canvas>
-        <Scene />
+      <Scene />
     </Canvas>
   </div>
 </div>
 
 <style>
-    .visualisation-panel {
-        width: 100vw;
-        height: 100vw;
-        background-color: white;
-        border-radius: 10px;
-        border: 1px solid lightgrey;
-    }
+  .visualisation-panel {
+      width: 100%;
+      height: 100%;
+      background-color: white;
+      border-radius: 10px;
+      border: 1px solid lightgrey;
+  }
 
-    .canvas-container {
-        padding: 20px;
-        width: calc(100% - 40px);
-        height: calc(100% - 40px);
-    }
+  .canvas-container {
+      padding: 20px;
+      width: calc(100% - 40px);
+      height: calc(100% - 40px);
+  }
 </style>
