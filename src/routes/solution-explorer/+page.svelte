aedd33c5ab490195adb3432f2d0892e6c38ce013
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-23 14:10:41 +0100

    Remove 3d stuff

--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -1,35 +1,13 @@
 <script>
     import { onMount } from 'svelte';
-    // import { page } from '$app/stores'
-    import { Canvas } from '@threlte/core'
-    import Scene from './Scene.svelte'
-
-    let someData;
 
     onMount(async () => {
     });
 </script>
 
-<div class="visualisation-panel">
-  <div class="canvas-container">
-    <Canvas>
-        <Scene />
-    </Canvas>
-  </div>
+<div>
+    solution explorer here
 </div>
 
 <style>
-    .visualisation-panel {
-        width: 50vh;
-        height: 50vh;
-        background-color: white;
-        border-radius: 10px;
-        border: 1px solid lightgrey;
-    }
-
-    .canvas-container {
-        padding: 20px;
-        width: calc(100% - 40px);
-        height: calc(100% - 40px);
-    }
 </style>
