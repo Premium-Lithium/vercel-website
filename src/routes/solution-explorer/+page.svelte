b192cbbeb1be47cafeac47fc710bef774afc5dfa
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-08-24 11:59:11 +0100

    done some stuff

--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -1,7 +1,12 @@
 <script>
     import { onMount } from 'svelte';
+    import { page } from "$app/stores";
 
     import ProgressHeader from "./ProgressHeader.svelte"
+    import ComponentProps from './ComponentProps.svelte';
+
+    let urlParams;
+    $: urlParams = $page.url.searchParams.toString();
 
     onMount(async () => {
     });
@@ -14,6 +19,12 @@
     />
     solution explorer here
 </div>
-
+<div>
+    <ComponentProps id="testid"/>
+    <p>
+        {urlParams}
+    </p>
+</div>
+    
 <style>
 </style>
---
+++ src/routes/solution-explorer/ComponentProps.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { page } from "$app/stores";
+
+	
+
+    export let id;
+    let inputContent;
+
+    function inputChanged() {
+        console.log(inputContent);
+        console.log(id);
+        $page.url.searchParams.set(id, inputContent);
+
+        goto('?' + $page.url.searchParams.toString(), {noScroll: true, keepFocus:true});
+    }
+
+</script>
+
+
+<input bind:value={inputContent} on:input={() => inputChanged()}>
