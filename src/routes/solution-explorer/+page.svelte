b62bd00f8f53df0849267006418287eb0d51d203
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-24 17:17:05 +0100

    Add logic for calling PVGIS API

--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -8,8 +8,13 @@
     import Solution3DView from './Solution3DView.svelte'
     import ProgressHeader from "./ProgressHeader.svelte"
 
+    import { queryPV } from './solarAPI';
     const stage = queryParam("stage", ssp.number())
-
+    let peakSolarPower = 8.8;
+    let solarLoss = 14;
+    let output;
+    let latitude = 53.95924825020342;
+    let longitude = -1.0772513524147558;
 </script>
 
 <!-- todo: arrange in new layout and make responsive -->
@@ -21,6 +26,13 @@
     {#if $stage === 0}
         <div class="map-view">
           <Map search={true} style=5/>
+        </div>
+        <div class="solar-api">
+          <label for="peakSolarPower">Peak Solar Power</label>
+          <input type="number" id="peakSolarPower" name="peakSolarPower" bind:value={peakSolarPower}>
+          <label for="solarLoss">Solar Loss</label>
+          <input type="number" id="solarLoss" name="solarLoss" bind:value={solarLoss}>
+          <input type="submit" value="Submit" on:click={async () => {queryPV(latitude, longitude, peakSolarPower, solarLoss)}}>
         </div>
     {:else if $stage === 1}
         <Solution3DView />
@@ -35,5 +47,11 @@
   .map-view {
     width: 100vw;
     height: 25vh;
+  }
+  .solar-api {
+    width: 90vw;
+    left: 5vw;
+    position: relative;
+    margin: 40px;
   }
 </style>
---
+++ src/routes/solution-explorer/solarAPI.js
@@ -0,0 +1,15 @@
+const options = {
+    method: 'GET',
+    headers: {
+      cookie: 'jrc_cookie=!%2Fpq4dgpKzCu4IC5V4sOiVqlGd4BvI1Bdj7Bso9xVClNrOCdwQfqbHd2IrSKGCOdy%2BOXYCvPNW5YhS5g%3D; TS01316d01=01f4fc03dd0d75ac8e9e777318dec4d8c81637e059ee1f6a6bdaa3982cb721d5f75602d88e38d426af2cdea302da4aef95e01017c0',
+      'Content-Type': 'application/json',
+    },
+    
+  };
+
+export async function queryPV(lat, lon, peakPower, loss) {
+    console.log("sending req")
+    let res = await fetch(`https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?lat=${lat}&lon=${lon}&peakpower=${peakPower}&loss=${loss}`, options)
+    res = await res.json();
+    return res;
+}
