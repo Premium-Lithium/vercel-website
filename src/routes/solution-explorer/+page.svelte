40702313302bf0bd551db3e37b10ccafab432a85
Evan Snoswell <evan.snoswell@gmail.com>
2023-08-24 15:39:25 +0000

    first question

--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -7,8 +7,14 @@
   
     import Solution3DView from './Solution3DView.svelte'
     import ProgressHeader from "./ProgressHeader.svelte"
+    import EnergyStage from "./EnergyStage.svelte"
 
     const stage = queryParam("stage", ssp.number())
+
+    const battery = queryParam("battery", ssp.boolean())
+    const solar = queryParam("solar", ssp.boolean())
+    const ev = queryParam("ev", ssp.boolean())
+    const epsups = queryParam("epsups", ssp.boolean())
 
 </script>
 
@@ -19,6 +25,12 @@
         selectedIndex={$stage}
     />
     {#if $stage === 0}
+        <EnergyStage
+            bind:battery={$battery}
+            bind:solar={$solar}
+            bind:ev={$ev}
+            bind:epsups={$epsups}
+        />
     {:else if $stage === 1}
         <div class="map-view">
           <Map search={true} style=5/>
--- src/routes/solution-explorer/EnergyStage.svelte
+++ src/routes/solution-explorer/EnergyStage.svelte
@@ -1,7 +1,60 @@
 <script>
+    export let battery
+    export let solar
+    export let ev
+    export let epsups
+
+    function notSure() {
+        battery = false
+        solar = false
+        ev = false
+        epsups = false
+    }
 </script>
 
-<div>
-    The energy stage
-</div>
+<table>
+    <tr>
+        <td>
+            <label for="battery" >Battery</label>
+        </td>
+        <td>
+            <input type="checkbox" bind:checked={battery}>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="solar">Solar</label>
+        </td>
+        <td>
+            <input type="checkbox" bind:checked={solar}>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="ev">Electric Vehicle Charger</label>
+        </td>
+        <td>
+            <input type="checkbox" bind:checked={ev}>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="epsups">EPS / UPS</label>
+        </td>
+        <td>
+            <input type="checkbox" bind:checked={epsups}>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label for="notsure">Not sure</label>
+        </td>
+        <td>
+            <input type="checkbox" on:change={notSure} checked={!(battery || solar || ev || epsups)}>
+        </td>
+    </tr>
+</table>
 
+<style>
+    table td:nth-child(1) { text-align: end; }
+</style>
