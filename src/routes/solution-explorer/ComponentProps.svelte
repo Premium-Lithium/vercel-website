20a7a4adea0daa4ed233736052b6270419304b5a
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-08-24 17:43:09 +0100

    test

--- src/routes/solution-explorer/ComponentProps.svelte
+++ src/routes/solution-explorer/ComponentProps.svelte
@@ -6,6 +6,11 @@
 	
     // use component ID as value in store
     export let id;
+    export let type;  // component type
+    export let text = "";
+    export const defaultValue = "";
+
+    console.log(type);
     const componentParam = queryParam(id);
     
     // navigate to new url
@@ -13,7 +18,43 @@
         goto('?' + $page.url.searchParams.toString(), {noScroll: true, keepFocus:true});
     }
 
+    function buttonClicked() {
+        if ($componentParam == "true") {
+            $componentParam = "false";
+        } else if ($componentParam = "false") {
+            $componentParam = "true"
+        } else {
+            console.log("Button not initialised, default to false");
+            $componentParam = "false";
+        }
+    }
+
 </script>
 
+{#if type == "number"}
+    <input {id} type="number" bind:value={$componentParam} on:input={() => inputChanged()}>
+{:else if type == "toggle"}
+    <script>
+        // default value
+        defaultValue ? $componentParam = defaultValue: $componentParam = "false" ;
+        console.log($componentParam);
+    </script>
+    <button type="button"  class={$componentParam} on:click={() => buttonClicked()}>
+        {text}
+    </button>
+{/if}
+<style>
+    button[type="button"] {
+        background-color: white;
+        color: black   
+    }
+    button.true[type="button"] {
+        background-color: black;
+        color: white
+    }
 
-<input bind:value={$componentParam} on:input={() => inputChanged()}>
+    button.false[type="button"] {
+        background-color: white;
+        color: black         
+    }
+</style>
