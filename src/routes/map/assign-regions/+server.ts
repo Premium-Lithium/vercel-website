5bbadeed083effcdb93dda4ad728d0056cb0de9f
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-25 15:04:40 +0100

    FINALLY, now we set up the webhook

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -12,6 +12,9 @@ let polygons;
 export async function POST ({request}){
     if(!request.body) return json({message: "Request needs a body"}, {status: 400});
     let dealInfo = await request.json();
+    if(!(dealInfo.previous['status'] == "open" && dealInfo.current['status'] == 'won')) {
+        return json({message: "Unchanged deal"}, {status: 200});
+    }
     await loadPolygonsFromDatabase();
     let latlon = (await fetchLatlonFromPostcodesPostcodes([dealInfo.current['80ebeccb5c4130caa1da17c6304ab63858b912a1_postal_code']]))[0];
     console.log(latlon);
