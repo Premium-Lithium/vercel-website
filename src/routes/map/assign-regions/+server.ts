0c35b3168384b3bb21086620c3c7f80874ac6110
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-25 14:56:07 +0100

    update query params in put

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -20,7 +20,7 @@ export async function POST ({request}){
     let polygonPointIsIn = pointInPolygonFromList(dealGeographicalPoint, polygons)
     if(polygonPointIsIn) {
         console.log(installationManagerDetails[polygonPointIsIn].name);
-        syncJobOwnersToPipedrive(dealInfo.meta.id, installationManagerDetails[polygonPointIsIn].id);
+        await syncJobOwnersToPipedrive(dealInfo.meta.id, installationManagerDetails[polygonPointIsIn].id);
     }
     return json({message: "okay"}, {status: 200});
 }
@@ -49,7 +49,7 @@ const loadPolygonsFromDatabase = async () => {
 async function syncJobOwnersToPipedrive(dealId: Number, installerManagerUserID: Number) {
     // This needs to be ran at time of close, so possibly set up an endpoint with a webhook for this.
     // on deal.update.. if deal.status open -> won.. update owner in pipedrive. 
-    let res = await fetch(`https://api.pipedrive.com/api/v1/deals/${dealId}?api_token=77a5356773f422eb97c617fd7c37ee526da11851`, {
+    let res = await fetch(`https://api.pipedrive.com/api/v1/deals/?${dealId}&api_token=77a5356773f422eb97c617fd7c37ee526da11851`, {
         method: 'PUT',
         body: JSON.stringify({'user_id': installerManagerUserID}),
         headers: {
