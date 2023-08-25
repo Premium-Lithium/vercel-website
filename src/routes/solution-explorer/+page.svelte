9ae2ea3e9a9b48c2d834eb2c240ccf44d7e614b6
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-25 09:09:58 +0100

    Go back to https

--- src/routes/solution-explorer/+page.svelte
+++ src/routes/solution-explorer/+page.svelte
@@ -12,9 +12,9 @@
     const stage = queryParam("stage", ssp.number())
     let peakSolarPower = 8.8;
     let solarLoss = 14;
-    let output;
     let latitude = 53.95924825020342;
     let longitude = -1.0772513524147558;
+    let output;
 </script>
 
 <!-- todo: arrange in new layout and make responsive -->
--- src/routes/solution-explorer/solarAPI.js
+++ src/routes/solution-explorer/solarAPI.js
@@ -7,7 +7,7 @@ const options = {
 
 export async function queryPV(lat, lon, peakPower, loss) {
     console.log("sending req")
-    let res = fetch('http://re.jrc.ec.europa.eu/api/v5_2/PVcalc?lat=53.95924825020342&lon=-1.0772513524147558&peakpower=8.8&loss=14', options)
+    let res = fetch('https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?lat=53.95924825020342&lon=-1.0772513524147558&peakpower=8.8&loss=14', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
