fc67227c04904d379ce571b5cdbd77a4f374dea8
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-08-25 14:58:01 +0100

    update url again WHY DOESN'T THIS WORK

--- src/routes/map/assign-regions/+server.ts
+++ src/routes/map/assign-regions/+server.ts
@@ -49,7 +49,7 @@ const loadPolygonsFromDatabase = async () => {
 async function syncJobOwnersToPipedrive(dealId: Number, installerManagerUserID: Number) {
     // This needs to be ran at time of close, so possibly set up an endpoint with a webhook for this.
     // on deal.update.. if deal.status open -> won.. update owner in pipedrive. 
-    let res = await fetch(`https://api.pipedrive.com/api/v1/deals/?${dealId}&api_token=77a5356773f422eb97c617fd7c37ee526da11851`, {
+    let res = await fetch(`https://api.pipedrive.com/api/v1/deals/${dealId}?api_token=77a5356773f422eb97c617fd7c37ee526da11851`, {
         method: 'PUT',
         body: JSON.stringify({'user_id': installerManagerUserID}),
         headers: {
