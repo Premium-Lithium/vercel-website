a8d1f562fc6513391b9725c9bcdf1f0a73439b83
Peter Gillingham <peter.gillingham@premiumlithium.com>
2023-08-30 17:26:34 +0100

    Began implementation of JSBuySDK for accessing store, product list, checkout & checkout ids, cart Added button object based on next/back buttons but for purchasing - not connected to anything yet.

---
+++ src/lib/components/PurchaseButton.svelte
@@ -0,0 +1,35 @@
+
+<script>
+    function makePurchase() {
+        /*
+            Todo after obtaining product info from store
+             - add to cart
+             - 
+        */
+    }
+</script>
+
+<div class=body>
+    <button type="button" on:click={makePurchase}>
+        Purchase 
+    </button>
+</div>
+
+
+<style>
+	button{
+        background-color: var(--plblue);
+        color: white;
+        border: none; 
+        font-size: 20px;
+        height:auto; 
+        width:100px; 
+        padding:10px 5px; 
+        margin:30px; 
+        border-radius:5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+</style>
--- src/routes/solution-explorer/InvestmentStage.svelte
+++ src/routes/solution-explorer/InvestmentStage.svelte
@@ -1,54 +1,59 @@
 <script lang="ts">
+	//Test admin key: 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
 	import '@shopify/shopify-api/adapters/web-api';
 	import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
+	import Client from 'shopify-buy';
 
+	// Button
+	import PurchaseButton from '$lib/components/PurchaseButton.svelte';
+
+	//Todo - don't have details hardcoded
 	const shopify_apiKey = '3c53bc6cca0f0c0253b4853e0dafce90';
-	const shopify_secretApiKey = '5ed4c35759414e6f6b0c835d6f8944bc';
-	const shopify_scope = ['read_products'];
-	const shopify_hostname = 'localhost:3000';
-	//let shopify_shop;
+	const shopify_domain = 'premium-lithium-limited.myshopify.com';
 
-	// Shopify API
-	/*
-        Test admin key: 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
-        Todo - don't have details hardcoded
-             - OAuth process
-    */
-	const shopify = shopifyApi({
-		apiKey: shopify_apiKey, // key from partners dashboard, string
-		apiSecretKey: shopify_secretApiKey, // secret key from partners dashboard, string
-		scopes: shopify_scope, // products on shopify app, array
-		hostName: shopify_hostname, //just for testing, might be premiumlithium.com, string
-		apiVersion: LATEST_API_VERSION, // imported from @shopify api
-		isEmbeddedApp: true //shopify admin, bool
+	let current_checkout;
+
+	// Build client - link this instance to the target store
+    const client = Client.buildClient({
+		domain: shopify_domain,
+		storefrontAccessToken: shopify_apiKey
 	});
 
-	/*
-        Todo get this working for this project - currently just an example 
-            Look into Request Docs
-            Look into Response Docs
-    */
-	async function handleFetch(request: Request): Promise<Response> {
-		const { searchParams } = new URL(request.url);
-		return shopify.auth.begin({
-			shopify_shop: shopify.utils.sanitizeShop(searchParams.get(shopify_hostname), true),
-			callbackPath: '/auth/callback',
-			isOnline: false,
-			rawRequest: request
+	// Retrieve shop and product info
+	const shopInfo = client.shop.fetchInfo();
+	const products = client.product.fetchAll()
+
+	// Fetch the checkout or create a new one if not defined
+	if (current_checkout) {
+		client.checkout.fetch(current_checkout.id).then((checkout) => {
+			current_checkout = checkout
 		});
+	} else {
+		createCheckout() 
 	}
 
-	// function detectScopeChange() {
-	// 	if (!shopify.config.scopes.equals(session.scope)) {
-	// 		// Scopes have changed, the app should redirect the merchant to OAuth
-	// 	}
-	// }
+	// Create a checkout
+	function createCheckout() {
+		client.checkout.create().then((checkout) => {
+      	current_checkout = checkout;
+    	});
+	}
+
+	function findProduct(productName) {
+		return products.find(productName).id;
+	}
+
+	function addItemsToCart(productName, current_checkout) {
+		const itemsToAdd {
+			variantID: findProduct(productName),
+			quantity: 1, //only 1 purchase at a time as product is a deposit
+		};
+		client.checkout.addLineItems(current_checkout.id, itemsToAdd);
+	}
+
 </script>
 
 <!-- Button todo export onclick signal at some point-->
-<div>
-	<label for="purchaseBtn" />
-	<input type="button" value="Purchase" id="purchaseBtn" />
-</div>
-
-<h2>Purchase Order</h2>
+<body>
+	<PurchaseButton />
+</body>
