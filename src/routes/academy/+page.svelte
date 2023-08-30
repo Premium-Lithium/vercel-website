c4a23ac6cbe0c19b3cd367b1437910fd39a66b80
lbowes <33807016+lbowes@users.noreply.github.com>
2023-08-30 15:17:51 +0100

    Add new form html

--- src/routes/academy/+page.svelte
+++ src/routes/academy/+page.svelte
@@ -1,32 +1,164 @@
 <div class="body">
-    <img class="logo" src="https://premiumlithium.com/cdn/shop/files/Website_Logo_PNG_8c3726b3-6ebd-489e-9a38-06885f16236b.png?v=1653833196&width=500">
-    <h1>Become an instalation partner today</h1>
-    <h2> Please complete the form below and one of the team will be in touch to discuss this great opportunity: </h2>
-    <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/c52ZWEwVqlYePQNnG2tLjaykDOToJpX6swh3EoqHQMLiPud4OqfqVTM39prRN2pLIn">
+	<img
+		class="logo"
+		src="https://premiumlithium.com/cdn/shop/files/Website_Logo_PNG_8c3726b3-6ebd-489e-9a38-06885f16236b.png?v=1653833196&width=500"
+	/>
+	<h1>Become an instalation partner today</h1>
+	<h2>
+		Please complete the form below and one of the team will be in touch to discuss this great
+		opportunity:
+	</h2>
+
+	<form action="#" name="Academy Registration">
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_label"
+				class="sc-dAlyuH dWziHy"><span>Name</span></label
+			>
+			<div>
+				<input
+					name="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					type="text"
+					id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					aria-invalid="false"
+					aria-required="true"
+					aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_error V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMS0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_helpText"
+					class="sc-aXZVg jXABNp"
+					value=""
+                    autocomplete="one-time-code"
+				/>
+			</div>
+		</div>
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_label"
+				class="sc-dAlyuH dWziHy"><span>Email</span></label
+			>
+			<div>
+				<input
+					name="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					type="email"
+					id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					aria-invalid="false"
+					aria-required="true"
+					aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_error V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMi0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_helpText"
+					class="sc-aXZVg jXABNp"
+					value=""
+                    autocomplete="one-time-code"
+				/>
+			</div>
+		</div>
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_label"
+				class="sc-dAlyuH dWziHy"><span>Phone</span></label
+			>
+			<div>
+				<input
+					name="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					type="tel"
+					id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					aria-invalid="false"
+					aria-required="true"
+					aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_error V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjMy0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_helpText"
+					class="sc-aXZVg jXABNp"
+					value=""
+                    autocomplete="one-time-code"
+				/>
+			</div>
+		</div>
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I_label"
+				class="sc-dAlyuH dWziHy"><span>Academy Name <br /></span></label
+			>
+			<div>
+				<input
+					name="V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I"
+					type="text"
+					id="V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I"
+					aria-invalid="false"
+					aria-required="true"
+					aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I_error V2ViRm9ybUNhcHR1cmVCbG9jazo1YTFiNDhlMC0zYWEwLTExZWUtODUwZC05NTE1ZWRlMDQ4N2I_helpText"
+					class="sc-aXZVg jXABNp"
+					value=""
+                    autocomplete="one-time-code"
+				/>
+			</div>
+		</div>
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_label"
+				class="sc-dAlyuH dWziHy"><span>Company Name<br /></span></label
+			>
+			<div>
+				<input
+					name="V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					type="text"
+					id="V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+					aria-invalid="false"
+					aria-required="true"
+					aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_error V2ViRm9ybUNhcHR1cmVCbG9jazoxYTEzNWFjMC0zMTFlLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_helpText"
+					class="sc-aXZVg jXABNp"
+					value=""
+                    autocomplete="one-time-code"
+				/>
+			</div>
+		</div>
+		<div class="sc-jlZhew dHgxMY">
+			<label
+				for="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+				id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_label"
+				class="sc-dAlyuH dWziHy"><span>Company Address<br /></span></label
+			>
+			<div>
+				<div class="sc-dCFHLb bnzPIg">
+					<input
+						name="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+						id="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM"
+						aria-invalid="false"
+						aria-required="true"
+						aria-describedby="V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_error V2ViRm9ybUNhcHR1cmVCbG9jazpiMDE2ZTVjNC0zMTFjLTExZWUtODAxNC0xNzlmNDMzYmQ5MTM_helpText"
+						aria-haspopup="listbox"
+						aria-expanded="false"
+						class="sc-aXZVg bjsJSY"
+						value=""
+						autocomplete="one-time-code"
+					/>
+				</div>
+			</div>
+		</div>
+		<button type="submit" class="sc-cwHptR gTJoxE">Submit</button>
+	</form>
+
+	<!-- <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/c52ZWEwVqlYePQNnG2tLjaykDOToJpX6swh3EoqHQMLiPud4OqfqVTM39prRN2pLIn">
         <script src="https://webforms.pipedrive.com/f/loader"></script>
-    </div>
-    <script src="webform.js"></script>
+    </div> -->
+	<!-- <script src="webform.js"></script> -->
 </div>
 
 <style>
-    .body {
-        background-color: black;
-        color: white;
-        margin: auto;
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-    }
+	.body {
+		background-color: black;
+		color: white;
+		margin: auto;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+	}
 
-    .pipedriveWebForms {
-        display: flex;
-        flex-direction: column;
-        align-items: center; 
-        text-align: center;
-    }
-
-
+	.pipedriveWebForms {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+	}
 </style>
