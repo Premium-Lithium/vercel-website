475c52334d2f54ad2b88d60f5291cba632582503
Peter Gillingham <peter.gillingham@premiumlithium.com>
2023-08-31 16:20:38 +0100

    WIP of OAuth process implementation, Shopify session initialised.

--- src/routes/solution-explorer/PurchaseDeposit.svelte
+++ src/routes/solution-explorer/PurchaseDeposit.svelte
@@ -1,55 +1,67 @@
 <script lang="ts">
-	//Test admin key: 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
-	import '@shopify/shopify-api/adapters/web-api';
-	import fetch from 'node-fetch';
-	import Client from 'shopify-buy';
+	import '@shopify/shopify-api/adapters/node';
+	import { shopifyApi, ApiVersion } from '@shopify/shopify-api';
+	import { restResources } from '@shopify/shopify-api/rest/admin/2023-04';
+	import express from 'express';
 
 	// Button
 	import PurchaseButton from '$lib/components/PurchaseButton.svelte';
 
-	//Todo - don't have details hardcoded
 	const shopify_apiKey = '3c53bc6cca0f0c0253b4853e0dafce90';
+	const shopify_apiSecretKey = '3c53bc6cca0f0c0253b4853e0dafce90';
+	const shopify_adminAccessToken = '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';
 	const shopify_domain = 'premium-lithium-limited.myshopify.com';
 
-	let current_checkout;
-
-	// Build client - link this instance to the target store
-    const client = Client.buildClient({
-		storefrontAccessToken: shopify_apiKey
-		domain: shopify_domain,
+	/**
+	 * Shopify initialisation
+	 *	- connects to the custom app (deposit-test) on shopify
+	 *	-
+	 */
+	const shopify = shopifyApi({
+		apiSecretKey: shopify_apiSecretKey,
+		apiVersion: ApiVersion.July23,
+		isCustomStoreApp: true,
+		adminApiAccessToken: shopify_adminAccessToken,
+		isEmbeddedApp: true,
+		hostName: shopify_domain,
+		scopes: ['read_inventory', 'read_product_listings', 'read_products', 'read_purchase_options'],
+		restResources
 	});
 
-	// Retrieve shop and product info
-	const shopInfo = client.shop.fetchInfo();
-	const products = client.product.fetchAll()
+	/**
+	 * Begins the shopify session
+	 */ 
+	const session = shopify.session.customAppSession(shopify_domain);
 
-	// Fetch the checkout or create a new one if not defined
-	if (current_checkout) {
-		client.checkout.fetch(current_checkout.id).then((checkout) => {
-			current_checkout = checkout
+	/**
+	 * OAuth process - begin
+	 * 	- if the sessions scope is not the same as the shopify scope then redo the auth process
+	 * 	- 
+	 */
+	async function authProcessBegin(req, res) {
+		if (!shopify.config.scopes.equals(session.scope)) {
+			let shopVar = shopify.utils.sanitizeShop(session.shop, true) !== null ? String(shopify.utils.sanitizeShop(session.shop, true)) : '';
+			await shopify.auth.begin({
+				shop: shopVar,
+				callbackPath: '',
+				isOnline: false, // only comes here if the scope has changed so will be offline
+				rawRequest: req,
+				rawResponse: res
+			})
+		}
+	}
+
+	/**
+	 * OAuth process - callback
+	 * 	- after the app installation is approved in auth.begin
+	*/
+	async function authProcessCallback(req, res) {
+		const callback = await shopify.auth.callback({
+			rawRequest: req,
+			rawResponse: res,
 		});
-	} else {
-		createCheckout() 
 	}
 
-	// Create a checkout
-	function createCheckout() {
-		client.checkout.create().then((checkout) => {
-      	current_checkout = checkout;
-    	});
-	}
-
-	function findProduct(productName) {
-		return products.find(productName).id;
-	}
-
-	function addItemsToCart(productName, current_checkout) {
-		const itemsToAdd {
-			variantID: findProduct(productName),
-			quantity: 1, //only 1 purchase at a time as product is a deposit
-		};
-		client.checkout.addLineItems(current_checkout.id, itemsToAdd);
-	}
 
 </script>
 
