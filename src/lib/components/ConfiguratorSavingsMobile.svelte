9250d7910d0db5e5008c93c84de44fc521dcfe3e
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-08-31 16:33:50 +0100

    start

---
+++ src/lib/components/ConfiguratorSavingsMobile.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div class=body>
+
+</div>
+
+<style>
+    .body {
+        background-color: #6d6c6c;
+        width: 100%;
+        height: 2em;
+    }
+
+</style>
