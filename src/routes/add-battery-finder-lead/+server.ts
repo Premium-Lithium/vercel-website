7604b8e6dba527f0bf7a2e30c963e7805dbc9ab2
lbowes <33807016+lbowes@users.noreply.github.com>
2023-09-04 17:48:27 +0100

    Copy  route files into new

---
+++ src/routes/add-battery-finder-lead/+server.ts
@@ -0,0 +1,48 @@
+import { json } from '@sveltejs/kit';
+import validate from '../../lib/validation-utils.js';
+import { createPipedriveLeadFrom } from './logic.js';
+
+
+// TODO - CONVERT THIS TO THE NEW LOGIC FOR ADDING DEALS
+
+
+const schema = {
+    type: "object",
+    required: [ "recipients", "sender", "subject", "mail_body", "content_type" ],
+    properties: {
+        recipients: {
+            type: "array",
+            items: { "type": "string", "format": "email" },
+            errorMessage: "Recipients should be an array of valid email addresses"
+        },
+        sender: { "type": "string", "format": "email", "errorMessage": "Invalid sender email format" },
+        subject: { "type": "string", "errorMessage": "Subject should be a string" },
+        body: { "type": "string", "errorMessage": "mail_body should be a string" },
+        content_type: {
+            type: "string",
+            enum: [ "HTML", "TEXT" ],
+            errorMessage: "Content type should be either 'HTML' or 'TEXT'"
+        }
+    }
+}
+
+export async function POST({ request }) {
+    if(!request.body)
+        return json({ message: "No request body found" }, { status: 400 });
+
+    const requestData = await request.json();
+    const validationErrors = validate(requestData, schema);
+
+    // Check that the request body obeys the schema
+    if(validationErrors.length) {
+        const message = validationErrors.join(", ");
+        return json({ message: `${message}` }, { status: 400 })
+    }
+
+    const mailAttempt = await sendMail(...Object.values(requestData));
+
+    return json(
+        { message: mailAttempt.message },
+        { status: mailAttempt.success ? 200 : 500 }
+    );
+}
---
+++ src/routes/add-battery-finder-lead/logic.js
@@ -0,0 +1,10 @@
+// todo: import pipedrive stuff here
+
+
+function createPipedriveLeadFrom(batteryFinderData) {
+    // todo: use pipedrive api to add a new lead if the user provided their phone number
+
+}
+
+
+export { createPipedriveLeadFrom };
