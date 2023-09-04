9b68f1ea4c2bc7e259f443f736fe9abcd76b224e
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-04 10:19:29 +0100

    installation date picker

---
+++ src/routes/solution-explorer/InstallationDate.svelte
@@ -0,0 +1,18 @@
+<script>
+    let earliestInstall = earliestInstallMonth();
+    export let installationDate;
+
+    function earliestInstallMonth() {
+        const now = new Date();
+        const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 2);
+        return nextMonth;
+    }
+</script>
+
+<body>
+    <h2>When Do You Want This Installed?</h2>
+    <label for="date">Installation Date:</label>
+    <input type="date" id="date" name="date" bind:value={installationDate} min={earliestInstall.toISOString().slice(0, 10)}>
+</body>
+<style>
+</style>
