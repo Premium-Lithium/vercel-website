ad2def17d5175fe769f5e84bacfa81506a18639d
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-09-05 10:08:19 +0100

    get rid of jquery - unneeded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,3 @@
-<svelte:head>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-</svelte:head>
 <slot />
 
 <style>
