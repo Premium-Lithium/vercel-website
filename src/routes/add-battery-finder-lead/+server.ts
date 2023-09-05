7a1f5443fe57cffc49dde327db21566f72997c4c
lbowes <33807016+lbowes@users.noreply.github.com>
2023-09-06 00:34:13 +0100

    Remove request body validation (complex data from shopify), parse data

--- src/routes/add-battery-finder-lead/+server.ts
+++ src/routes/add-battery-finder-lead/+server.ts
@@ -1,48 +1,20 @@
-import { json } from '@sveltejs/kit';
-import validate from '../../lib/validation-utils.js';
 import { createPipedriveLeadFrom } from './logic.js';
 
 
-// TODO - CONVERT THIS TO THE NEW LOGIC FOR ADDING DEALS
-
-
-const schema = {
-    type: "object",
-    required: [ "recipients", "sender", "subject", "mail_body", "content_type" ],
-    properties: {
-        recipients: {
-            type: "array",
-            items: { "type": "string", "format": "email" },
-            errorMessage: "Recipients should be an array of valid email addresses"
-        },
-        sender: { "type": "string", "format": "email", "errorMessage": "Invalid sender email format" },
-        subject: { "type": "string", "errorMessage": "Subject should be a string" },
-        body: { "type": "string", "errorMessage": "mail_body should be a string" },
-        content_type: {
-            type: "string",
-            enum: [ "HTML", "TEXT" ],
-            errorMessage: "Content type should be either 'HTML' or 'TEXT'"
-        }
-    }
-}
-
 export async function POST({ request }) {
-    if(!request.body)
-        return json({ message: "No request body found" }, { status: 400 });
+    // if(!request.body)
+    //     return json({ message: "No request body found" }, { status: 400 });
 
-    const requestData = await request.json();
-    const validationErrors = validate(requestData, schema);
+    const rawData = await request.text();
+    const answers = JSON.parse(rawData).answers;
 
-    // Check that the request body obeys the schema
-    if(validationErrors.length) {
-        const message = validationErrors.join(", ");
-        return json({ message: `${message}` }, { status: 400 })
-    }
+    // todo: schema validation?
+    const leadAddAttempt = await createPipedriveLeadFrom(answers);
 
-    const mailAttempt = await sendMail(...Object.values(requestData));
-
-    return json(
-        { message: mailAttempt.message },
-        { status: mailAttempt.success ? 200 : 500 }
+    const response = new Response(
+        JSON.stringify({ message: leadAddAttempt.message }),
+        { status: leadAddAttempt.success ? 200 : 500 }
     );
+
+    return response;
 }
