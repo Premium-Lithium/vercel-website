00eda65854b10c0682d4ec3a85d5d53f76b7da93
lbowes <33807016+lbowes@users.noreply.github.com>
2023-09-06 01:52:48 +0100

    Remove comments

--- src/routes/add-battery-finder-lead/+server.ts
+++ src/routes/add-battery-finder-lead/+server.ts
@@ -2,13 +2,9 @@ import { createPipedriveLeadFrom } from './logic.js';
 
 
 export async function POST({ request }) {
-    // if(!request.body)
-    //     return json({ message: "No request body found" }, { status: 400 });
-
     const rawData = await request.text();
     const answers = JSON.parse(rawData).answers;
 
-    // todo: schema validation?
     const leadAddAttempt = await createPipedriveLeadFrom(answers);
 
     const response = new Response(
