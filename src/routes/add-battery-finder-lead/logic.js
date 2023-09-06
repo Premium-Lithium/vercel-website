b075e5b2e053e67976f184401fc084787e51845f
lbowes <33807016+lbowes@users.noreply.github.com>
2023-09-06 01:52:36 +0100

    Create new person and new lead on pipedrive

--- src/routes/add-battery-finder-lead/logic.js
+++ src/routes/add-battery-finder-lead/logic.js
@@ -1,6 +1,8 @@
-// todo: import pipedrive stuff here
+import pipedrive from 'pipedrive';
+import { pd } from '../../lib/pipedrive-utils.js'
 
-const question = {
+
+const questions = {
     BUILDING_TYPE: 'are you looking for a battery for your...',
     ENERGY_USAGE: 'how many kwh',
     EMAIL_ADDRESS: 'enter your email address',
@@ -16,7 +18,7 @@ async function createPipedriveLeadFrom(batteryFinderAnswers) {
         "message": `Added new lead to pipedrive.`
     };
 
-    let getDataFor = (question) => {
+    let getAnswerTo = (question) => {
         const answer = batteryFinderAnswers.find(answer => answer.question_title.toLowerCase().includes(question));
 
         if(answer === undefined)
@@ -30,7 +32,7 @@ async function createPipedriveLeadFrom(batteryFinderAnswers) {
     };
 
     // We only want to add users as leads if they provided their phone number
-    const phoneNumber = getDataFor(question.PHONE_NUMBER);
+    const phoneNumber = getAnswerTo(questions.PHONE_NUMBER);
     console.log(`Phone number: ${phoneNumber}`);
 
     if (phoneNumber === undefined) {
@@ -38,12 +40,67 @@ async function createPipedriveLeadFrom(batteryFinderAnswers) {
         return result;
     }
 
-    const emailAddress = getDataFor(question.EMAIL_ADDRESS);
-    const energyUsage = getDataFor(question.ENERGY_USAGE);
-    const fullName = getDataFor(question.NAME);
-    const source = getDataFor(question.SOURCE);
+    let answers = Object.fromEntries(
+        Object.entries(questions).map(
+            ([name, q]) => [name, getAnswerTo(q)]
+        )
+    );
+
+    try {
+        const name = answers.NAME;
+        const personId = await addPerson(answers.EMAIL_ADDRESS, name, answers.PHONE_NUMBER);
+        await addLead(answers.SOURCE, answers.ENERGY_USAGE, answers.BUILDING_TYPE, name, personId);
+    }
+    catch(error) {
+        const msg = `Error adding lead: ${error}`;
+        result.success = false;
+        result.message = msg;
+        console.log(msg);
+    }
 
     return result;
+}
+
+
+async function addPerson(emailAddress, name, phone) {
+    console.log(`Adding person ${name} with email ${emailAddress} and phone ${phone} to pipedrive...`);
+
+    const persons = new pipedrive.PersonsApi(pd);
+
+    let person = null;
+
+    try {
+        person = await persons.addPerson({
+            name: name,
+            email: emailAddress,
+            phone: phone,
+            // temp
+            ownerId: 15215441 // Lewis
+        });
+    }
+    catch(error) {
+        console.log(`Error adding person: ${error}`);
+    }
+
+    return person.data.id;
+}
+
+
+async function addLead(source, energyUsage, buildingType, personName, personId) {
+    console.log(`Adding lead with source ${source}, energy usage ${energyUsage}, building type ${buildingType}, and person id ${personId} to pipedrive...`);
+
+    const leads = new pipedrive.LeadsApi(pd);
+
+    try {
+        await leads.addLead({
+            title: `Battery Finder: ${personName}`,
+            personId: personId,
+            // todo: add custom fields like energy usage here
+        });
+    }
+    catch(error) {
+        console.log(`Error adding lead: ${error}`);
+    }
 }
 
 
