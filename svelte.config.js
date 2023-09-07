6ec15fb5cfd402dedac7bc1c41abffbe78040f34
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-09-07 09:58:24 +0100

    added back something from merge

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,12 @@ const config = {
 	kit: {
 		adapter: adapter()
 	},
+	vite: {
+		optimizeDeps: {
+			include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+		}
+	}
 };
 
 export default config;
+
