2c6bb07d3536ec461c159521454f108087f468d3
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-09-08 13:41:38 +0100

    test file uploads to upload knowledge base

---
+++ src/routes/chat/upload-documents/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let file;
+  
+    async function uploadFile() {
+      if (!file) return;
+  
+      const formData = new FormData();
+      formData.append('file', file);
+  
+      try {
+        const response = await fetch('/upload', {
+          method: 'POST',
+          body: formData,
+        });
+  
+        if (response.ok) {
+          const result = await response.json();
+          console.log('File uploaded:', result);
+        } else {
+          console.log('Upload failed:', await response.text());
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  </script>
+  
+  <h1>File Upload</h1>
+  <input type="file" accept=".txt" bind:files={file} />
+  <button on:click={uploadFile}>Upload</button>
+  
