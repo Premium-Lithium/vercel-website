bc435f41d8570d6f0c9121632540075e76be6c1b
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-09-08 13:43:20 +0100

    put code in function

--- src/routes/chat/upload-documents/+server.ts
+++ src/routes/chat/upload-documents/+server.ts
@@ -5,27 +5,30 @@ import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/publi
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import {json} from '@sveltejs/kit';
 
-const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+function uploadDocument(text) {
+    const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
-let sections = text.split('---');
-const textSplitter = new RecursiveCharacterTextSplitter( {
-    chunkSize: 500,
-    chunkOverlap: 0,
-})
+    let sections = text.split('---');
+    const textSplitter = new RecursiveCharacterTextSplitter( {
+        chunkSize: 500,
+        chunkOverlap: 0,
+    })
 
-const docs = await textSplitter.createDocuments(sections);
+    const docs = await textSplitter.createDocuments(sections);
 
-const splitDocs = await textSplitter.splitDocuments(docs);
+    const splitDocs = await textSplitter.splitDocuments(docs);
 
-await SupabaseVectorStore.fromDocuments(
-    splitDocs,
-    new OpenAIEmbeddings(),
-    {
-        client: supabase,
-        tableName: "documents",
-        queryName: "match_documents",
-    },
-);
+    await SupabaseVectorStore.fromDocuments(
+        splitDocs,
+        new OpenAIEmbeddings(),
+        {
+            client: supabase,
+            tableName: "documents",
+            queryName: "match_documents",
+        },
+    );
+}
+
    
 const allowedFileTypes = ['txt'];
 
