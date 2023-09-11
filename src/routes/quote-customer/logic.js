0609dc903434e35c794636ddf768268199628b8c
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-11 10:52:19 +0100

    test

--- src/routes/quote-customer/logic.js
+++ src/routes/quote-customer/logic.js
@@ -35,7 +35,7 @@ export default async function quoteCustomer(dealId) {
     const { data, error } = await supabase
     .storage
     .from('email-template')
-    .download('customer-quote-template.mjml');
+    .download('/customer-quote-template.mjml');
     
     console.log(data, error)
     const templatePath = data;
