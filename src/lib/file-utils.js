5765e9bb413dddd0c17b7227a6699d09db50eb9b
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-11 12:05:29 +0100

    test

--- src/lib/file-utils.js
+++ src/lib/file-utils.js
@@ -2,16 +2,23 @@ import mjml2html from 'mjml';
 import nunjucks from 'nunjucks';
 
 async function populateEmailTemplateWith(data, mjmlTemplateRelPath, importMetaUrl) {
-    const response = await fetch(mjmlTemplateRelPath);
     try {
-        const mjmlString = response.text();
-        console.log(mjmlString)
-        const { html } = mjml2html(mjmlString);
-        console.log(html)
-        nunjucks.configure({ autoescape: true });
-        const renderedEmail = nunjucks.renderString(html, data);
-
-        return renderedEmail;
+        var rawFile = new XMLHttpRequest();
+            rawFile.open("GET",mjmlTemplateRelPath,false);
+            rawFile.onreadystatechange = function() {
+                if(rawFile.readyState === 4) {
+                    if(rawFile.status === 200 || rawFile.status === 0)
+                    {
+                        var mjmlString = rawFile.responseText;
+                        console.log(mjmlString);
+                        const { html } = mjml2html(mjmlString);
+                        console.log(html)
+                        nunjucks.configure({ autoescape: true });
+                        const renderedEmail = nunjucks.renderString(html, data);
+                        return renderedEmail;
+                    }
+                }
+            }
     } catch (err) {
         const message = "Error processing the email template";
         console.error(message, err);
