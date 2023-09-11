f47d37f7e940c6c9be9fe930534c0fc423005e14
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-11 14:21:31 +0100

    test

--- src/routes/quote-customer-pd-panel/+page.svelte
+++ src/routes/quote-customer-pd-panel/+page.svelte
@@ -9,8 +9,8 @@
     let dealId;
 
     onMount(async () => {
-        // dealId = $page.url.searchParams.get('selectedIds');
-        dealId = 6592;
+        dealId = $page.url.searchParams.get('selectedIds');
+        
 
         sdk = await new AppExtensionsSDK().initialize();
         await sdk.execute('resize', { height: 100 });
