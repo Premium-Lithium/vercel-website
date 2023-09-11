c926d0b9b99513fcaa95f01ee9da0dc252b2de30
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-11 16:00:46 +0100

    test

--- src/routes/quote-customer/logic.js
+++ src/routes/quote-customer/logic.js
@@ -85,7 +85,7 @@ async function getCustomerInfo(dealId) {
 
 
 function extractEmailFrom(customerData) {
-    const emails = customerData.person_id.email;
+    const emails = customerData.person_id.email.DealPersonDataEmail;
     console.log(customerData.person_id)
     console.log(emails);
 
