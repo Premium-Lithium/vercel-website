175d8e8409f5e3d53a8c36424f29f203ad255b7b
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-12 11:45:47 +0100

    test

--- src/routes/quote-customer-pd-panel/+page.svelte
+++ src/routes/quote-customer-pd-panel/+page.svelte
@@ -28,12 +28,12 @@
             if (response.status === 200) {
                 toastr.success('Quote draft generated successfully!', '', {
                 "positionClass": "toast-bottom-center",
-                "timeOut": "10000",
+                "timeOut": "1000",
                 });
             } else {
                 toastr.error('Failed to send quote draft', '', {
                     "positionClass": "toast-bottom-center",
-                    "timeOut": "10000",
+                    "timeOut": "1000",
                 })
                 console.log('Error:', response.status);
                 return null;
