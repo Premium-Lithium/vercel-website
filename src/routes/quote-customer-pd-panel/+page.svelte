661bd1352b407916c6804515928dabd27a10fcc7
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-13 16:38:06 +0100

    another unhandled promise

--- src/routes/quote-customer-pd-panel/+page.svelte
+++ src/routes/quote-customer-pd-panel/+page.svelte
@@ -14,34 +14,36 @@
     });
 
     async function sendQuoteEmail() {
-        console.log("sending quote email....");
-        try{
-            const response = await fetch('/quote-customer', {
+    console.log("sending quote email....");
+    try {
+        const response = await fetch('/quote-customer', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 deal_id: dealId
             })
-            });
-            console.log(response);
-            if (response.status === 200) {
-                toastr.success('Quote draft generated successfully!', '', {
+        });
+
+        if (response.status === 200) {
+            toastr.success('Quote draft generated successfully!', '', {
                 "positionClass": "toast-bottom-center",
                 "timeOut": "10000",
-                });
-                // setTimeout(() => {
-                //     location.reload();
-                // }, 50);
-                return response
-            } else {
-                console.log("error sending quote");
-                toastr.error('Failed to send quote draft Please Try again', '', {
-                    "positionClass": "toast-bottom-center",
-                    "timeOut": "10000",
-                })
-            }
+            });
+            return response;
+        } else {
+            console.log("error sending quote");
+            toastr.error('Failed to send quote draft. Please try again.', '', {
+                "positionClass": "toast-bottom-center",
+                "timeOut": "10000",
+            });
+        }
     } catch (error) {
-       
+        // Handle the error here, or rethrow it if needed.
+        console.error("Error sending quote:", error);
+        toastr.error('An error occurred while sending the quote draft. Please try again later.', '', {
+            "positionClass": "toast-bottom-center",
+            "timeOut": "10000",
+        });
         return error;
     }
 }
