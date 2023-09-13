6cfbfba2336dd90cf882ca63288b31c2f15e1656
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-13 17:24:12 +0100

    test4

--- src/routes/quote-customer/logic.js
+++ src/routes/quote-customer/logic.js
@@ -238,35 +238,35 @@ async function markAsQuoteIssued(dealId) {
     console.log("marking quote as issued ")
     // Update the `Quote Issued` field on pipedrive with todays date
     // todo: this assumes the dealFieldsRequest in pipedrive-utils was successful
-    const dealFields = dealFieldsRequest.data;
-    const dealsApi = new pipedrive.DealsApi(pd);
+    // const dealFields = dealFieldsRequest.data;
+    // const dealsApi = new pipedrive.DealsApi(pd);
 
-    // const quoteIssuedField = dealFields.find(f => f.name === "Quote issued");
+    // // const quoteIssuedField = dealFields.find(f => f.name === "Quote issued");
 
-    // // if(quoteIssuedField === undefined) {
-    // //     console.log(`Could not find the "Quote issued" field on pipedrive`);
-    // //     return false;
-    // // }
+    // // // if(quoteIssuedField === undefined) {
+    // // //     console.log(`Could not find the "Quote issued" field on pipedrive`);
+    // // //     return false;
+    // // // }
 
-    // // await dealsApi.updateDeal(dealId, {
-    // //     [quoteIssuedField.key]: today()
-    // // });
+    // // // await dealsApi.updateDeal(dealId, {
+    // // //     [quoteIssuedField.key]: today()
+    // // // });
 
-    // // Move the deal to the quote issued stage
-    const stagesApi = new pipedrive.StagesApi(pd);
-    const B2C_PIPELINE_ID = 23;
-    let opts = {
-        'pipelineId': B2C_PIPELINE_ID,
-        'start': 0,
-        'limit': 56
-    };
-    const stages = await stagesApi.getStages(opts);
+    // // // Move the deal to the quote issued stage
+    // const stagesApi = new pipedrive.StagesApi(pd);
+    // const B2C_PIPELINE_ID = 23;
+    // let opts = {
+    //     'pipelineId': B2C_PIPELINE_ID,
+    //     'start': 0,
+    //     'limit': 56
+    // };
+    // const stages = await stagesApi.getStages(opts);
 
-    const quoteIssuedStage = stages.data.find(s => s.name === "Quote Issued");
+    // const quoteIssuedStage = stages.data.find(s => s.name === "Quote Issued");
 
-    await dealsApi.updateDeal(dealId, {
-        stage_id: quoteIssuedStage.id
-    });
+    // await dealsApi.updateDeal(dealId, {
+    //     stage_id: quoteIssuedStage.id
+    // });
 
     return true;
 }
