e17d4994b094b670380d00d4c45666a4ba7063d3
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-13 16:30:39 +0100

    more unhandled promise

--- src/routes/send-mail/+server.ts
+++ src/routes/send-mail/+server.ts
@@ -2,9 +2,26 @@ import { json } from '@sveltejs/kit';
 import validate from '../../lib/validation-utils.js';
 import { sendMail } from './logic.js';
 
+
 const schema = {
-    // Your schema definition here...
-};
+    type: "object",
+    required: [ "recipients", "sender", "subject", "mail_body", "content_type" ],
+    properties: {
+        recipients: {
+            type: "array",
+            items: { "type": "string", "format": "email" },
+            errorMessage: "Recipients should be an array of valid email addresses"
+        },
+        sender: { "type": "string", "format": "email", "errorMessage": "Invalid sender email format" },
+        subject: { "type": "string", "errorMessage": "Subject should be a string" },
+        body: { "type": "string", "errorMessage": "mail_body should be a string" },
+        content_type: {
+            type: "string",
+            enum: [ "HTML", "TEXT" ],
+            errorMessage: "Content type should be either 'HTML' or 'TEXT'"
+        }
+    }
+}
 
 export async function POST({ request }) {
     if (!request.body)
