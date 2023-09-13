964523eb13311852014960d0cf5404d2e8a864eb
jasminep123 <44727280+jasminep123@users.noreply.github.com>
2023-09-13 16:26:32 +0100

    trying more try catches

--- src/routes/send-mail/+server.ts
+++ src/routes/send-mail/+server.ts
@@ -2,45 +2,34 @@ import { json } from '@sveltejs/kit';
 import validate from '../../lib/validation-utils.js';
 import { sendMail } from './logic.js';
 
-
 const schema = {
-    type: "object",
-    required: [ "recipients", "sender", "subject", "mail_body", "content_type" ],
-    properties: {
-        recipients: {
-            type: "array",
-            items: { "type": "string", "format": "email" },
-            errorMessage: "Recipients should be an array of valid email addresses"
-        },
-        sender: { "type": "string", "format": "email", "errorMessage": "Invalid sender email format" },
-        subject: { "type": "string", "errorMessage": "Subject should be a string" },
-        body: { "type": "string", "errorMessage": "mail_body should be a string" },
-        content_type: {
-            type: "string",
-            enum: [ "HTML", "TEXT" ],
-            errorMessage: "Content type should be either 'HTML' or 'TEXT'"
-        }
-    }
-}
+    // Your schema definition here...
+};
 
 export async function POST({ request }) {
-    if(!request.body)
+    if (!request.body)
         return json({ message: "No request body found" }, { status: 400 });
 
     const requestData = await request.json();
     const validationErrors = validate(requestData, schema);
 
     // Check that the request body obeys the schema
-    if(validationErrors.length) {
+    if (validationErrors.length) {
         const message = validationErrors.join(", ");
-        return json({ message: `${message}` }, { status: 400 })
+        return json({ message: `${message}` }, { status: 400 });
     }
 
-    const mailAttempt = await sendMail(...Object.values(requestData));
-    console.log(mailAttempt)
+    try {
+        const mailAttempt = await sendMail(...Object.values(requestData));
+        console.log(mailAttempt);
 
-    return json(
-        { message: mailAttempt.message },
-        { status: mailAttempt.success ? 200 : 500 }
-    );
+        return json(
+            { message: mailAttempt.message },
+            { status: mailAttempt.success ? 200 : 500 }
+        );
+    } catch (error) {
+        // Handle the error here, or log it if needed.
+        console.error("Error sending mail:", error);
+        return json({ message: "Internal server error" }, { status: 500 });
+    }
 }
