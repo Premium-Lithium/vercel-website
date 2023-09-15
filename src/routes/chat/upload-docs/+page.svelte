3634f64464846c265305493e19b977d923025f41
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-09-15 13:57:01 +0100

    fix /chat/upload-docs to work

---
+++ src/routes/chat/upload-docs/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from "svelte";
+    import Loading from "$lib/components/Loading.svelte";
+    let loading = false;
+    let success = false;
+    onMount(async () => {
+        loading = true;
+        const res = await fetch('upload-docs/', {
+            method: "GET",
+        })
+        loading = false;
+        if(res.ok) success = true;
+    })
+
+</script>
+
+<body>
+{#if loading}
+<Loading></Loading>
+{/if}
+{#if success}
+    <h2>documents uploaded</h2>
+{/if}
+</body>
+
+<style>
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
