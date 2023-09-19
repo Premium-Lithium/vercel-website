4a4a29e9c89add0773efdc23666fe09696e0ec12
Peter Gillingham <petergillingham80@gmail.com>
2023-09-19 17:48:53 +0100

    begin implementing calculation for solar energy generation

--- src/routes/energy-arbitrage/prediction/+page.svelte
+++ src/routes/energy-arbitrage/prediction/+page.svelte
@@ -1,23 +1,44 @@
 <script lang="ts">
+	// Energy Arbitrage
 	let timestep = 0;
 	let batteryInstruction;
 
+	// Solar Energy Generation
+	let lon = 0;
+	let lat = 0;
+	let area = 0;
+	let generationForecast;
+
 	async function sendTimestepGetInstruction() {
 		const body = JSON.stringify(timestep);
-		const resp = fetch('/energy-arbitrage/prediction/arbitrage-calculation', {
+		const res = fetch('/energy-arbitrage/prediction/arbitrage-calculation', {
 			method: 'POST',
 			body: body,
 			headers: {
 				'content-type': 'application/json'
 			}
 		});
-        resp.then((responseBody) => {
-            responseBody.json().then(
-                (respVal) => {
-                    batteryInstruction = respVal;
-                }
-            )
-        });
+		res.then((responseBody) => {
+			responseBody.json().then((respVal) => {
+				batteryInstruction = respVal;
+			});
+		});
+	}
+
+	async function calculateSolarGeneration() {
+		const body = JSON.stringify([lat, lon]);
+		const res = fetch('/energy-arbitrage/prediction/generation-forecast', {
+			method: 'POST',
+			body: body,
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+		res.then((responseBody) => {
+			responseBody.json().then((respVal) => {
+				generationForecast = respVal;
+			});
+		});
 	}
 </script>
 
@@ -27,13 +48,34 @@
 		Not a visible component on the final product, but we can use this to play around with sample
 		data
 	</p>
-	<label>
-		Timestep:<input type="range" min="0" max="48" bind:value={timestep} />{timestep}
-	</label>
-	<br />
-	<button on:click={sendTimestepGetInstruction}>Get Instruction for Timestep</button>
-	<br />
-	<p>{batteryInstruction === undefined ? 'No Instruction' : batteryInstruction}</p>
+	<div>
+		<h2>Energy Arbitrage Model</h2>
+		<label>
+			Timestep:<input type="number" min="0" max="48" bind:value={timestep} />
+		</label>
+		<br />
+		<button on:click={sendTimestepGetInstruction}>Get Instruction for Timestep</button>
+		<br />
+		<p>{batteryInstruction === undefined ? 'No Instruction' : batteryInstruction}</p>
+		<br />
+	</div>
+
+	<div>
+		<h2>Solar Energy Generation</h2>
+		<label>
+			Longitude<input type="number" min={-90} max=90 bind:value={lon} />
+		</label>
+		<br />
+		<label>
+			Latitude<input type="number" min={-180} max=180 bind:value={lat} />
+		</label>
+		<br />
+		<label>
+			Solar Panel Area<input type="number" min=0 bind:value={area} />
+		</label>
+		<br />
+		<button on:click={calculateSolarGeneration}> Get Solar Forecast</button>
+	</div>
 </body>
 
 <style>
--- src/routes/energy-arbitrage/prediction/generation-forecast/+server.ts
+++ src/routes/energy-arbitrage/prediction/generation-forecast/+server.ts
@@ -4,6 +4,8 @@
  * Nominal Power Output
  * Area of Solar Panels
  * Efficiency of Solar Panels
+ * Solar Panels Tilt Angle (Beta)
+ * Azimuth Angle of Panel (Ap)
  * Latitude
  * Longitude
  * 
@@ -24,3 +26,80 @@
  * Energy output
  */
 
+import { json } from "@sveltejs/kit";
+
+const nominalPowerOutput = 0;
+const efficiency = 0;
+let forecast = [];
+let solarDeclination; // del
+let solarAngle; // H
+let sunAltitudeAngle; // alpha
+let azimuthAngle; // Az
+let zenith; // theta z
+let angleOfIncidence; // theta
+let irradianceIncident; // Ipoa
+let energyOutput; // Eout
+
+
+function getDay(now) {
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);;
+    var oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+
+}
+
+function getLeapYear(now) {
+    let year = now.getFullYear;
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+}
+
+export async function POST({ request }) {
+    // array of [lat, lon, area]
+    const loc = await request.json();
+    const lat = loc[0];
+    const lon = loc[1];
+    const area = loc[2];
+    const panelTilt = loc[3];
+    const panelAzimuth = loc[4];
+
+    // API calls go here, for now just temp variables
+    let cloud; // array of length 24, used in irrad to determine whether to use cloudy or clear model
+    let irrad; // 2D array of [[DHI, DNI, GHI]] of length 24
+
+    // Calculating day of year and whether it is a leap year
+    let now = new Date();
+    let day = getDay(now);
+    let leapYear = getLeapYear(now);
+    let daysOfYear;
+    if (leapYear) {
+        daysOfYear = 365;
+    } else {
+        daysOfYear = 366;
+    }
+
+    solarDeclination = -23.45 * Math.cos(360 / daysOfYear) * (day - 81);
+
+    // Working over each timestep now
+    for (let i = 0; i < irrad[0].length(); i++) {
+        solarAngle = (60 * i) / 4 - 180;
+
+        sunAltitudeAngle = Math.asin(
+            Math.sin(lat) * Math.sin(solarDeclination) + Math.cos(lat) * Math.cos(solarDeclination) * Math.cos(solarAngle)
+        );
+
+        azimuthAngle = Math.asin(-1 * Math.cos(solarDeclination) * Math.sin(solarAngle));
+
+        zenith = 90 - sunAltitudeAngle;
+
+        angleOfIncidence = Math.acos(Math.cos(azimuthAngle)*Math.cos(panelTilt)+Math.sin(zenith)*Math.cos(panelAzimuth - azimuthAngle));
+
+        irradianceIncident = irrad[i][2] - (irrad[i][1] * Math.cos(angleOfIncidence)) + irrad[i][0];
+
+        energyOutput = nominalPowerOutput * irradianceIncident * area * efficiency;
+
+        forecast.push(energyOutput);
+    }
+
+    return json(forecast)
+}
