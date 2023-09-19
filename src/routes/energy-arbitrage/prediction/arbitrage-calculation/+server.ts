d1a28b344d4ed3fe52ecea86b8a1194b1ffa6bd8
Peter Gillingham <petergillingham80@gmail.com>
2023-09-19 12:32:26 +0100

    Simple Expert Model system for battery instruction

--- src/routes/energy-arbitrage/prediction/arbitrage-calculation/+server.ts
+++ src/routes/energy-arbitrage/prediction/arbitrage-calculation/+server.ts
@@ -2,5 +2,26 @@
 
 
 // takes as input "current" time, forecasted variables: of energy usage, genreation and price
-    // next horizons worth (currently array of 24)
+    // next horizons worth (currently array of 48)
 // sends target charge capacity to inverter
+
+import { json } from '@sveltejs/kit';
+export async function POST({url, request}) {
+    const instruction = "";
+
+    const arbitrageCalculationDest = "/energy-arbitrage/inverter/arbitrage-calculation"
+    let targetUrl = url.origin + arbitrageCalculationDest;
+    const timestep = await request.json();
+    console.log(timestep)
+
+    // Expert User Model goes here
+    if (timestep >=7 && timestep <= 10) {
+        return json(JSON.stringify("Charge (buy energy)"))
+    } else if (timestep >= 34 && timestep <= 36) {
+        return json(JSON.stringify("Discharge (Sell energy)"))
+    } else {
+        return json(JSON.stringify("Normal Operation"))
+    }
+    
+
+}
