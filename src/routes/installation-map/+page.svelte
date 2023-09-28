93dbae393ba45d93798a3065fce7774d6e4b7db1
Peter Gillingham <petergillingham80@gmail.com>
2023-09-28 10:42:25 +0100

    change style

--- src/routes/installation-map/+page.svelte
+++ src/routes/installation-map/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
-	import Map from "$lib/components/Map.svelte";
-    let map;
-    let mapZoom = 4;
+	import Map from '$lib/components/Map.svelte';
+	let map;
+	let mapZoom = 4;
+	let style = 5;
+
+	function changeStyle() {
+		console.log(style);
+		style = style % 7;
+		style += 1;
+	}
 </script>
 
 <div>
 	<h1>Installation Map</h1>
-    <div class="map-view">
-        <Map search={false} style=4 bind:map={map} zoom={mapZoom} --border-radius='10px'></Map>
-    </div>
+	<div class="map-view">
+		{#key style}
+			<Map search={false} bind:style bind:map zoom={mapZoom} --border-radius="10px" />
+		{/key}
+	</div>
+	<div id="styleButton">
+		<button on:click={changeStyle}>Change Style</button>
+	</div>
 </div>
 
 <style>
-    .map-view{
-        width: 100%;
-        height: 80vh;
-    }
+	.map-view {
+		width: 100%;
+		height: 80vh;
+	}
 </style>
