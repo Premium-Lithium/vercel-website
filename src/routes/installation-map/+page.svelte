8f20025e823fdb43f665cbd40cfe63bd408c0086
Peter Gillingham <petergillingham80@gmail.com>
2023-09-28 10:13:04 +0100

    map acquired

--- src/routes/installation-map/+page.svelte
+++ src/routes/installation-map/+page.svelte
@@ -1,11 +1,19 @@
-<script lan="ts">
-
+<script lang="ts">
+	import Map from "$lib/components/Map.svelte";
+    let map;
+    let mapZoom = 4;
 </script>
 
-<body>
-
-</body>
+<div>
+	<h1>Installation Map</h1>
+    <div class="map-view">
+        <Map search={false} style=4 bind:map={map} zoom={mapZoom} --border-radius='10px'></Map>
+    </div>
+</div>
 
 <style>
-    
+    .map-view{
+        width: 100%;
+        height: 80vh;
+    }
 </style>
