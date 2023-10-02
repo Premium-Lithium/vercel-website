244e8be252454ed0fdc90a1d2b0daea661f667eb
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-02 11:54:15 +0100

    wip

--- src/routes/installation-map-popup-callback/server.ts
+++ src/routes/installation-map-popup-callback/server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 import { ApiClient } from 'pipedrive';
 
-
+console.log("TEST")
 export async function GET({ request }) {
     const url = new URL(request.url);
     const code = url.searchParams.get('code');
@@ -15,15 +15,17 @@ export async function GET({ request }) {
 
     return json({}, {status: 200})
 }
-    
+
 function initAPIClient({ accessToken = '', refreshToken = '' }) {
     const client = new ApiClient();
     const oAuth2 = client.authentications.oauth2;
 
-    oAuth2.clientId = process.env.PIPEDRIVE_EXTENSION_CLIENT_ID;
-    oAuth2.clientSecret = process.env.PIPEDRIVE_EXTENSION_CLIENT_SECRET;
-    oAuth2.redirectUri = process.env.PIPEDRIVE_EXTENSION_REDIRECT_URI;
-
+    //oAuth2.clientId = process.env.PIPEDRIVE_MAP_CLIENT_ID;
+    //oAuth2.clientSecret = process.env.PIPEDRIVE_MAP_CLIENT_SECRET;
+    //oAuth2.redirectUri = process.env.PIPEDRIVE_EXTENSION_REDIRECT_URI;
+    oAuth2.clientId = "ef5e6f3326300586";
+    oAuth2.clientSecret = "4144bf18049e77eec669fe54c248e23b6211167c";
+    oAuth2.redirectUri = "https://734a-148-252-132-46.ngrok-free.app/";
     if (accessToken)
         oAuth2.accessToken = accessToken;
 
