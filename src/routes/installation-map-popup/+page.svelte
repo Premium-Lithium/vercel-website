8408d6ef25482d92d4181e1a2f9cf5045bc27a9e
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-02 11:54:15 +0100

    wip

--- src/routes/installation-map-popup/+page.svelte
+++ src/routes/installation-map-popup/+page.svelte
@@ -2,49 +2,13 @@
     import { onMount, tick } from 'svelte';
     import { page } from '$app/stores'
     import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
-    import toastr from 'toastr';
-    import 'toastr/build/toastr.min.css';
 
     let sdk;
-    let addAttachment = true;
-    let dealId = $page.url.searchParams.get('selectedIds');
-
     onMount(async () => {
         sdk = await new AppExtensionsSDK().initialize();
         await sdk.execute('resize', { height: 100 });
     });
 
-    async function sendQuoteEmail() {
-        console.log("sending quote email....");
-        console.log(addAttachment);
-        try {
-            const response = await fetch('/quote-customer', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    deal_id: dealId,
-                    addAttachment: addAttachment
-                })
-            });
-
-            if (response.status === 200) {
-                toastr.success('Quote draft generated successfully!', '', {
-                    "positionClass": "toast-bottom-center",
-                    "timeOut": "10000",
-                });
-                return response;
-            }else{
-                // Handle the error here, or rethrow it if needed.
-                console.error("Error sending quote:", response.status);
-                toastr.error('An error occurred while sending the quote draft. Please try again later.', '', {
-                    "positionClass": "toast-bottom-center",
-                    "timeOut": "10000",
-                });
-            }
-        } catch (error) {
-            return error;
-        }
-    }
 </script>
 
 <div style="padding: 0px 15px;">
