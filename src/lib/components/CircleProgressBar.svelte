5a8e13d2e23a52b929aed65dbe7f17ace4e18f7a
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-09 16:26:02 +0100

    circle progress bar wip

---
+++ src/lib/components/CircleProgressBar.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let progress;
+	const angle = 360 * progress;
+
+	const background = `radial-gradient(white 50%, transparent 51%),
+    conic-gradient(transparent 0deg ${angle}deg, gainsboro ${angle}deg 360deg),
+    conic-gradient(blue 0deg, blue 90deg, blue 180deg, blue);`;
+
+	$: cssVarStyles = `--background:${background}`;
+</script>
+
+<div id="progress-circle" style={cssVarStyles} /> {progress}
+
+<style>
+	#progress-circle {
+		background: var(--background);
+		border-radius: 50%;
+		width: 120px;
+		height: 120px;
+		will-change: transform;
+	}
+</style>
