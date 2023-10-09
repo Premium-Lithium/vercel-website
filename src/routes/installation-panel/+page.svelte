45dcb99ba05c569a6f231ef04e47d447b5270790
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-09 10:32:09 +0100

    implement progress bar

--- src/routes/installation-panel/+page.svelte
+++ src/routes/installation-panel/+page.svelte
@@ -4,11 +4,16 @@
 	import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
 	import pipedrive from 'pipedrive';
 	import { json } from '@sveltejs/kit';
+    import ProgressBar from '$lib/components/ProgressBar.svelte';
 
 	let sdk;
 	let checkListData = {};
 	let selectedPhase = {};
 	let selectedTasks = {};
+    let counter = {
+            assigned: 0,
+            inProgress:0
+        }
 	const dealId = $page.url.searchParams.get('dealId');
 
 	const fieldNames = {
@@ -96,16 +101,19 @@
 									if (!selectedTasks[stage]) {
 										selectedTasks[stage] = [];
 									}
+                                    counter[stage] += 1
 									selectedTasks[stage].push({ id: task.id, label: task.label });
 								} else {
-									console.log(`Task not found for label: ${label}`);
+									console.log("Task not found");
 								}
 							} else {
-								console.log(`Task list not found for stage: ${stage}`);
+								console.log("Task list not found for stage");
 							}
 						}
 					});
 				});
+                console.log("Init counter:", counter)
+                
 			}
 		} catch (error) {
 			console.log('Error', error);
@@ -115,14 +123,18 @@
 		const taskId = Number(event.target.value);
 		const taskLabel = event.target.name;
 		const stageName = checkList;
+        console.log(counter)
+        
 
 		if (event.target.checked) {
 			if (!selectedTasks[stageName]) {
 				selectedTasks[stageName] = [];
 			}
+            counter[stageName] += 1;
 			selectedTasks[stageName].push({ id: taskId, label: taskLabel });
 		} else {
 			// Remove the task from selectedTask if unchecked
+            counter[stageName] -= 1;
 			selectedTasks[stageName] = selectedTasks[stageName].filter((task) => task.id !== taskId);
 		}
 		// Call the function to send PUT request (you need to implement this)
@@ -153,6 +165,10 @@
 <div class="project-panel">
 	{#each Object.entries(checkListTemplate) as [stage, tasks]}
 		<h3>{fieldNames[stage]}</h3>
+        <div class="progress-container">
+            <p>{counter[stage]} / {Object.keys(tasks).length}</p>
+            <ProgressBar width={(counter[stage] / Object.keys(tasks).length)*100}%/><!-- Progress % -->
+        </div>
 		{#each tasks as task}
 			<li>
 				<label>
@@ -174,5 +190,6 @@
 	.project-panel {
 		list-style: none;
 		padding: 0px 15px;
+        width: 100%;
 	}
 </style>
