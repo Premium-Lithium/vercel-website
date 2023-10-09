222c15f859acc9e14f9126f22da7b78b6ad9d8ec
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-09 09:10:19 +0100

    implement dynamic checkboxes

--- src/routes/installation-panel/+server.ts
+++ src/routes/installation-panel/+server.ts
@@ -6,7 +6,12 @@ import { PIPEDRIVE_API_TOKEN } from '$env/static/private';
 
 const companyDomainFields = 'https://api.pipedrive.com/v1/deals/'
 
-let dealId = 7083;
+let dealId;
+// Stage, {Task id, Task label}
+const fieldNames = {
+    assigned: "Assigned Checklist",
+    inProgress: "In Progress Checklist"
+}
 
 async function fetchDealData(dealId) {
     try {
@@ -25,27 +30,11 @@ async function fetchDealData(dealId) {
         return json({ error: 'An error occurred' }, { status: 500 });
     }
 }
-updateCustomOption(7083);
-async function updateCustomOption(dealId){
-    try {
-        const pdDealFieldsApi = new pipedrive.DealFieldsApi(pd);
-        const dealFieldsRequest = await pdDealFieldsApi.getDealFields();
-        const allField = dealFieldsRequest.data;
-        console.log("ALL FIELD",allField)
-        
-        let opts = dealFieldsRequest.FieldUpdateRequest({"Test"})
-        console.log("opts", opts)
-    } catch (error) {
-        console.log("Error")
-    }
-}
 
 function getChecklistData(dealData) {
-    let assignedChecklist = readCustomDealField("Assigned Checklist", dealData);
-    let inProgressChecklist = readCustomDealField("In Progress Checklist", dealData);
     let checkListData = {
-        assigned: assignedChecklist,
-        inProgress: inProgressChecklist
+        assigned: readCustomDealField("Assigned Checklist", dealData),
+        inProgress: readCustomDealField("In Progress Checklist", dealData)
     }
     return checkListData;
 }
@@ -63,51 +52,46 @@ export async function POST({ request }) {
         return json({ error: "Can't get checkListData" })
     }
 }
-let checkListTemplate = {
-    'Assigned Checklist': [
-        'Once initial desktop investigation has been completed call client to advise on next steps',
-        'Arrange follow up call with client'
-    ],
-    'In Progress Checklist': [
-        'Update Ticket details with outcome of discussion with client',
-        'Resolve ticket remotely where possible',
-        'Liaise with installer to arrange rectification visit as required',
-        'Order any new materials required',
-        'Book site visit in with client'
-    ]
-};
-
-
-
 // request = fieldName, data
 // GetKeyFrom fieldName
 // Use that key to get the actual field
 // Update that field value to data (data have to be value options)
+
+// PUT Request Body: {assigned: [{id: , label: }]}
+// Convert to Assigned Checklist: [999,1000], In Progress Checklist:[1002,1003]     //for getKeysforCustomFields format
 export async function PUT({ request }) {
     try {
         const response = await request.json();
-        let addRes = getKeysForCustomFields(response)
-        addRes = { [addRes[0][0]]: addRes[0][1] }
-        const addResponse = await addFieldsForOptions(addRes)
-        console.log(addResponse);
+        const propertyName = Object.keys(response)[0];// Extract the property name from response object
+        const checklistLabel = fieldNames[propertyName];// Map the property name to the corresponding label using fieldNames
+        const idValues = response[propertyName].map(item => item.id);// Extract the id values from the array of objects
+
+        const result = {[checklistLabel]: idValues};
+        console.log(result);
+        const addResponse = await updateFieldsForOptions(result, response.dealId);
         return json(addResponse);
     } catch (error) {
         console.log("Error:", error);
         return json({ error: "An error occured" })
     }
-
 }
+
 // Add data to multiple options
 // request = fieldName, data
 // GetKeyFrom fieldName
 // Use that key to get the actual field
 // Update that field value to data (data have to be value options)
-async function addFieldsForOptions(response) {
+async function updateFieldsForOptions(response, dealId) {
+    const keyedData = getKeysForCustomFields(response)
+    console.log(keyedData)
     const req = {
         method: "PUT",
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(response)
+        body: JSON.stringify({[keyedData[0][0]]: keyedData[0][1]})
     };
     let res = await fetch(companyDomainFields + dealId + '?api_token=' + PIPEDRIVE_API_TOKEN, req);
-    return (new Response(JSON.stringify(await res.json())));
+
+    return (new Response(JSON.stringify(res.json())));
 }
+
+
