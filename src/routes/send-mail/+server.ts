5ac764c43412a5df85bce6bb07acc8ca751783a0
nicholasdharmadi <145343679+nicholasdharmadi@users.noreply.github.com>
2023-10-17 17:24:34 +0100

    Update +server.ts

--- src/routes/send-mail/+server.ts
+++ src/routes/send-mail/+server.ts
@@ -39,7 +39,6 @@ export async function POST({ request }) {
     try {
         const mailAttempt = await sendMail(...Object.values(requestData));
         console.log(mailAttempt);
-        
         return json(
             { message: mailAttempt.message },
             { status: mailAttempt.success ? 200 : 500 }
