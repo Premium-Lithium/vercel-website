636ec8356d550a2af02fc3b0d0174f7f0d13de41
Peter Gillingham <petergillingham80@gmail.com>
2023-10-19 16:52:40 +0100

    WIP

--- src/routes/qr-codes/+page.svelte
+++ src/routes/qr-codes/+page.svelte
@@ -4,9 +4,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { createClient } from '@supabase/supabase-js';
-	import { GET } from '../energy-arbitrage/inverter/battery-level/+server';
 	import { json } from '@sveltejs/kit';
-	// TODO put these in env
 	const PUBLIC_SUPABASE_ANON_KEY =
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtZnV3bWR5bm92ZGx5b2psdGhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MjE3NzUsImV4cCI6MjAxMTI5Nzc3NX0.PzKOk4uqHBfSRGg8j2LjL0R8WAyp_iBdLckYPEigCvc';
 	const PUBLIC_SUPABASE_URL = 'https://tmfuwmdynovdlyojlthe.supabase.co';
@@ -69,9 +67,13 @@
 			const clotheQrCodeFileName = qrCodeFileName + '_clothing';
 
 			// QR Code maker
-			const qrCodeSVG = await GET({json.stringify(qrCodeURL)})
-			console.log(qrCodeSVG);
+			const body = JSON.stringify(qrCodeURL)
 
+			const qrRes = await fetch("/qr-codes", {
+				method: "POST",
+				body: body
+			})
+			console.log(qrRes);
 		}
 	}
 </script>
--- src/routes/qr-codes/+server.ts
+++ src/routes/qr-codes/+server.ts
@@ -1,10 +1,33 @@
 import { json } from "@sveltejs/kit";
-const domain = "http(s)://api.qrserver.com/v1/create-qr-code/?data="
+const url = 'https://qrcode-monkey.p.rapidapi.com/qr/custom';
 
-export async function GET({ request }) {
-    const qrReq = await request.json()
-    const qrRes = await fetch(domain+qrReq+"&bgcolor=255-255-255&qzone=4&format=svg");
+export async function POST({ request }) {
 
-    return qrRes;
+    const data = await request.json()
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '3ca4e3ad93msh88c8bfda74eede9p15e7f5jsncd24c2b319d1',
+            'X-RapidAPI-Host': 'qrcode-monkey.p.rapidapi.com'
+        },
+        body: {
+            data: data,
+            size: 300,
+            download: false,
+            file: 'svg'
+        }
+    };
+    let result;
+    try {
+        const response = await fetch(url, options);
+        result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+
+    return result;
 }
 
