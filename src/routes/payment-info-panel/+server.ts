82c4dbc86d767da3413d71c60b41cfa08817c566
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-23 09:12:13 +0100

    the pdf now can be attached to pipedrive deals!

--- src/routes/payment-info-panel/+server.ts
+++ src/routes/payment-info-panel/+server.ts
@@ -4,13 +4,10 @@ import { XeroClient, Invoices, Invoice, LineItem, Contact } from 'xero-node';
 import { pd, readCustomDealField } from '../../lib/pipedrive-utils.js';
 import querystring from 'querystring';
 import fs from 'fs';
-import path from 'path';
 
 const XERO_CLIENT_ID = "58566968C54B401F82854F6C633E43B5"
 const XERO_CLIENT_SECRET = "xHotLIrz1eeZqG3Ggeny7SNISo3XcLkFuwC9hMewAGmJodD2"
 const XERO_REDIRECT_URI = "http://localhost:3000/payment-info-panel"
-
-
 
 const xero = new XeroClient({
     clientId: XERO_CLIENT_ID,
@@ -31,13 +28,14 @@ export async function POST({ request }) {
         //store token to supabase (?)
         const activeTenantId = await startXero(tokenSet)
         const allInvoices = await getInvoices(activeTenantId);
-        const bufferD = await getInvoiceAsPDF(activeTenantId, "ca348509-7c4f-4bf4-8a94-0b43c71cd1d1")
-        //xero.invoices.streamInvoice('3f887d04-cbd9-4853-b594-bc1ab207558c', 'pdf', stream);
+        const pdfBuffer = await getInvoiceAsPDF(activeTenantId, "ca348509-7c4f-4bf4-8a94-0b43c71cd1d1")
+
         let data = {
             paymentData: paymentData,
             invoice: getInvoiceFromRef(allInvoices, 'PL0005577'),
-            buffer: bufferD
+            buffer: pdfBuffer
         }
+
         return json(data);
     } catch (error) {
         console.log("Error:", error);
@@ -57,18 +55,10 @@ export async function GET({ request }) {
 
 //Initialization 
 //Get Active Tenant
+// TO DO - refresh the token so user does not need to keep on clicking authorize multiple times
 async function startXero(tokenSet) {
     console.log(tokenSet)
-    
-    tokenSet = {
-        id_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjFDQUY4RTY2NzcyRDZEQzAyOEQ2NzI2RkQwMjYxNTgxNTcwRUZDMTkiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJISy1PWm5jdGJjQW8xbkp2MENZVmdWY09fQmsifQ.eyJuYmYiOjE2OTgwMDc1MjMsImV4cCI6MTY5ODAwNzgyMywiaXNzIjoiaHR0cHM6Ly9pZGVudGl0eS54ZXJvLmNvbSIsImF1ZCI6IjU4NTY2OTY4QzU0QjQwMUY4Mjg1NEY2QzYzM0U0M0I1IiwiaWF0IjoxNjk4MDA3NTIzLCJhdF9oYXNoIjoiWTZ1OEJmZVp6NjFSN0c0OVY5SE9YdyIsInNpZCI6IjIzYjI5MjU4NTgyODQ5ZTFhZjI0MGJmOTAzOTg1YmRiIiwic3ViIjoiOGMzY2UzNjdjNmNkNWZlM2FhMWUwNmI2MjBhNmY5OWEiLCJhdXRoX3RpbWUiOjE2OTgwMDY0NTMsInhlcm9fdXNlcmlkIjoiYWExZTFlNzYtYTY4ZS00ZjRmLThlNWEtZmFjZjdkMzA1MDVlIiwiZ2xvYmFsX3Nlc3Npb25faWQiOiIyM2IyOTI1ODU4Mjg0OWUxYWYyNDBiZjkwMzk4NWJkYiIsInByZWZlcnJlZF91c2VybmFtZSI6Im5pY2hvbGFzLmRoYXJtYWRpQHByZW1pdW1saXRoaXVtLmNvbSIsImVtYWlsIjoibmljaG9sYXMuZGhhcm1hZGlAcHJlbWl1bWxpdGhpdW0uY29tIiwiZ2l2ZW5fbmFtZSI6Ik5pY2hvbGFzIiwiZmFtaWx5X25hbWUiOiJEaGFybWFkaSIsIm5hbWUiOiJOaWNob2xhcyBEaGFybWFkaSIsImFtciI6WyJwd2QiXX0.Zbkj0O7HJLbMPrpW2MTHvVvAMyG-YOQduBQQRgh8m0-XiOKvbGCFewXEIj_ehKmFYZhip9CbPGE4MeYJZZzawPOY4YnyCdRtH_Bh24N8amlghfELLLC2tuPH-30v-hz5xZv0NBcK9caMa7u7fG9aXRlUR5E_iShFMUDhN4cIXAK3cSqOK3ArhnO2n9LUjjn68RtTtLYCo7hU10AOS70GXr-R3ZUNK9O9eS78E8IN8N1LAtKJH_U3yF_gc9QBShutnhO4PUmY9onbWUaud11cK7aNpLNrRx9a2Ns80WsmUoEjN1MLufEyJkb4zX8nfE_Z5Yg2zVJaUX9ZYzwc82M28Q',
-        access_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjFDQUY4RTY2NzcyRDZEQzAyOEQ2NzI2RkQwMjYxNTgxNTcwRUZDMTkiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJISy1PWm5jdGJjQW8xbkp2MENZVmdWY09fQmsifQ.eyJuYmYiOjE2OTgwMDc1MjMsImV4cCI6MTY5ODAwOTMyMywiaXNzIjoiaHR0cHM6Ly9pZGVudGl0eS54ZXJvLmNvbSIsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHkueGVyby5jb20vcmVzb3VyY2VzIiwiY2xpZW50X2lkIjoiNTg1NjY5NjhDNTRCNDAxRjgyODU0RjZDNjMzRTQzQjUiLCJzdWIiOiI4YzNjZTM2N2M2Y2Q1ZmUzYWExZTA2YjYyMGE2Zjk5YSIsImF1dGhfdGltZSI6MTY5ODAwNjQ1MywieGVyb191c2VyaWQiOiJhYTFlMWU3Ni1hNjhlLTRmNGYtOGU1YS1mYWNmN2QzMDUwNWUiLCJnbG9iYWxfc2Vzc2lvbl9pZCI6IjIzYjI5MjU4NTgyODQ5ZTFhZjI0MGJmOTAzOTg1YmRiIiwic2lkIjoiMjNiMjkyNTg1ODI4NDllMWFmMjQwYmY5MDM5ODViZGIiLCJqdGkiOiI5MDlGMzczNUJDMUI1MTNBOURFRUU3QzA2OTAwNDVBMiIsImF1dGhlbnRpY2F0aW9uX2V2ZW50X2lkIjoiZWUwMTVlMjktYTZhZi00YmRjLWExYTEtM2U4MzVkYTcyYjE4Iiwic2NvcGUiOlsiZW1haWwiLCJwcm9maWxlIiwib3BlbmlkIiwiYWNjb3VudGluZy5zZXR0aW5ncy5yZWFkIiwiYWNjb3VudGluZy5hdHRhY2htZW50cyIsImFjY291bnRpbmcudHJhbnNhY3Rpb25zIiwiYWNjb3VudGluZy5jb250YWN0cyIsIm9mZmxpbmVfYWNjZXNzIl0sImFtciI6WyJwd2QiXX0.pnCU_rHEmynL66kaDKJge5NZW_8Yj9cbo7fsXqlHmKoZXD2zncPSSTn-_nTGLz15xmM2t_RyjskiTNwlGtbTd2t640Yk7M6Wl0gvLo7aAg6NB5nh5VtllKxY5kgLEyuaNXTWTTG5UObedBai11CUUSQum8GjL-VU44QLZu-6X9UI-24zLedpUwsbRNtS0I_-pasxEsirGj637uVG0caeBE4DdAyZyG29XVtWYsh-GJhJVtRNgvyyievctGEMh_G5YLJNFbBgTdmC0StRUq1CMSZS1HDh-yjEKjvCjaNCowkpAS_k1gWAYJaSzgY_eCltZUDCZa6wtVGHvfrsWLr4gw',
-        expires_in: 1800,
-        token_type: 'Bearer',
-        refresh_token: '43s3N48uwNIqwIsyLak3YAIKxP8bfqQbAGrKYb8me1w',
-        scope: 'openid profile email accounting.transactions accounting.attachments accounting.contacts accounting.settings.read offline_access'
-      }
-      
+
     try {
         await xero.initialize();
         await xero.setTokenSet(tokenSet);
@@ -108,6 +98,25 @@ async function fetchDealData(dealId) {
     }
 }
 
+// TO DO - PDF needs to be stored as a file instead of a buffer to be attached to Pipedrive deals. So might need to upload to supabase.
+async function attachPDFTo(buffer, dealId) {
+    try {
+        // Store the invoice to static folder temporarily so it can be attached to pipedrive deals (API reads a file and does not like blob objects)
+        const filePath = './static/invoice.pdf';
+
+        // Write the Buffer to the file
+        fs.writeFileSync(filePath, buffer);
+
+        const pdFilesApi = new pipedrive.FilesApi(pd);
+        const addFileRequest = await pdFilesApi.addFile('./static/invoice.pdf', { 'dealId': dealId })
+
+        console.log(addFileRequest)
+
+    } catch (error) {
+        console.error("Error attaching PDF to deal:", error)
+    }
+}
+
 async function fetchProductsFromDeal(dealId) {
     try {
         const response = await fetch(`https://api.pipedrive.com/v1/deals/${dealId}/products?api_token=77a5356773f422eb97c617fd7c37ee526da11851`)
@@ -129,16 +138,17 @@ async function fetchProductsFromDeal(dealId) {
     }
 }
 
-//Type
-//Contact
-//Line Items
+// Type
+// Contact
+// Line Items
+// https://developer.xero.com/documentation/api/accounting/types#invoices
 async function createXeroInvoice(activeTenantId) {
     try {
         const dateValue = '2020-10-10'
         const dueDateValue = '2020-10-28'
 
         const contact: Contact = {
-            contactID: "817588c0-b1e5-4e4c-9e2e-3ded7ae9335c"
+            contactID: "817588c0-b1e5-4e4c-9e2e-3ded7ae9335c" // Choose from contact lists
         };
 
         const lineItem: LineItem = {
