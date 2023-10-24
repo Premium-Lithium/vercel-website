ce214ec1bef54691db65c6122b3a9bfcd30454dd
Peter Gillingham <petergillingham80@gmail.com>
2023-10-24 12:14:35 +0100

    update packages

--- src/routes/qr-codes/+page.svelte
+++ src/routes/qr-codes/+page.svelte
@@ -5,6 +5,7 @@
 	import { onMount } from 'svelte';
 	import { createClient } from '@supabase/supabase-js';
 	import { json } from '@sveltejs/kit';
+	import FileSaver, { saveAs } from 'file-saver'
 	const PUBLIC_SUPABASE_ANON_KEY =
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtZnV3bWR5bm92ZGx5b2psdGhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MjE3NzUsImV4cCI6MjAxMTI5Nzc3NX0.PzKOk4uqHBfSRGg8j2LjL0R8WAyp_iBdLckYPEigCvc';
 	const PUBLIC_SUPABASE_URL = 'https://tmfuwmdynovdlyojlthe.supabase.co';
@@ -43,6 +44,8 @@
 	 * 	- download the file
 	 * Add error message to display whenever there is an error
 	 */
+
+	downloadFile("test");
 
 	let addName: string = '',
 		lookupName: string = '',
@@ -127,6 +130,10 @@
 			// Need to reload 
 			location.reload();
 		}
+	}
+
+	async function downloadFile(file) {
+		let fileSaved = saveAs(file, "QRCodes.zip");
 	}
 
 	function lookupQRCode() {
