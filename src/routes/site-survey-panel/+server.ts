41d5cc347e0844ad9ef23cf399ad2363ba9035be
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-10-27 09:11:56 +0100

    collecting custom field mappings

--- src/routes/site-survey-panel/+server.ts
+++ src/routes/site-survey-panel/+server.ts
@@ -11,7 +11,8 @@ const companyDomainFields = 'https://api.pipedrive.com/v1/deals/'
 const SAFETY_CULTURE_TOKEN = 'f5a8b512b90d4ea239858d63f768cdbcdb8cd83c6bd2216001ceb5f20a35632c'
 
 const templates = {
-    'PV, Battery and EV Survey - Testing': 'template_c8a5e85ccaf948358be9c7854aed847d'
+    'PV, Battery and EV Survey - Testing': 'template_c8a5e85ccaf948358be9c7854aed847d',
+    'PV, Battery and EV Survey': 'template_ca68194ee1fa4fae98c923fb5e3b4edb' // new template
 }
 
 export async function POST({ request }) {
@@ -70,14 +71,56 @@ function getCustomerDataFrom(dealData) {
     return customerData
 }
 
-const sectionsMapping = {
-    'PL Number': '866ec171-9b09-40af-86f1-4dc20fbf2b75',
-    'Customer Name': '3f38c167-5a2b-40a5-aa88-329886e7a5ef',
+
+const safetyCultureFieldsMapping = {
+    'PL Reference': '2ccff49f-03fa-4ab2-a2bf-7fa948bf06eb',
+    'Customer Name': 'd9930677-8f40-448d-8766-26a9ebdd55ad',
     'Property Address': '7288b6ad-d800-410e-94bb-f52c92fcbf5f',
+    'Conducted on': '5e88e4da-6dfb-48e2-8ddd-925564f690aa',
+    'Prepared by': 'cc609f47-fc22-4df9-b624-726421d150d5',
+    'Does the customer have an existing installation?': '8e5901b1-5c04-48fb-a878-7c069e507ff5',
+    'Make and model of existing inverter': '3c696419-8380-434c-85d5-8836487761e9',
     'MPAN': '047b6bc5-f478-44d4-bf12-91fc51f560a9',
-    'Estimated Electricity Usage Per Year': 'cadce879-b36a-436a-b65f-201b5c99e710',
-    'Comments': 'e54cd46a-d8cb-4a2e-9190-2a6efd69dcbf'
+    'Trenching?': null,
+    'Proposed Battery Location': null,
+    'Scaffolding?': null,
+    'Stringing Configuration': null,
+    'Azimuth, Pitch': null,
+    'Anual consumption': 'cadce879-b36a-436a-b65f-201b5c99e710',
+    'Shade factor': null,
+    'Roof Structure Type': null,
+    'Roof Type': 'e2e4d156-dc1a-4079-b1f4-826f1ea4efce',
+    'Roof Tile Type': null,
+    'Site Survey Status': null,
+    'Additional Comments': '7d67e682-b402-41ef-bedd-a53e5292bf33'
 }
+
+const safetyCultureResponsesMapping = {
+    'Roof Tile Type': {
+      'rosemary': '88e5b264-45fe-4f6a-a410-fa7a5fa07372',
+      'concrete': '47f9144e-ae4e-4c8b-8304-abba1f5eaa01',
+      'slate': '055d6483-9e8e-4abe-9289-4ecfd7e7cc1f',
+      'trapezoidal': 'e06d490f-0678-4629-8740-ae6586730744',
+      'flat': '4f77c3ec-0b33-4b24-802e-91e0d5fec5f8',
+      'other': 'b6f61759-5ea3-4f2d-ac48-4f19ae770271'
+    }
+  };
+
+const pipeDriveFieldsToUpdate = {
+    'Existing Inverter - Make/Model/Size': '66ae80e6e27e7af328cb51c2de5a6c3df2afd04a',
+    'MPAN number': '75b418263a46a2ee1025fc8f87d730219484b56b',
+    'Is trenching required?': '2d988809bd18aa3c93a6aa7be58bea337aed59a4',
+    'Proposed battery location - request photo': 'c648ac1a374a3569a74cd39e646529152bb944b7',
+    'Scaffolding Required?': '120b9ae729965a5bbce64521e8d100c1b75366a8',
+    'Stringing Configuration': 'c7565066588250604677c6a9f33fbba76ea2643b',
+    'Azimuth, Pitch': '1c7c3a1b307f040a89e95dab1cfe5e177e45d51f',
+    'Annual consumption & Unit Rate': '8ff87ffe3025b9834523d114e8f68c03b794609f',
+    'Shade factor': 'bdc33651d8e28f0ff89f054d2a6ae9cd6aa846f9',
+    'Roof Structure Type': '102498b690aae6ccf74ba24764adc239c23a3aae',
+    'Roof Type': '5969028bbc28e9f087522eab021d261abddf3dcf',
+    'Roof Tile Type': '47692599a527f65407125f43a1a4fb2f79ad0df6',
+    'Site Survey Status': 'e775d10dd3e167c96a1d7cf0a12639ad9cfc1548',
+};
 
 async function createInspectionFrom(dealData) {
     const customerData = getCustomerDataFrom(dealData)
@@ -280,7 +323,7 @@ async function exportInspectionAsPDF(inspection_id) {
     }
     const response = await fetch('https://api.safetyculture.io/inspection/v1/export', options)
     const responseData = await response.json() // returns PDF download link
-    
+
     const pdfResponse = await fetch(responseData.url) // Make HTTP request to download the file
 
     // Convert readable stream to buffer
@@ -323,7 +366,7 @@ async function updateMPAN(dealData) {
     }
 
     if (inspectionAnswer) {
-        const keyedData = getKeysForCustomFields(fieldsToUpdate)
+        const keyedData = getKeysForCustomFields(fieldsToUpdate) // returns the mapping of custom field key 
         const customFieldKey = keyedData[0][0];
         const customFieldValue = keyedData[0][1];
 
