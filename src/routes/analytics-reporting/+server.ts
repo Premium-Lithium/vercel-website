fc8d37061d84423b77fda501328b768fba590500
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-10-31 17:46:08 +0000

    additional option function

--- src/routes/analytics-reporting/+server.ts
+++ src/routes/analytics-reporting/+server.ts
@@ -5,7 +5,7 @@ import { matomoAPICall } from "./matomoQuery.server";
 
 export const POST: RequestHandler = async ({ request }) => {
     const params: Array<Array<string>> = await request.json()
-    // make a call to matomp API for whatever data is asked for 
+    // make a call to matomo API for whatever data is asked for 
 
     const data = await matomoAPICall(params)
     console.log(data)
