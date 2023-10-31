98f84647b7a7d99d7e0b0ac61d5f3b8d2b08e2e7
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-10-31 17:09:40 +0000

    got interaction with the API

--- src/routes/analytics-reporting/+server.ts
+++ src/routes/analytics-reporting/+server.ts
@@ -1,2 +1,13 @@
 // API endpoint for fetching data from matomo
 
+import { json, type RequestHandler } from "@sveltejs/kit";
+import { matomoAPICall } from "./matomoQuery.server";
+
+export const POST: RequestHandler = async ({ request }) => {
+    const params: Array<Array<string>> = await request.json()
+    // make a call to matomp API for whatever data is asked for 
+
+    const data = await matomoAPICall(params)
+    console.log(data)
+    return json(data)
+};
