6f597b9bbf9e025e2311766138fac548fff876ec
Nicholas <nicholas.dharmadi@premiumlithium.com>
2023-11-01 12:27:45 +0000

    panel to update more fields

--- src/routes/site-survey-panel/+server.ts
+++ src/routes/site-survey-panel/+server.ts
@@ -31,6 +31,7 @@ export async function POST({ request }) {
             response = await updateCustomFieldsFrom(dealData);
             await addNote(dealData)
         } else {
+            console.log(getField('Roof Tile Type'))
             response = await getStatusFromInspection(dealData);
         }
 
@@ -86,49 +87,45 @@ const safetyCultureFieldsToRetrieveMapping = {
     'Scaffolding?': '76fb0ffe-3e3d-45d9-9554-78e229ff112e',
     'Roof Type': 'e2e4d156-dc1a-4079-b1f4-826f1ea4efce',
     //'Additional Comments': '7d67e682-b402-41ef-bedd-a53e5292bf33',
+    'Roof Pitch': '530917c3-ac73-49d6-b3b0-9b73c75ef3ab',
+    'Roof orientation from south': '22bd6c9b-5802-4b1e-9b53-12fc3f6d4012',
+    'Is it a truss roof?': '1e5da9a2-11aa-4e33-9d48-00ce2880f1ed',
 }
-
-
-
 
 // TO - DO create mapping between pipedrive options field and safetyculture response answer
 const pipeDriveSafetyCultureOptionMapping = {
     // PIPEDRIVE CUSTOM FIELD KEY : SAFETYCULTURE QUESTIONS ID
-    '66ae80e6e27e7af328cb51c2de5a6c3df2afd04a': '3c696419-8380-434c-85d5-8836487761e9', // Existing Inverter 
+    '66ae80e6e27e7af328cb51c2de5a6c3df2afd04a': '3c696419-8380-434c-85d5-8836487761e9', // Existing Inverter - Make/Model/Size 
     '75b418263a46a2ee1025fc8f87d730219484b56b': '047b6bc5-f478-44d4-bf12-91fc51f560a9', // MPAN
     '47692599a527f65407125f43a1a4fb2f79ad0df6': 'e2e4d156-dc1a-4079-b1f4-826f1ea4efce', // Roof Tile Type 
     '120b9ae729965a5bbce64521e8d100c1b75366a8': '76fb0ffe-3e3d-45d9-9554-78e229ff112e', // Is scaffolding required?
+    '102498b690aae6ccf74ba24764adc239c23a3aae': '1e5da9a2-11aa-4e33-9d48-00ce2880f1ed', // Roof structure type / is it a truss roof?
+    '2784a6bfc692ef55a36ec269500bc196a6aa9f2d': '530917c3-ac73-49d6-b3b0-9b73c75ef3ab', // Pitch / Roof Pitch
+    '1c7c3a1b307f040a89e95dab1cfe5e177e45d51f': '22bd6c9b-5802-4b1e-9b53-12fc3f6d4012', // Azimuth / Roof orientation from south
 
     // PIPEDRIVE CUSTOM FIELD OPTIONS ID : SAFETYCULTURE RESPONSES ID
-    823: '8fadf2e2-4cee-4efe-ae29-3ed4e8ee954c', // 1 SIDE - 1 FLOOR
-    824: '6470c37e-0703-489e-8ce0-0608fbdc5fe1', // 1 SIDE - 2 FLOOR
-    1033: '9fcb11e1-713b-4e54-875f-c47eac114c4b', // 1 SIDE - 3 FLOOR
-    1034: '2576b336-95c7-4b66-b605-fa73414e5fe6', // 2 SIDE - 1 FLOOR
-    1035: '95d5f388-f72c-4026-b678-c137a23e2fb5', // 2 SIDE - 2 FLOOR
-    1036: '98a8954c-0843-46d4-baee-771b302fdc61', // 2 SIDE - 3 FLOOR
-    1037: '07aa0d81-e099-4565-adfc-b7da5508d37d', // 3 SIDE - 1 FLOOR
-    1038: 'dd392ea7-cdb2-4e2a-beb0-6e0b0fe472da', // 3 SIDE - 2 FLOOR
-    1039: '2c6f72bb-7b17-4419-8de3-eb5a83af816e', // 3 SIDE - 3 FLOOR
-    1023: '47f9144e-ae4e-4c8b-8304-abba1f5eaa01', // Concrete
-    1024: '88e5b264-45fe-4f6a-a410-fa7a5fa07372', // Rosemary
-    1025: '055d6483-9e8e-4abe-9289-4ecfd7e7cc1f', // Slate
-    1026: 'be2a5ac6-c9cd-46ec-847f-bf7f8c96c5f3', // Yorkshire stone
-    1027: 'e06d490f-0678-4629-8740-ae6586730744', // Trapezoidal
-    1028: 'f23275eb-6099-414b-ac99-0a0e746ee09e', // Felted
-    1059: '6d554cfa-0e32-4d03-b09c-965b7ea152f6', // Ground Mount
-    1060: '4f77c3ec-0b33-4b24-802e-91e0d5fec5f8', // Flat
-    1029: 'b6f61759-5ea3-4f2d-ac48-4f19ae770271', // Other
+    823: '8fadf2e2-4cee-4efe-ae29-3ed4e8ee954c', // Scaffolding Required - 1 SIDE - 1 FLOOR
+    824: '6470c37e-0703-489e-8ce0-0608fbdc5fe1', // Scaffolding Required - 1 SIDE - 2 FLOOR
+    1033: '9fcb11e1-713b-4e54-875f-c47eac114c4b', // Scaffolding Required - 1 SIDE - 3 FLOOR
+    1034: '2576b336-95c7-4b66-b605-fa73414e5fe6', // Scaffolding Required - 2 SIDE - 1 FLOOR
+    1035: '95d5f388-f72c-4026-b678-c137a23e2fb5', // Scaffolding Required - 2 SIDE - 2 FLOOR
+    1036: '98a8954c-0843-46d4-baee-771b302fdc61', // Scaffolding Required - 2 SIDE - 3 FLOOR
+    1037: '07aa0d81-e099-4565-adfc-b7da5508d37d', // Scaffolding Required - 3 SIDE - 1 FLOOR
+    1038: 'dd392ea7-cdb2-4e2a-beb0-6e0b0fe472da', // Scaffolding Required - 3 SIDE - 2 FLOOR
+    1039: '2c6f72bb-7b17-4419-8de3-eb5a83af816e', // Scaffolding Required - 3 SIDE - 3 FLOOR
+    1023: '47f9144e-ae4e-4c8b-8304-abba1f5eaa01', // Roof Tile Type - Concrete
+    1024: '88e5b264-45fe-4f6a-a410-fa7a5fa07372', // Roof Tile Type - Rosemary
+    1025: '055d6483-9e8e-4abe-9289-4ecfd7e7cc1f', // Roof Tile Type - Slate
+    1026: 'be2a5ac6-c9cd-46ec-847f-bf7f8c96c5f3', // Roof Tile Type - Yorkshire stone
+    1027: 'e06d490f-0678-4629-8740-ae6586730744', // Roof Tile Type - Trapezoidal
+    1028: 'f23275eb-6099-414b-ac99-0a0e746ee09e', // Roof Tile Type - Felted
+    1059: '6d554cfa-0e32-4d03-b09c-965b7ea152f6', // Roof Tile Type - Ground Mount
+    1060: '4f77c3ec-0b33-4b24-802e-91e0d5fec5f8', // Roof Tile Type - Flat
+    1029: 'b6f61759-5ea3-4f2d-ac48-4f19ae770271', // Roof Tile Type - Other
+    1030: '8ffd7a5e-5957-4be8-be49-ea1a46eddc80', // Roof Structure Type - Traditional
+    1031: '60f61b2d-88a3-47a8-8588-463c1066efce', // Roof Structure Type - Trussed
+    1032: 'a29bdcb4-4d38-47ac-8b74-1a9ec1fa2402', // Roof Structure Type - Other
 }
-
-const pipeDriveFieldsToUpdate = {
-    'Existing Inverter - Make/Model/Size': '66ae80e6e27e7af328cb51c2de5a6c3df2afd04a',
-    'MPAN number': '75b418263a46a2ee1025fc8f87d730219484b56b',
-    'Is trenching required?': '2d988809bd18aa3c93a6aa7be58bea337aed59a4',
-    'Scaffolding Required?': '120b9ae729965a5bbce64521e8d100c1b75366a8',
-    'Roof Type': '5969028bbc28e9f087522eab021d261abddf3dcf',
-    'Roof Tile Type': '47692599a527f65407125f43a1a4fb2f79ad0df6',
-    'Site Survey Status': 'e775d10dd3e167c96a1d7cf0a12639ad9cfc1548',
-};
 
 async function createInspectionFrom(dealData, templateName) {
     const customerData = getCustomerDataFrom(dealData)
@@ -209,10 +206,12 @@ async function getInspectionAnswersFrom(dealData) {
         // returns an object of {questionId : answer}
         let answerObject = {};
         for (let i in foundResults) {
-            if (foundResults[i].result.text_answer) {
+            if ('text_answer' in foundResults[i].result) {
                 answerObject[foundResults[i].result.question_id] = foundResults[i].result.text_answer.answer
-            } else {
+            } else if ('list_answer' in foundResults[i].result) {
                 answerObject[foundResults[i].result.question_id] = foundResults[i].result.list_answer.responses
+            } else if ('question_answer' in foundResults[i].result) {
+                answerObject[foundResults[i].result.question_id] = foundResults[i].result.question_answer.responses
             }
         }
         return answerObject
@@ -243,6 +242,8 @@ async function getInspectionSingleAnswerFrom(dealData, question_id) {
         // Return null if empty string
         if ('list_answer' in foundResult.result) {
             return foundResult.result.list_answer.responses;
+        } else if ('question_answer' in foundResult.result) {
+            return foundResult.result.question_answer.responses;
         } else if (foundResult.result.text_answer.answer === "") {
             return null;
         } else {
