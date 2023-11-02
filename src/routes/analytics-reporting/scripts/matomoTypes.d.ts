be49c157d243c6a680230d4ce5971cdb68f64e04
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-02 17:38:22 +0000

    started data collection for summary panel

---
+++ src/routes/analytics-reporting/scripts/matomoTypes.d.ts
@@ -0,0 +1,40 @@
+export interface MatomoAPIOpts {
+    siteID?: number;
+    period?: 'day' | 'week' | 'month' | 'year' | 'range';
+    date?: 'today' | 'yesterday' | 'lastWeek' | 'lastMonth' | 'lastYear' | string;
+    segment?: string;
+    format?: 'xml' | 'json' | 'csv' | 'tsv' | 'html' | 'original';
+    filterLimit?: number;
+    expanded?: '0' | '1';
+    flat?: '0' | '1';
+    additionalOpts?: Array<Array<string>>; // any other params, will jsut be passed through
+}
+
+export interface DataSummary {
+    sessions: number;
+    totalSessionTime: number;
+    avgSessionTime: number;
+    numConsultationsBooked: number;
+    totalConsultationValue: number;
+    numSurveysBooked: number;
+    totalSurveyValue: number;
+    conversionRate: string;
+    sessionValue: number;
+    sessionValuePerMinute: number;
+}
+
+export interface EnergiserSummary {
+    sessions: number,
+    conversionRate: string,
+    unBouncedSessions: number,
+    bouncedSessions: number,
+    avgSessionLength: number,
+    consultationsBooked: number,
+    totalConsultationValue: number,
+    surveysBooked: number,
+    totalSurveyValue: number,
+    preorderNum: number,
+    preorderVal: number,
+    expressNum: number,
+    expressVal: number,
+}
