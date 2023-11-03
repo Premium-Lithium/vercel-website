51e87d401abc2ea47c8a0f1bac09b91242e3e0e7
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-03 12:52:34 +0000

    got data retrieval working

--- src/routes/analytics-reporting/components/summaryReport.svelte
+++ src/routes/analytics-reporting/components/summaryReport.svelte
@@ -1,128 +1,30 @@
 <script lang="ts">
 	import SummaryTable from "./summaryTable.svelte";
     import type { EnergiserSummary, MatomoAPIOpts } from "../scripts/matomoTypes";
+	import { getSummary, siteId } from "../scripts/summaryReportLogic";
 
     const STORE_ID = 3;
     const WEBSITE_ID = 2;
     const DEV_ID = 1;
 
-    enum siteId {
-        STORE = 3,
-        WEBSITE = 2,
-        DEV = 1,
+    let summaryData: EnergiserSummary = {
+        sessions: 0,
+        conversionRate: "0%",
+        unBouncedSessions: 0,
+        bouncedSessions: 0,
+        avgSessionLength: 0,
+        consultationsBooked: 0,
+        totalConsultationValue: 0,
+        surveysBooked: 0,
+        totalSurveyValue: 0,
+        preorderNum: 0,
+        preorderVal: 0,
+        expressNum: 0,
+        expressVal: 0,
     }
 
-    const unbouncedSessionSegment = "interactions>%3D3";
+    
 
-    async function matomoDataCall(method: string, opts?: MatomoAPIOpts) {
-        if (!opts) {
-			opts = {};
-		}
-		let queryData = [
-			['method', method],
-			['idSite', String(opts.siteID || 1)],
-			['period', opts.period || 'day'],
-			['date', opts.date || 'yesterday'],
-			['format', opts.format || 'JSON']
-		];
-
-        // any extra params
-		if (opts.additionalOpts) {
-			queryData.push(...opts.additionalOpts);
-		}
-		const data = await fetch('', {
-			method: 'POST',
-			body: JSON.stringify(queryData),
-			headers: {
-				'content-type': 'application/json'
-			}
-		});
-		const res = await data.json();
-        if (res.result === "error") {
-            return {
-                result: "error",
-                message: res.message,
-            }
-        }
-		return res;
-    }
-
-    async function getSummary(siteId: number) {
-        let summaryData: EnergiserSummary = {
-            sessions: 0,
-            conversionRate: "0%",
-            unBouncedSessions: 0,
-            bouncedSessions: 0,
-            avgSessionLength: 0,
-            consultationsBooked: 0,
-            totalConsultationValue: 0,
-            surveysBooked: 0,
-            totalSurveyValue: 0,
-            preorderNum: 0,
-            preorderVal: 0,
-            expressNum: 0,
-            expressVal: 0,
-        }
-        // get info from metadata API
-        // stats should include bounced sessions
-        siteId = 1
-        const metaData = await matomoDataCall("API.get", {
-            siteID: siteId
-        });
-        // if there is an error, leave at defaults
-        if (metaData.result === "error") {
-            console.log(metaData.message);
-        } else {
-            summaryData.conversionRate = metaData.conversion_rate;
-            summaryData.sessions = metaData.nb_visits;
-            summaryData.avgSessionLength = metaData.avg_time_on_site;
-        }
-        // parse and error check non-bounced sessions
-        const unbouncedMetaData = await matomoDataCall("API.get", {
-            siteID: siteId,
-            additionalOpts: [
-                ["segment", unbouncedSessionSegment]
-            ],
-        });
-        if (unbouncedMetaData.result === "error") {
-            console.log(unbouncedMetaData.message);
-        } else {
-            summaryData.unBouncedSessions = unbouncedMetaData.nb_visits;
-        }
-        summaryData.bouncedSessions = summaryData.sessions = summaryData.unBouncedSessions;
-
-        // get booking data
-        const eventData = await matomoDataCall("Events.getActionFromCategoryId", {
-            additionalOpts: [
-                ["idSubtable", "4"] // successful booking event
-            ]
-        });
-        for (const booking of eventData) {
-            if (booking.label === "Consultation") {
-                summaryData.consultationsBooked = booking.nb_visits;
-                summaryData.totalConsultationValue = booking.sum_event_value;
-            } else if (booking.label === "Survey") {
-                summaryData.surveysBooked = booking.nb_visits;
-                summaryData.totalSurveyValue = booking.sum_event_value;
-            }
-        }
-
-        // get name (plan selection), need to wait till tomorrow to properly test
-        const orderData = await matomoDataCall("Events.getNameFromCategoryId", {
-            additionalOpts: [
-                ["idSubtable", "4"] // successful booking event
-            ]
-        });
-        console.log(orderData)
-        // query events API for order types
-        let preorderNum = 1;
-        let preOrderVal = 4000;
-        let expressNum = 4;
-        let expressVal = 14000;
-
-
-        return 0;
-    }
 
     async function getStoreSummary() {
         // make a call to site 3 (if not testing)
@@ -138,10 +40,11 @@
     
     async function getOverallSummary () {
         // combine store and online summaries
-        const energiserData = getSummary(siteId.WEBSITE)
-        const storeData = getSummary(siteId.STORE)
+       // const energiserData = getSummary(siteId.WEBSITE)
+        //const storeData = getSummary(siteId.STORE)
         const devData = getSummary(siteId.DEV)
     }
+
 </script>
 
 <body>
@@ -165,7 +68,7 @@
         </div>
         <div class=panel>
             <h4>
-                Overall summary
+                Overall summary (dev for now)
             </h4>
             <button on:click={getOverallSummary}>Get overall summary</button>
             <SummaryTable />
