a1497599d087443eecb6ec7ed2f15be2d0a95eef
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-06 13:32:41 +0000

    API endpoint that gets some data

--- src/routes/analytics-reporting/+server.ts
+++ src/routes/analytics-reporting/+server.ts
@@ -6,7 +6,6 @@ import { matomoAPICall } from "./matomoQuery.server";
 export const POST: RequestHandler = async ({ request }) => {
     const params: Array<Array<string>> = await request.json()
     // make a call to matomo API for whatever data is asked for 
-
     const data = await matomoAPICall(params)
     return json(data)
 };
