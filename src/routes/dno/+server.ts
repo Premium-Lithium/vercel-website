e17a7c9ab91154ddf09d9444aadbf7bf4e4802d5
Peter Gillingham <petergillingham80@gmail.com>
2023-11-08 12:10:15 +0000

    input postcode

--- src/routes/dno/+server.ts
+++ src/routes/dno/+server.ts
@@ -1,5 +1,7 @@
-export async function GET() {
-    let res = await fetch("http://www.ssen.co.uk/distributor-results/Index?distributorTerm=YO26+4UP")
+export async function POST ({ request }) {
+    const { postcode } = await request.json()
+    console.log(postcode)
+    let res = await fetch(`http://www.ssen.co.uk/distributor-results/Index?distributorTerm=${postcode}`)
 
     var regex = /<dd class="c-results-list__value">(.*?)<\/dd>/;
     var match = regex.exec(await res.text())?.[0];
