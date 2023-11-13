652079e7047c4a3e5d4ea91a5264296f605a735c
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-13 14:44:52 +0000

    this might deploy?

--- src/routes/analytics-reporting/scheduled-reports/summaryReport.server.ts
+++ src/routes/analytics-reporting/scheduled-reports/summaryReport.server.ts
@@ -2,8 +2,8 @@
 
 import { getSummary } from "./logic/summaryReportLogic.server";
 import { summary } from './recipients.json'
-import nunjucks, { render } from "nunjucks"
-import mjml2html from "mjml"
+// import nunjucks, { render } from "nunjucks"
+// import mjml2html from "mjml"
 
 import type { MatomoAPIOpts } from "../scripts/matomoTypes";
 
@@ -21,65 +21,65 @@ enum Sites {
 // send to relevent people
 
 export async function emailSummaryReport(origin: string, date: MatomoAPIOpts["date"], period: MatomoAPIOpts["period"]) {
-    // use graph API to send an email to everyone on recipients list
+//     // use graph API to send an email to everyone on recipients list
 
-    const template = await (await fetch(origin + "/email-templates/summaryTemplate.mjml")).text()
+//     const template = await (await fetch(origin + "/email-templates/summaryTemplate.mjml")).text()
 
-    // construct email template
-    const { summaryHeader, storeSummary, siteSummary } = await constructSummaryReport(date, period);
+//     // construct email template
+//     const { summaryHeader, storeSummary, siteSummary } = await constructSummaryReport(date, period);
 
-    for (const recipient of summary) {
-        // send email
-        const templateBody = mjml2html(template).html
-        nunjucks.configure({ autoescape: true });
-        const renderedEmail = nunjucks.renderString(templateBody, {
-            name: recipient.name,
-            energiserModeString: "overall energiser performance",
-            date: date,
-            period: period,
-            summaryHeader: summaryHeader,
-            storeReport: storeSummary,
-            siteReport: siteSummary,
-        });
+//     for (const recipient of summary) {
+//         // send email
+//         const templateBody = mjml2html(template).html
+//         nunjucks.configure({ autoescape: true });
+//         const renderedEmail = nunjucks.renderString(templateBody, {
+//             name: recipient.name,
+//             energiserModeString: "overall energiser performance",
+//             date: date,
+//             period: period,
+//             summaryHeader: summaryHeader,
+//             storeReport: storeSummary,
+//             siteReport: siteSummary,
+//         });
         
-        const mailBody = `
-            total: ${summaryHeader.totalRevenue}<br>
-            consultations: ${summaryHeader.consultations}<br>
-            surveys: ${summaryHeader.surveys}<br>
-            preorders: ${summaryHeader.preorders}<br>
-            express orders: ${summaryHeader.expressOrders}<br>
-            <br><br>
-            the summary json blobs:<br>
-            Store summary ${JSON.stringify(storeSummary)}<br><br>
-            Site summary ${JSON.stringify(siteSummary)}<br><br>
-            All the best,
-            Me
-        `
-        const emailData = {
-            sender: "andrew.packer@premiumlithium.com",
-            recipients: [recipient.email],
-            subject: "Test email",
-            mail_body: renderedEmail,
-            content_type: "HTML"
-        }
+//         const mailBody = `
+//             total: ${summaryHeader.totalRevenue}<br>
+//             consultations: ${summaryHeader.consultations}<br>
+//             surveys: ${summaryHeader.surveys}<br>
+//             preorders: ${summaryHeader.preorders}<br>
+//             express orders: ${summaryHeader.expressOrders}<br>
+//             <br><br>
+//             the summary json blobs:<br>
+//             Store summary ${JSON.stringify(storeSummary)}<br><br>
+//             Site summary ${JSON.stringify(siteSummary)}<br><br>
+//             All the best,
+//             Me
+//         `
+//         const emailData = {
+//             sender: "andrew.packer@premiumlithium.com",
+//             recipients: [recipient.email],
+//             subject: "Test email",
+//             mail_body: renderedEmail,
+//             content_type: "HTML"
+//         }
         
-        const options = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(emailData)
-        }
-        const mailEndpoint = origin + "/send-mail"
-        const mailAttempt = await fetch(mailEndpoint, options)
-        if (mailAttempt.status === 200) {
-        } else {
-            console.log(mailAttempt);
-            return mailAttempt
-        }
+//         const options = {
+//             method: "POST",
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(emailData)
+//         }
+//         const mailEndpoint = origin + "/send-mail"
+//         const mailAttempt = await fetch(mailEndpoint, options)
+//         if (mailAttempt.status === 200) {
+//         } else {
+//             console.log(mailAttempt);
+//             return mailAttempt
+//         }
 
-    }
+//     }
 
 
-    return [summaryHeader, storeSummary, siteSummary];
+//     return [summaryHeader, storeSummary, siteSummary];
 
 }
 
