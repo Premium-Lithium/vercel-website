cbb7c81b906e327a1d461b8823c157331e330eec
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-13 14:58:12 +0000

    !!!!!!!!!!!

--- src/routes/analytics-reporting/scheduled-reports/summaryReport.server.ts
+++ src/routes/analytics-reporting/scheduled-reports/summaryReport.server.ts
@@ -3,7 +3,7 @@
 import { getSummary } from "./logic/summaryReportLogic.server";
 import { summary } from './recipients.json'
 
-//import mjml2html from "mjml"
+import mjml2html from 'mjml';
 
 import type { MatomoAPIOpts } from "../scripts/matomoTypes";
 
@@ -30,6 +30,7 @@ export async function emailSummaryReport(origin: string, date: MatomoAPIOpts["da
 
     for (const recipient of summary) {
         // send email
+        const mjml = await mjml2html(template);
         //const templateBody = mjml2html(template).html
         //nunjucks.configure({ autoescape: true });
         const renderedEmail = "<h1>aaaaaaaaaa</h1>"//nunjucks.renderString(templateBody, {
