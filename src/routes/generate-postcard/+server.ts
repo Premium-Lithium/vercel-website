c8df84a1bea0375639f400ea16b012fcd2cce7c9
lbowes <33807016+lbowes@users.noreply.github.com>
2023-11-14 12:16:21 +0000

    Create new endpoint skeleton for generating a postcard (two jpgs for front/back and an address)

---
+++ src/routes/generate-postcard/+server.ts
@@ -0,0 +1,47 @@
+// todo: endpoint responsible for creating a "postcard" object, containing:
+// * front image (jpg)
+// * back image (jpg)
+// * address
+
+import { json } from '@sveltejs/kit';
+import validate from '$lib/validation-utils.js';
+
+
+const schema = {
+    type: "object",
+    required: [ "customerId" ],
+    properties: {
+        customerId: {
+            type: "string",
+            format: "uuid",
+            errorMessage: "some error processing customer id"
+        },
+    }
+}
+
+
+export async function POST({ request }) {
+    if (!request.body)
+        return json({ message: "No request body found" }, { status: 400 });
+
+    const requestData = await request.json();
+    const validationErrors = validate(requestData, schema);
+
+    if (validationErrors.length) {
+        const message = validationErrors.join(", ");
+        return json({ message: `${message}` }, { status: 400 });
+    }
+
+    try {
+        // todo: generate postcard images here
+
+        return json(
+            { message: true },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        console.error("Error generating postcard:", error);
+        return json({ message: "Error generating postcard" }, { status: 500 });
+    }
+}
