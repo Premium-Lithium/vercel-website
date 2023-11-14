dcb5e9ac3cb00e6e554cde12985e84c594cc4fd5
lbowes <33807016+lbowes@users.noreply.github.com>
2023-11-14 15:49:08 +0000

    Get it working

--- src/routes/generate-postcard/+server.ts
+++ src/routes/generate-postcard/+server.ts
@@ -6,6 +6,7 @@
 import { json } from '@sveltejs/kit';
 import validate from '$lib/validation-utils.js';
 import { generatePostcardFor } from './logic';
+import { Buffer } from 'buffer';
 
 
 const schema = {
@@ -37,14 +38,67 @@ export async function POST({ request }) {
         const customerId = requestData.customerId;
         console.log("Generating postcard for customer:", customerId);
 
-        const postcard = generatePostcardFor(customerId);
+        // 1. Create the actual content
+        const postcard = await generatePostcardFor(customerId);
 
-        // todo: check that the postcard was generated correctly
+        // 2. Convert the content into a form that can be sent to Stannp
+        console.log("converting to base64");
+        const streamToBuffer = async (stream) => {
+            const chunks = [];
+            for await (let chunk of stream) {
+                chunks.push(chunk instanceof Buffer ? chunk : Buffer.from(chunk));
+            }
+            return Buffer.concat(chunks);
+        };
 
-        return json(
-            { message: `Successfully generated postcard content for customer id ${customerId}` },
-            { status: 200 }
-        );
+        const blobToBuffer = async (blob) => {
+            const stream = blob.stream();
+            return await streamToBuffer(stream);
+        };
+
+        // Usage
+        const frontBuffer = await blobToBuffer(postcard.front);
+        const frontBase64 = frontBuffer.toString('base64');
+
+        const backBuffer = await blobToBuffer(postcard.back);
+        const backBase64 = backBuffer.toString('base64');
+
+        // 3. Send the data to Stannp
+        const stannpPayload = {
+            test: true,
+            size: 'A5',
+            front: frontBase64,
+            back: backBase64,
+            recipient: {
+                title: 'Mr',
+                firstname: 'Lewis',
+                lastname: 'Bowes',
+                address1: '5 Whittam Road, Whalley',
+                address2: 'Lancashire',
+                city: 'Clitheroe',
+                postcode: 'BB7 9SB',
+                country: 'GB'
+            }
+        }
+
+        // https://www.stannp.com/uk/direct-mail-api/postcards?lang=python
+        const API_KEY = "f2186ada5b497f22e32752f5";
+
+        // make request to stannp
+        const response = await fetch("https://dash.stannp.com/api/v1/postcards/create?api_key=" + API_KEY, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(stannpPayload)
+        });
+
+        return response;
+
+        // return json(
+        //     { message: `Successfully generated postcard content for customer id ${customerId}` },
+        //     { status: 200 }
+        // );
 
     } catch (error) {
         console.error("Error generating postcard:", error);
--- src/routes/generate-postcard/logic.ts
+++ src/routes/generate-postcard/logic.ts
@@ -2,42 +2,45 @@ import { supabase } from '$lib/supabase.ts';
 
 
 export async function generatePostcardFor(customerId: string) {
-    const template_png = downloadTemplate_png();
+    const template_png = await downloadTemplate_png();
     if(template_png === null) {
         // todo: handle condition where the template could not be fetched
     }
 
-    const propertyImage_png = getPropertyImage_png(customerId);
+    const propertyImage_png = await getPropertyImage_png(customerId);
     if(propertyImage_png === null) {
         // todo: handle case where we couldn't fetch the property image
     }
 
-    // At this point we've got the template image for the front of the
-    // postcard along with the image of the customer's property
     const postcard_png = combine(template_png, propertyImage_png);
 
     // 2. Use the customerId to get the customer's address
     // todo
 
     return {
-        postcard_png: postcard_png
+        front: postcard_png,
+        back: postcard_png
     }
 }
 
 
 async function downloadTemplate_png() {
-    const response = await supabase.storage.from('postcard-resources').createSignedUrl('postcard-template.png', 60);
+    console.log("Downloading template png");
 
-    if(response.error)
-        // todo
-        return null;
+    // const response = await supabase.storage.from('postcard-resources').createSignedUrl('postcard-template.png', 60);
+    const response = await supabase.storage.from('postcard-resources').download('postcard-template.png');
 
     const template_png = response.data;
+    if(template_png === null) {
+        console.log("Template was null: ", response.error);
+    }
+
     return template_png;
 }
 
 
 async function getPropertyImage_png(customerId: string) {
+    console.log("Fetching property image");
     let propertyImage = null;
 
     // todo:
@@ -47,8 +50,11 @@ async function getPropertyImage_png(customerId: string) {
 
 
 function combine(template_png: any, propertyImage_png: any) {
+    console.log("Merging results");
     // Returns a png image containing the result of adding the property image to the template
     let combined = null;
+
+    combined = template_png;
 
     // todo:
 
