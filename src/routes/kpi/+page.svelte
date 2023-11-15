1c68853b08bddf24a00cc1ea438258b7d5097eaf
andrew-packer-premiumlithium <andrew.packer@premiumlithium.com>
2023-11-15 12:57:01 +0000

    pipedrive module isn't working locally

--- src/routes/kpi/+page.svelte
+++ src/routes/kpi/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { GeneralKPI as KPIBody } from './KPITypes';
+	import { getMatomoAnalytics } from './scripts/matomoAnalytics';
+	import { getPipedriveAnalytics } from './scripts/pipedriveAnalytics';
+
+    enum siteIDs {
+        ENERGISER_WEB = 2,
+        SHOPIFY = 4,
+    }
 
 	// get all the KPIs Robert wants and display them in a nice dashboard
 	// then re-use the code to turn the dashboard into an email
@@ -21,18 +28,29 @@
     
     projected total lead value: ???
         consultation and survey value can come from Matomo where there is a value
-*/
+    */
 
 	// initialise KPIs
-	let generalKPIs: Array<KPIBody> = [];
-
+	let generalKPIs: Array<KPIBody>;
+    
 	let leadKPIs: Array<KPIBody> = [];
 
-	onMount(() => {
-		// load in the values for the previous day
-	});
-</script>
+        let projectedLeadValue: number; // I have no idea how to track this
 
+	onMount(async () => {
+		// load in the values for the previous day
+        let day = "yesterday";
+        let period = "day"
+        generalKPIs = await getMatomoAnalytics(day, period, siteIDs.SHOPIFY);
+
+
+	});
+
+    async function getData() {
+        console.log(getPipedriveAnalytics())
+    }
+</script>
+<button on:click={getData}>Get data</button>
 
 
 <style>
---
+++ src/routes/kpi/scripts/matomoAnalytics.ts
@@ -0,0 +1,44 @@
+// traffic: API.get / nb_visits
+// total page views: API.get / nb_pageviews
+// average page views per visitor: derived
+// average time on site: API.get / avg_time_on_site
+
+import type { GeneralKPI } from "../KPITypes"
+
+export async function getMatomoAnalytics(day: string, period: string, site: number) {
+    // call to matomo API, needs various matomo utils I built
+    
+
+    // dummy response, pending merge of other PR
+    const data = {
+        nb_visits: 20875,
+        nb_pageviews: 35489,
+        avg_time_on_site: 238,
+    }
+
+    const secondsToMinuteString = function(seconds: number) {
+        const minutes = Math.floor(seconds / 60).toString();
+        const remainder = (seconds % 60).toString();
+        return minutes + " minutes " + remainder + " seconds"
+    }
+    const matomoKPIs: Array<GeneralKPI> = [
+        {
+            name: "visits",
+            value: data.nb_visits.toLocaleString(),
+        },
+        {
+            name: "page views",
+            value: data.nb_pageviews.toLocaleString(),
+        },
+        {
+            name: "page views per visitor",
+            value: (data.nb_pageviews / data.nb_visits).toLocaleString("en-GB", {maximumFractionDigits: 2})
+        },
+        {
+            name: "time spent per visitor",
+            value: secondsToMinuteString(data.avg_time_on_site)
+        }
+    ]
+    // return the KPIs in proper format
+    return matomoKPIs;
+}
