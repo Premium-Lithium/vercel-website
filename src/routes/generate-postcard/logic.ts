a3fc94cb8ee51bec52846a00c2cbf2198490e944
lbowes <33807016+lbowes@users.noreply.github.com>
2023-11-16 12:07:03 +0000

    Shift to .svg based approach

--- src/routes/generate-postcard/logic.ts
+++ src/routes/generate-postcard/logic.ts
@@ -3,12 +3,14 @@ import sharp from 'sharp';
 import type { Sharp } from 'sharp';
 import type { Postcard, PostcardRecipient } from './types.ts';
 import { Buffer } from 'buffer';
+import { createSVGWindow } from 'svgdom';
+import { SVG, registerWindow, namespaces } from '@svgdotjs/svg.js';
 
 
 export async function generatePostcardFor(customerId: string): Promise<Postcard> {
     // Front of postcard
-    const frontTemplate_png = await getTemplate_png('postcard-front.png');
-    if(frontTemplate_png === null) {
+    const frontTemplate_svg = await getSvg('postcard-front-new.svg');
+    if(frontTemplate_svg === null) {
         // todo: handle condition where the template could not be fetched
     }
 
@@ -17,10 +19,10 @@ export async function generatePostcardFor(customerId: string): Promise<Postcard>
         // todo: handle case where we couldn't fetch the property image
     }
 
-    const front_png = await createFront_png(frontTemplate_png, propertyImage_png);
+    const front_png = await createFront_png(frontTemplate_svg, propertyImage_png);
 
     // Back of postcard
-    const backTemplate_png = await getTemplate_png('postcard-back.png');
+    const backTemplate_png = await getImage('postcard-back.png');
     if(backTemplate_png === null) {
         // todo: handle condition where the template could not be fetched
     }
@@ -35,9 +37,18 @@ export async function generatePostcardFor(customerId: string): Promise<Postcard>
 }
 
 
-async function getTemplate_png(pngName: string): Promise<Buffer> {
+async function getSvg(svgName: string): string {
+    const response: any = await supabase.storage.from('postcard-resources').download(svgName); // private
+    const svgData: Blob = response.data;
+    const svgString: string = await svgData.text();
+
+    return svgString;
+}
+
+
+async function getImage(imageName: string): Promise<Buffer> {
     // const response = await supabase.storage.from('postcard-resources').createSignedUrl('postcard-template.png', 60); // public
-    const response: any = await supabase.storage.from('postcard-resources').download(pngName); // private
+    const response: any = await supabase.storage.from('postcard-resources').download(imageName); // private
 
     // todo: handle case where the image has not been found
 
@@ -74,42 +85,46 @@ export function getCustomerDetailsFor(customerId: string): PostcardRecipient {
 }
 
 
-
-
 async function getPropertyImage_png(customerId: string): Promise<Buffer> {
     console.log("Fetching property image");
 
     // todo: use the open solar api to get the image of the property using the customer's id
-    const propertyImage = await getTemplate_png('pl-hq.png');
+    const propertyImage = await getImage('pl-hq.png');
 
     return propertyImage;
 }
 
 
-async function createFront_png(template_png: Buffer, propertyImage_png: Buffer): Promise<Buffer> {
-    try {
-        // const propertyImage: Sharp = sharp(propertyImage_png).rotate(-90);
-        const templateImage: Sharp = sharp(template_png);
+async function createFront_png(template_svg: string, propertyImage_png: Buffer): Promise<Buffer> {
+    // 1. Convert the background image to a base64 image
+    const propertyImage_base64 = propertyImage_png.toString('base64');
 
-        const metadata = await templateImage.metadata();
-        const templateWidth = metadata.width;
-        const templateHeight = metadata.height;
+    // 2. Create a new SVG
+    const window = createSVGWindow();
+    const document = window.document;
+    registerWindow(window, document);
 
-        // Now resize propertyImage to match templateImage's dimensions
-        const propertyImage: Sharp = sharp(propertyImage_png)
-            .resize(templateHeight)
-            .rotate(260);
+    // 3. Load the SVG document with the template
+    const canvas = SVG(template_svg);
 
-        const combined: Buffer = await templateImage.composite([{
-            input: await propertyImage.toBuffer(),
-            blend: 'dest-over'
-        }]).toBuffer();
+    // 4. Add the property image to the background of the SVG
+    const backgroundImage = canvas.image(`data:image/png;base64, ${propertyImage_base64}`);
+    backgroundImage.back();
 
-        return combined;
-    } catch (error) {
-        console.error('Error during image merging:', error);
-        return null;
-    }
+    // 5. Move the property image to the correct location in the image
+    backgroundImage.size('80%', '100%');
+    backgroundImage.move('20%', '0%');
+
+    // SVG.js bug fix: See https://github.com/svgdotjs/svg.js/issues/1285
+    canvas.attr('xmlns:svgjs', namespaces.svgjs);
+
+    // 6. Export the result
+    const outputSvg = canvas.svg();
+    const outputPng = await sharp(Buffer.from(outputSvg, 'utf-8'), {
+        density: 300 // 300 dpi is considered "print quality"
+    }).png().toBuffer();
+
+    return outputPng
 }
 
 
