75e3a917aa6921cfdc8b0ffdc512ed17c9c2bda0
Peter Gillingham <petergillingham80@gmail.com>
2023-11-28 11:50:39 +0000

    init

---
+++ src/routes/document-builder-platform/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+    let alertMessage: string
+    
+    async function generateDNO() {
+
+    }
+
+
+</script>
+
+<div class="dno-panel">
+	{#if alertMessage}
+		<div class="alert-message">
+			<span>{alertMessage}</span>
+		</div>
+	{/if}
+	<button disabled={false} class="link-btn" on:click={generateDNO}
+		>Generate DNO Application</button
+	>
+</div>
+
+<style>
+    .dno-panel {
+		padding: 15px;
+		/* border: 1px solid grey; */
+		display: grid;
+	}
+
+	.link-btn {
+		background-color: #c6c6c6;
+		color: black;
+		text-align: center;
+		padding: 10px;
+		margin: 10px 0;
+		border-radius: 10px;
+		border: 1px solid black;
+		cursor: pointer;
+		font-weight: bold;
+		font-size: 16px;
+		&:hover {
+			background-color: #9f9f9f;
+		}
+		&:disabled {
+			cursor: default;
+			background-color: rgb(101, 101, 101);
+		}
+	}
+</style>
