6b58ed01fd640f21c0442e3c43ce4bfd5ada55a0
Peter Gillingham <petergillingham80@gmail.com>
2023-11-30 18:15:39 +0000

    migration

--- src/routes/report-lookup/+server.ts
+++ src/routes/report-lookup/+server.ts
@@ -19,6 +19,7 @@ export async function POST({ request }) {
         if (error)
             return new Response(JSON.stringify(error))
         let currAnalytics = data
+        console.log(data)
         if (currAnalytics === null) {
             currAnalytics = {
                 scannedQrCode: false,
