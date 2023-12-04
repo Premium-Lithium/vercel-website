ae860d047b76fb7844ca652b61bb68c30c52f327
lbowes <33807016+lbowes@users.noreply.github.com>
2023-12-04 11:04:56 +0000

    Remove code that's been abstracted away behind `Campaign` class

--- scripts/campaign-runner/run_campaign.py
+++ scripts/campaign-runner/run_campaign.py
@@ -7,10 +7,7 @@ Options:
 """
 
 from docopt import docopt
-import supabase
-import yaml
 
-# from model import Campaign
 from campaign import Campaign
 
 
@@ -19,29 +16,6 @@ def main(args):
 
     campaign = Campaign(name)
     campaign.run()
-    input()
-
-    campaign_data = campaign["campaign_specific_schema"]
-    tasks = get_task_execution_order_to_calc(campaign_data)
-    run(tasks)
-
-
-
-def run(task_names: list[str]):
-    for name in task_names:
-        task = task_mapping[name]
-
-        args = {}
-        results = task(**args)
-
-        # todo: store intermediate results in database (in the "campaign specific schema" json blob)
-
-        store_results_in_database(results, name)
-
-
-def store_results_in_database(results: any, key: str):
-    print(f"Saving results of calculating {key} to database: {results}")
-    # todo: use supabase sdk to add `results` to a new key in the campaign_specific_schema json blob, named `key`
 
 
 if __name__ == "__main__":
