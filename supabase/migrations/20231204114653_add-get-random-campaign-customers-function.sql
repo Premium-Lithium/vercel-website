756fb237d34113450093e2e10510139366d286b4
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-04 11:46:56 +0000

    add migration

---
+++ supabase/migrations/20231204114653_add-get-random-campaign-customers-function.sql
@@ -0,0 +1,30 @@
+alter table "public"."campaign_customers" drop constraint "campaign_customers_pkey";
+
+drop index if exists "public"."campaign_customers_pkey";
+
+alter table "public"."campaign_customers" alter column "address" set not null;
+
+alter table "public"."existing-solar-properties" drop column "analytics";
+
+CREATE UNIQUE INDEX campaign_customers_pkey ON public.campaign_customers USING btree (campaign_id, address);
+
+alter table "public"."campaign_customers" add constraint "campaign_customers_pkey" PRIMARY KEY using index "campaign_customers_pkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_random_campaign_customers(numrows integer, campaignid text)
+ RETURNS SETOF campaign_customers
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+        SELECT *
+        FROM campaign_customers
+        WHERE campaign_id = campaignid
+        ORDER BY random()
+        LIMIT numrows;
+END;
+$function$
+;
+
+
