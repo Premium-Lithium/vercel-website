27bfdec782b7b83460fd8c73489649c0d7c21403
Joe-Lonsdale <joe.ai.dev@gmail.com>
2023-12-06 17:25:49 +0000

    Add functionality to upload a finished kml file to supabase

--- src/routes/battery-proposals/+page.svelte
+++ src/routes/battery-proposals/+page.svelte
@@ -2,13 +2,14 @@
 	import { browser } from '$app/environment'
 	import { page } from '$app/stores'
 	import Auth from '$lib/components/Auth.svelte'
-	import GoogleMap from '$lib/components/GoogleMap.svelte'
 	import { supabase } from '$lib/supabase'
 	import type { Loader } from '@googlemaps/js-api-loader'
 	import type { UUID } from 'crypto'
 	import { onMount } from 'svelte'
 	import { generateUUID } from 'three/src/math/MathUtils'
 	import { default as template } from './Template.kml'
+	import type { RealtimeChannel } from '@supabase/supabase-js'
+	import Accordian from '$lib/components/Accordian.svelte'
 
 	let supabaseAuth: Object
 	let activeArea: Array<any> | undefined = undefined
@@ -23,21 +24,24 @@
 	let campaignAreas: Array<any>
 	let uniqueIdentifier: UUID
 	let loadedWorkerDetails: boolean = false
+	let broadcastRoom: RealtimeChannel
+	let numRegionsCompleted: number = 0
+	let kmlFileInput
+	let uploadedFile: File
+	let uploadedFilesFromFileInput: FileList
 
 	onMount(async () => {
-		supabase
-			.channel('room1')
-			.on('broadcast', { event: 'claim_project' }, async (payload) => {
-				console.log(payload)
-				console.log('test')
-				campaignAreas = await loadCampaignAreas(campaign)
-			})
-			.subscribe()
 		campaignAreas = await loadCampaignAreas(campaign)
 	})
 
 	$: if (supabaseAuth) {
 		uniqueIdentifier = supabaseAuth.user.id
+		broadcastRoom = supabase.channel('room1')
+		broadcastRoom
+			.on('broadcast', { event: 'claim_project' }, async (payload) => {
+				campaignAreas = await loadCampaignAreas(campaign)
+			})
+			.subscribe()
 		loadWorkerDetails().then(() => {
 			loadedWorkerDetails = true
 		})
@@ -57,6 +61,7 @@
 			if (workerData[0]['assigned_region']) {
 				activeArea = workerData[0]['assigned_region'].area
 			}
+			numRegionsCompleted = workerData[0]['completed_regions'].length
 		}
 	}
 
@@ -73,15 +78,25 @@
 	}
 
 	async function completeRegion() {
+		if (uploadedFile) {
+			if (!(await uploadFile(uploadedFile))) return
+		}
 		let { data: selectData, error: selectError } = await supabase
 			.from('battery_turk_workers')
-			.select('assigned_region')
+			.select('*')
 			.eq('worker_id', uniqueIdentifier)
 
-		let regionToUpload = { ...selectData[0], 'time_finished': new Date(Date.now()).toISOString() }
+		let regionToUpload = {
+			...selectData[0]['assigned_region'],
+			'time_finished': new Date(Date.now()).toISOString()
+		}
+		numRegionsCompleted = selectData[0]['completed_regions'].length + 1
 		const { data: uploadData, error: uploadError } = await supabase
 			.from('battery_turk_workers')
-			.update({ 'assigned_region': null, 'completed_regions': [regionToUpload] })
+			.update({
+				'assigned_region': null,
+				'completed_regions': [...selectData[0]['completed_regions'], regionToUpload]
+			})
 			.eq('worker_id', uniqueIdentifier)
 
 		console.log('Worker table updated', uploadData, uploadError)
@@ -97,25 +112,25 @@
 			}
 			return x
 		})
-		console.log(masterDataToUpload)
-
 		const { data: masterUploadData, error: masterUploadError } = await supabase
 			.from('campaign_master')
 			.update({ 'area': masterDataToUpload })
 			.eq('campaign_id', campaign)
 		console.log('Master table updated', masterUploadData, masterUploadError)
 		activeArea = null
-		supabase.channel('room1').send({
+		broadcastRoom.send({
 			type: 'broadcast',
 			event: 'claim_project',
 			payload: { message: 'claimed this project' }
 		})
+		uploadedFile = undefined
+		uploadedFilesFromFileInput = undefined
 	}
 
 	async function saveKml(region) {
 		let res = await fetch(template)
 		let kml = await res.text()
-		let uuid = generateUUID().split('-').at(-1)
+		let uuid = `${uniqueIdentifier}-region-${numRegionsCompleted}`
 		kml = kml
 			.replace(
 				'COORDINATE-LIST',
@@ -134,7 +149,7 @@
 		const url = URL.createObjectURL(blob)
 		const a = document.createElement('a')
 		a.href = url
-		a.download = `region-${uuid}.kml`
+		a.download = `${uuid}.kml`
 		document.body.appendChild(a)
 		a.click()
 
@@ -154,12 +169,59 @@
 			})
 			.eq('worker_id', uniqueIdentifier)
 		activeArea = campaignAreas[0].area
+		await saveKml(activeArea)
+	}
+
+	async function uploadFile(file) {
+		let successful = false
+		try {
+			const { data, error } = await supabase.storage
+				.from('worker-kml-upload')
+				.upload(`${campaign}/${uniqueIdentifier}/${file.name}`, file, { upsert: true })
+			successful = !error
+			console.log(error)
+		} catch (error) {
+			console.error('Error uploading file:', error.message)
+		}
+		return successful
+	}
+
+	function handleDragOver(event) {
+		event.preventDefault()
+		event.currentTarget.classList.add('drag-over')
+	}
+
+	function handleDragEnter(event) {
+		event.preventDefault()
+	}
+
+	function handleDragLeave(event) {
+		event.currentTarget.classList.remove('drag-over')
+	}
+
+	async function handleDrop(event) {
+		uploadedFile = undefined
+		event.preventDefault()
+		event.currentTarget?.classList.remove('drag-over')
+		const files = event.dataTransfer.files
+		if (files.length > 0 && files[0].name.endsWith('.kml')) {
+			kmlFileInput.files = files
+			uploadedFile = files[0]
+		}
+	}
+
+	async function handleUpload(event) {
+		uploadedFile = undefined
+		const files = event.srcElement.files
+		if (files.length > 0 && files[0].name.endsWith('.kml')) {
+			uploadedFile = files[0]
+		}
 	}
 </script>
 
 {#if browser}
 	{#if !supabaseAuth}
-		<Auth redirectUrl="battery-proposals" bind:supabaseAuth />
+		<Auth redirectUrl={`battery-proposals/?campaign-id=${campaign}`} bind:supabaseAuth />
 	{:else if !loadedWorkerDetails}
 		<p>Loading...</p>
 	{:else}
@@ -172,7 +234,33 @@
 							saveKml(activeArea)
 						}}>Download KML</button
 					>
-					<button class="complete-region-button" on:click={completeRegion}>Complete Area</button>
+					<button
+						class="complete-region-button"
+						class:disabled={!uploadedFile ? true : false}
+						on:click={completeRegion}>Complete Area</button
+					>
+					<div
+						class="drop-zone"
+						on:drop={handleDrop}
+						on:dragover={handleDragOver}
+						on:dragenter={handleDragEnter}
+						on:dragleave={handleDragLeave}
+					>
+						{#if uploadedFile}
+							<p>Uploaded: {uploadedFile.name}</p>
+						{:else}
+							<p>Drag and drop a file here, or click to select a file</p>
+						{/if}
+						<input
+							type="file"
+							id="fileInput"
+							accept=".kml"
+							style="width: 100%; height: 100%;"
+							bind:files={uploadedFilesFromFileInput}
+							on:change={handleUpload}
+							bind:this={kmlFileInput}
+						/>
+					</div>
 				</div>
 			{:else}
 				<h3>No region currently selected</h3>
@@ -201,6 +289,19 @@
 		overflow: hidden;
 	}
 
+	.drop-zone {
+		border: 2px dashed #ccc;
+		border-radius: 10px;
+		padding: 20px;
+		text-align: center;
+		cursor: pointer;
+		user-select: none;
+	}
+
+	.drop-zone.drag-over {
+		border-color: #000;
+	}
+
 	.button-container {
 		height: fit-content;
 		width: fit-content;
@@ -214,6 +315,7 @@
 		background-color: #eaeaea;
 		border-radius: 8px;
 		color: black;
+		user-select: none;
 
 		cursor: pointer;
 		transition: background-color 0.3s ease;
@@ -231,6 +333,8 @@
 
 	.button-container button.disabled {
 		background-color: #ccc;
+		border: 2px solid #ccc;
+		pointer-events: none;
 	}
 
 	.button-container button.negative {
