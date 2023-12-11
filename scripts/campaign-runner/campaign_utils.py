fbd9d1ed1c34d5aecb0d8ec74182a0a5a412fea3
lbowes <33807016+lbowes@users.noreply.github.com>
2023-12-11 09:47:42 +0000

    Don't differentiate between data that's common between vs specific to campaigns

--- scripts/campaign-runner/campaign_utils.py
+++ scripts/campaign-runner/campaign_utils.py
@@ -13,17 +13,25 @@ def getter_func_name(data_name: str) -> str:
     return f"get_{data_name}"
 
 
+def validate_arguments(required_task_name, required_params):
+    existing_args = list(signature(existing_tasks[required_task_name]).parameters.keys())
+
+    if not all(param in existing_args for param in required_params):
+        raise ValueError(f"Function {required_task_name} has incorrect arguments:\nExpected: {required_params}\nFound: {existing_args}")
+
+
 # Iterate through each of the functions we need to implement
 for name, required_params in data_dependencies.items():
     required_task_name = getter_func_name(name)
 
-    # If we can't find the required task, then tell the user it needs to be implemented
-    if required_task_name not in existing_tasks.keys():
-        raise ValueError(f"Could not find function named `{required_task_name}`")
+    # todo: items of data that don't have dependendcies should exist in the customer record (they don't need to be calculated)
+    if required_params:
+        if required_task_name not in existing_tasks.keys():
+            raise ValueError(f"Could not find function named `{required_task_name}`")
 
-    # We've found the task, check it has the correct arguments
-    else:
-        existing_args = list(signature(existing_tasks[required_task_name]).parameters.keys())
+        validate_arguments(required_task_name, required_params)
 
-        if not all(param in existing_args for param in required_params):
-            raise ValueError(f"Function {required_task_name} has incorrect arguments:\nExpected: {required_params}\nFound: {existing_args}")
+    # existing_args = list(signature(existing_tasks[required_task_name]).parameters.keys())
+
+    # if not all(param in existing_args for param in required_params):
+    #     raise ValueError(f"Function {required_task_name} has incorrect arguments:\nExpected: {required_params}\nFound: {existing_args}")
