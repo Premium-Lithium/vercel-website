dbc78bcb7ae589480181ac25ea4ada6612bb64d1
lbowes <33807016+lbowes@users.noreply.github.com>
2023-12-11 09:45:36 +0000

    Fix sequential database updating

--- scripts/campaign-runner/campaign.py
+++ scripts/campaign-runner/campaign.py
@@ -9,6 +9,8 @@ from data_deps import data_dependencies
 
 from supabase import create_client, Client
 
+import matplotlib.pyplot as plt
+
 
 # todo: add production supabase url and key
 url: str = "http://localhost:54321" # url: str = os.environ.get("SUPABASE_URL")
@@ -22,6 +24,7 @@ class Campaign:
         # todo: check if this succeeds
         definition = self._load_campaign_definition_for(name)
 
+        self._name = name
         self._outputs = definition["campaign_specific_schema"]  # e.g "solarFlyer"
         self._id = definition["campaign_id"]
 
@@ -44,56 +47,87 @@ class Campaign:
         self._fetch_data_in(data_ordering)
 
 
-    # def _generate_function_template_for(self, func_name: str, args: list[str]):
-    #     arg_string = ', '.join([f"{arg}=None" for arg in args]) + ', ' if args else ''
+    def _calc_data_fetch_order_to_calc_outputs(self):
+        dependency_graph = self._create_data_dependency_graph()
 
-    #     decorator = f"@campaign_task(name=\"{func_name}\")"
-    #     definition = f"def get_{func_name}({arg_string}**kwargs):"
-    #     body = f"{func_name} = \"todo\""
+        # plt.figure(figsize=(12, 8))
+        # nx.draw(dependency_graph, with_labels=True, node_color='lightblue',
+        #         node_size=2000, font_size=10, font_weight='bold', arrowstyle='->', arrowsize=20)
+        # plt.title("Graph Visualization")
+        # plt.show()
+        # input()
 
-    #     return f"{decorator}\n\t{definition}\n\t\t{body}\n\t\t# implementation here\n\t\treturn {func_name}"
+        # todo: implement this function
+        # if not path_from_inputs_to_outputs(dependency_graph):
+        #     raise ValueError("Cannot calculate outputs, no path from inputs to outputs.")
+
+        # required_inputs = self._get_required_inputs_from(dependency_graph)
+        ordered_required_inputs = self._calc_fetch_order_for(dependency_graph)
+
+        return ordered_required_inputs
+
+
+    def _create_data_dependency_graph(self) -> nx.DiGraph:
+        dependency_graph = nx.DiGraph()
+        visited = set()
+
+        def add_dependencies(node):
+            if node not in visited:
+                visited.add(node)
+                dependencies = data_dependencies.get(node, [])
+                for dep in dependencies:
+                    dependency_graph.add_edge(dep, node)
+                    add_dependencies(dep)
+
+        for output in self._outputs:
+            add_dependencies(output)
+
+        return dependency_graph
+
+
+    def _calc_fetch_order_for(self, dependency_graph):
+        # subG = dependency_graph.subgraph(required_inputs)
+
+        try:
+            return list(nx.topological_sort(dependency_graph))
+        except nx.NetworkXUnfeasible:
+            raise ValueError("Data dependencies form a cycle, cannot determine execution order!")
 
 
     def _fetch_data_in(self, data_ordering: list[str]):
-        # Get all the customers associated with this campaign
-        campaign_items = supabase.table('campaign_customers').select("*").eq('campaign_id', self._id).execute()
+        campaign_data_table = supabase.table('campaign_customers')
+
+        campaign_items = campaign_data_table.select("*").eq('campaign_id', self._id).execute()
         campaign_data = campaign_items.data
 
         if not campaign_data:
-            raise ValueError(f"Could not fetch campaign items for campaign {self._id}")
+            raise ValueError(f"Could not fetch campaign items for campaign '{self._name}'")
 
-        # For each customer record that we have
         for customer in campaign_data:
+            campaign_specific_data = customer['campaign_specific_data']
 
-            # For each of the tasks that we want to run
             for data_name in data_ordering:
-                task = getattr(tasks, getter_func_name(data_name))
-
-                # Work out what the task requires
                 required_arg_names = data_dependencies[data_name]
-                print(f"Calculating {data_name}{' which requires ' + ', '.join([arg for arg in required_arg_names]) if required_arg_names else ''}")
 
-                args = self._extract_data_from(customer, required_arg_names)
+                data = {}
 
-                # Call the task with the required arguments
-                results = task(**args)
+                if required_arg_names:
+                    args = self._extract_data_from(campaign_specific_data, required_arg_names)
+                    task = getattr(tasks, getter_func_name(data_name))
+                    data = task(**args)
+                else:
+                    data = customer.get(data_name, None)
 
-                # todo: store intermediate results in database (in the "campaign specific schema" json blob)
+                campaign_specific_data = self._add_results_to(campaign_specific_data, data, data_name)
 
-                # store_results_in_database(results, name)
+            self._update_campaign_specific_data_in(campaign_data_table, customer["customer_id"], campaign_specific_data)
 
 
-    def _extract_data_from(self, customer_db_row: dict[str, any], data: list[str]) -> dict[str, any]:
-        # Some items of data are common to all campaigns e.g address, others are stored in campaign specific data
-        campaign_independent_data = [ "address" ]
-
+    def _extract_data_from(self, campaign_specific_data: dict[str, any], data: list[str]) -> dict[str, any]:
         args = {}
 
         for data_name in data:
-            if data_name in campaign_independent_data:
-                value = customer_db_row.get(data_name, None)
-            else:
-                value = customer_db_row['campaign_specific_data'].get(data_name, None)
+            value = campaign_specific_data.get(data_name, None)
 
             if value is None:
                 raise ValueError(f"Could not find data '{data_name}' in customer record")
@@ -103,70 +137,18 @@ class Campaign:
         return args
 
 
-    def _store_results_in_database(self, results: any, key: str):
-        print(f"Saving results of calculating {key} to database: {results}")
-        # todo: use supabase sdk to add `results` to a new key in the campaign_specific_schema json blob, named `key`
+    def _add_results_to(self, campaign_specific_data, results: dict[any, any], name: str) -> dict[str, any]:
+        if campaign_specific_data:
+            campaign_specific_data[name] = results
+        else:
+            campaign_specific_data = { name: results }
+
+        return campaign_specific_data
 
 
-    def _calc_data_fetch_order_to_calc_outputs(self):
-        dependency_graph = self._create_data_dependency_graph()
-        required_inputs = self._get_required_inputs_from(dependency_graph)
-        ordered_required_inputs = self._calc_fetch_order_for(required_inputs, dependency_graph)
+    def _update_campaign_specific_data_in(self, campaign_data_table, customer_id: str, campaign_specific_data: dict[str, any]):
+        print(f"updating campaign_specific_data to {campaign_specific_data}")
+        data, count = campaign_data_table.update({ 'campaign_specific_data': campaign_specific_data }).eq('customer_id', customer_id).execute()
+        print(f"updated {count} rows, data: {data}")
 
-        return ordered_required_inputs
-
-
-    def _create_data_dependency_graph(self) -> nx.DiGraph:
-        global data_dependencies
-
-        dependency_graph = nx.DiGraph()
-
-        for data, dependencies in data_dependencies.items():
-            dependency_graph.add_node(data)
-
-            for dep in dependencies:
-                dependency_graph.add_edge(dep, data)
-
-        return dependency_graph
-
-
-    def _load_data_deps(self) -> dict[str, list[str]]:
-        deps_file: Path = Path(__file__).parent / "data_dependencies.yml"
-
-        with open(deps_file, 'r') as file:
-            data = yaml.load(file, Loader=yaml.FullLoader)
-
-        return data
-
-
-    def _get_required_inputs_from(self, dependency_graph):
-        required_inputs = set()
-        queue = list(self._outputs)
-
-        while queue:
-            data = queue.pop()
-
-            if data not in required_inputs:
-                required_inputs.add(data)
-                queue.extend([pred for pred in dependency_graph.predecessors(data)])
-
-        return required_inputs
-
-
-    def _calc_fetch_order_for(self, required_inputs, dependency_graph):
-        subG = dependency_graph.subgraph(required_inputs)
-
-        try:
-            return list(nx.topological_sort(subG))
-        except nx.NetworkXUnfeasible:
-            raise ValueError("Data dependencies form a cycle, cannot determine execution order!")
-
-
-    # def _find_unimplemented_tasks_in(self, task_ordering):
-    #     missing_data = []
-
-    #     for data in task_ordering:
-    #         if data not in task_mapping:
-    #             missing_data.append(data)
-
-    #     return missing_data
+        # todo: check errors here
