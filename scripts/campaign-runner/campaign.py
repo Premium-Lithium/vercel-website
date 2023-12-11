6fc96be3365eced7015c718cea2d18a32105d1fd
lbowes <33807016+lbowes@users.noreply.github.com>
2023-12-11 12:07:01 +0000

    Remove code and unnecessary data duplication

--- scripts/campaign-runner/campaign.py
+++ scripts/campaign-runner/campaign.py
@@ -25,7 +25,7 @@ class Campaign:
         definition = self._load_campaign_definition_for(name)
 
         self._name = name
-        self._outputs = definition["campaign_specific_schema"]  # e.g "solarFlyer"
+        self._output = definition["campaign_specific_schema"]
         self._id = definition["campaign_id"]
 
 
@@ -49,19 +49,6 @@ class Campaign:
 
     def _calc_data_fetch_order_to_calc_outputs(self):
         dependency_graph = self._create_data_dependency_graph()
-
-        # plt.figure(figsize=(12, 8))
-        # nx.draw(dependency_graph, with_labels=True, node_color='lightblue',
-        #         node_size=2000, font_size=10, font_weight='bold', arrowstyle='->', arrowsize=20)
-        # plt.title("Graph Visualization")
-        # plt.show()
-        # input()
-
-        # todo: implement this function
-        # if not path_from_inputs_to_outputs(dependency_graph):
-        #     raise ValueError("Cannot calculate outputs, no path from inputs to outputs.")
-
-        # required_inputs = self._get_required_inputs_from(dependency_graph)
         ordered_required_inputs = self._calc_fetch_order_for(dependency_graph)
 
         return ordered_required_inputs
@@ -86,48 +73,48 @@ class Campaign:
 
 
     def _calc_fetch_order_for(self, dependency_graph):
-        # subG = dependency_graph.subgraph(required_inputs)
-
         try:
             return list(nx.topological_sort(dependency_graph))
         except nx.NetworkXUnfeasible:
-            raise ValueError("Data dependencies form a cycle, cannot determine execution order!")
+            raise ValueError("Data dependencies form a cycle, cannot determine execution order.")
 
 
     def _fetch_data_in(self, data_ordering: list[str]):
         campaign_data_table = supabase.table('campaign_customers')
-
         campaign_items = campaign_data_table.select("*").eq('campaign_id', self._id).execute()
+
         campaign_data = campaign_items.data
 
         if not campaign_data:
             raise ValueError(f"Could not fetch campaign items for campaign '{self._name}'")
 
         for customer in campaign_data:
-            campaign_specific_data = customer['campaign_specific_data']
+            customer_data = customer
+            customer_data['campaign_specific_data'] = customer_data['campaign_specific_data'] or {}
 
             for data_name in data_ordering:
-                required_arg_names = data_dependencies[data_name]
+                dependency_names = data_dependencies[data_name]
 
-                data = {}
+                if not dependency_names:
+                    continue
 
-                if required_arg_names:
-                    args = self._extract_data_from(campaign_specific_data, required_arg_names)
-                    task = getattr(tasks, getter_func_name(data_name))
-                    data = task(**args)
-                else:
-                    data = customer.get(data_name, None)
+                args = self._extract_data_from(customer_data, dependency_names)
+                task = getattr(tasks, getter_func_name(data_name))
+                data = task(**args)
 
-                campaign_specific_data = self._add_results_to(campaign_specific_data, data, data_name)
+                customer_data['campaign_specific_data'][data_name] = data
 
-            self._update_campaign_specific_data_in(campaign_data_table, customer["customer_id"], campaign_specific_data)
+            data, count = campaign_data_table.update(customer_data).eq('customer_id', customer['customer_id']).execute()
 
 
-    def _extract_data_from(self, campaign_specific_data: dict[str, any], data: list[str]) -> dict[str, any]:
+    def _extract_data_from(self, customer_data: dict[str, any], data: list[str]) -> dict[str, any]:
         args = {}
 
         for data_name in data:
-            value = campaign_specific_data.get(data_name, None)
+            value = customer_data.get(data_name, None)
+
+            if value is None:
+                value = customer_data['campaign_specific_data'].get(data_name, None)
 
             if value is None:
                 raise ValueError(f"Could not find data '{data_name}' in customer record")
@@ -135,20 +122,3 @@ class Campaign:
             args[data_name] = value
 
         return args
-
-
-    def _add_results_to(self, campaign_specific_data, results: dict[any, any], name: str) -> dict[str, any]:
-        if campaign_specific_data:
-            campaign_specific_data[name] = results
-        else:
-            campaign_specific_data = { name: results }
-
-        return campaign_specific_data
-
-
-    def _update_campaign_specific_data_in(self, campaign_data_table, customer_id: str, campaign_specific_data: dict[str, any]):
-        print(f"updating campaign_specific_data to {campaign_specific_data}")
-        data, count = campaign_data_table.update({ 'campaign_specific_data': campaign_specific_data }).eq('customer_id', customer_id).execute()
-        print(f"updated {count} rows, data: {data}")
-
-        # todo: check errors here
