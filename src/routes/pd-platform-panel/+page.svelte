f47e5a19740f38ec7cdea15ca6966af3015fcc00
Peter Gillingham <petergillingham80@gmail.com>
2024-01-31 12:32:13 +0000

    strcture

---
+++ src/routes/pd-platform-panel/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { page } from '$app/stores'
+	import AppExtensionsSDK from '@pipedrive/app-extensions-sdk'
+
+	const dealId = $page.url.searchParams.get('selectedIds')
+
+	let sdk: AppExtensionsSDK
+
+	onMount(async () => {
+		sdk = await new AppExtensionsSDK().initialize({ size: { height: 300 } })
+		if (dealId) {
+			getDealDetails(dealId)
+		}
+	})
+
+	async function getDealDetails(id: string) {}
+</script>
+
+<style>
+    
+</style>
